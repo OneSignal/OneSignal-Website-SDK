7cd2b4ccc08e62fadf5d35df896593ade2247e24
Jason Pang <jason@onesignal.com>
2016-03-23 15:02:53 -0700

    Alt impl crypto in case it doesn't exist.

--- src/utils.js
+++ src/utils.js
@@ -292,8 +292,15 @@ export function when(condition, promiseIfTrue, promiseIfFalse) {
 export function guid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var crypto = window.crypto || window.msCrypto;
-    var r = crypto.getRandomValues(new Uint8Array(1))[0]%16|0, v = c == 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
+    if (crypto) {
+      var r = crypto.getRandomValues(new Uint8Array(1))[0] % 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    } else {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+      });
+    }
   });
 }
 
