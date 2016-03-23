6d6b21265504e1f28c0d36d19de74f7dd3b3bb97
Jason Pang <jason@onesignal.com>
2016-03-23 11:13:32 -0700

    Fix: guid() function breaks on IE11 before isPushNotificationsSupported() can be called. Use window.msCrypto instead of window.crypto.

--- src/utils.js
+++ src/utils.js
@@ -291,6 +291,7 @@ export function when(condition, promiseIfTrue, promiseIfFalse) {
 
 export function guid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var crypto = window.crypto || window.msCrypto;
     var r = crypto.getRandomValues(new Uint8Array(1))[0]%16|0, v = c == 'x' ? r : (r&0x3|0x8);
     return v.toString(16);
   });
