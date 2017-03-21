a4aaa5c43f9f7f7fe5c5977d8335ecfe1f5a3424
Jason Pang <jason@jasonpang.me>
2017-03-21 15:36:00 -0700

    Fix notification click navigating and focusing
    With standard SDK settings, when a clicked notification's URL is
    identical to an open tab's, that tab is focused. Any other
    notification URL would open a new tab.
    
    We offer a 'notificationClickHandlerMatch' option, which says as long as
    there's an open tab on your notification URL's origin, without needing
    the tab's URL to match the notification's URL exactly, we won't open a
    new tab, and we'll use the existing open tab.
    
    The bug was that this existing open tab was focused, and not navigated
    to the notification's URL.
    
    If the open tab was to https://google.com, and if the notification's URL
    was to https://google.com/page, the open tab would be focused and
    nothing else would happen. The correct behavior is to navigate this tab
    to https://google.com/page.
    
    One exception is if the URL is already the same. In this case, it seems
    unnecessary to refresh the page. The site dev can listen to
    addListenerForNotificationOpened() and hook onto the notification click
    event and refresh the page if necessary.
    
    Another exception is if client.navigate() isn't an available API.

--- src/service-worker/ServiceWorker.ts
+++ src/service-worker/ServiceWorker.ts
@@ -569,7 +569,7 @@ export default class ServiceWorker {
      be focused instead of an identical new tab being created.
      */
     let doNotOpenLink = false;
-    for (let client of activeClients) {
+    for (let client: WindowClient of activeClients) {
       let clientUrl = client.url;
       if ((client as any).isSubdomainIframe) {
         const lastKnownHostUrl = await this.database.get<string>('Options', 'lastKnownHostUrl');
@@ -591,15 +591,34 @@ export default class ServiceWorker {
       } catch (e) {
       }
 
+      await Database.put("NotificationOpened", { url: launchUrl, data: notification, timestamp: Date.now() });
+
       if ((notificationClickHandlerMatch === 'exact' && clientUrl === launchUrl) ||
         (notificationClickHandlerMatch === 'origin' && clientOrigin === launchOrigin)) {
-        (client as any).focus();
-        (swivel as any).emit(client.id, 'notification.clicked', notification);
+        if (client.url === launchUrl) {
+          (swivel as any).emit(client.id, 'notification.clicked', notification);
+          await client.focus();
+        } else {
+          /*
+          We must focus first; once the client navigates away, it may not be to a service worker-controlled page, and
+          the client ID may change, making it unable to focus.
+
+          client.navigate() is available on Chrome 49+ and Firefox 50+.
+           */
+          if (client.navigate) {
+            await client.focus();
+            await client.navigate(launchUrl);
+          } else {
+            /*
+            If client.navigate() isn't available, we have no other option but to open a new tab to the URL.
+             */
+            await ServiceWorker.openUrl(launchUrl);
+          }
+        }
         doNotOpenLink = true;
       }
     }
 
-    await this.database.put("NotificationOpened", { url: launchUrl, data: notification, timestamp: Date.now() });
     if (notificationOpensLink && !doNotOpenLink) {
       await this.openUrl(launchUrl);
     }
