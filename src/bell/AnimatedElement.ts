197bfccba9e59860dff8e313cdc9582e2b3b4db4
Jason Pang <jason@jasonpang.me>
2017-04-03 11:39:45 -0700

    Fix spelling error
    Correcting the spelling reduces log verbosity; unimportant events are
    hidden from logging.

--- src/bell/AnimatedElement.ts
+++ src/bell/AnimatedElement.ts
@@ -134,7 +134,7 @@ export default class AnimatedElement {
       SHOWING: 'animatedElementShowing',
       SHOWN: 'animatedElementShown',
       HIDING: 'animatedElementHiding',
-      HIDDEN: 'aniamtedElementHidden',
+      HIDDEN: 'animatedElementHidden',
     };
   }
 
