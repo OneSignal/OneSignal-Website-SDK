18844ec52f1bd56767f746f671d677274ad0e90c
Jason Pang <jason@jasonpang.me>
2017-04-19 14:39:23 -0700

    Safe initialize to empty object if null options
    Just to be safe, initialize to an empty object if no options are passed.
    In the few location sthat we are calling initHttp(), we always pass
    options (even if the option values are blank), but in case we do not
    pass an option, the SDK would not initialize since OneSignal.config
    would be null and we don't want it to hang in that case.

--- src/helpers/HttpHelper.ts
+++ src/helpers/HttpHelper.ts
@@ -59,7 +59,7 @@ must be opened as a result of a subscription call.</span>`);
     }
 
     // Forgetting this makes all our APIs stall forever because the promises expect this to be true
-    OneSignal.config = options;
+    OneSignal.config = options || {};
     OneSignal.initialized = true;
 
     let sendToOrigin = options.origin;
