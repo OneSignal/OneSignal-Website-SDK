dd16f4a41e46efdf0f4d1ddc94fb6d99856d647a
Jason Pang <jason@jasonpang.me>
2017-05-10 16:33:28 -0700

    Dont display database open errors
    To prevent error reporting tools like Sentry.io from picking up errors
    that the site owner can't do anything about and use up their quota, hide
    database open errors.
    
    For errors like "Quota Exceeded" or "Firefox profile
    upgraded/corrupted".

--- src/services/IndexedDb.ts
+++ src/services/IndexedDb.ts
@@ -43,14 +43,19 @@ export default class IndexedDb {
   }
 
   private onDatabaseOpenError(event) {
+    // Prevent the error from bubbling: https://bugzilla.mozilla.org/show_bug.cgi?id=1331103#c3
+    /**
+     * To prevent error reporting tools like Sentry.io from picking up errors that
+     * the site owner can't do anything about and use up their quota, hide database open
+     * errors.
+     */
+    event.preventDefault();
     const error = (<any>event.target).error;
     if (contains(error.message, 'The operation failed for reasons unrelated to the database itself and not covered by any other error code') ||
       contains(error.message, 'A mutation operation was attempted on a database that did not allow mutations')) {
       log.warn("OneSignal: IndexedDb web storage is not available on this origin since this profile's IndexedDb schema has been upgraded in a newer version of Firefox. See: https://bugzilla.mozilla.org/show_bug.cgi?id=1236557#c6");
-      // Prevent the error from bubbling: https://bugzilla.mozilla.org/show_bug.cgi?id=1331103#c3
-      event.preventDefault();
     } else {
-      log.debug('IndexedDb: Specific database open error event');
+      log.warn('OneSignal: Fatal error opening IndexedDb database:', error);
     }
   }
 
