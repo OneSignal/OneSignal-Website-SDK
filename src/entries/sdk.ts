110730e6fcdc903a3256bcf5f9dbf592e84b3931
Josh Kasten <josh@onesignal.com>
2018-05-10 00:26:51 -0700

    Switched tsconfig baseUrl from root to src dir

--- src/entries/sdk.ts
+++ src/entries/sdk.ts
@@ -1,9 +1,9 @@
 /**
  * Pages and older service workers will include this entry point, which is the full SDK.
  */
-import { oneSignalSdkInit } from 'src/utils/pageSdkInit';
-import { WindowEnvironmentKind } from 'src/models/WindowEnvironmentKind';
-import SdkEnvironment from 'src/managers/SdkEnvironment';
+import { oneSignalSdkInit } from 'utils/pageSdkInit';
+import { WindowEnvironmentKind } from 'models/WindowEnvironmentKind';
+import SdkEnvironment from 'managers/SdkEnvironment';
 
 const windowEnv = SdkEnvironment.getWindowEnv();
 if (windowEnv === WindowEnvironmentKind.ServiceWorker) {
