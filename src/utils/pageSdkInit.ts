9bad6bef74849e3d58962f2c8256738f8cdab832
Josh Kasten <josh@onesignal.com>
2018-05-11 10:57:56 -0700

    Added error checking to OneSignal.push
    * Exceptions are thrown on invalid parameters

--- src/utils/pageSdkInit.ts
+++ src/utils/pageSdkInit.ts
@@ -2,7 +2,6 @@ import OneSignalStub from "../OneSignalStub";
 import Log from "../libraries/Log";
 import { incrementSdkLoadCount, getSdkLoadCount, isPushNotificationsSupported } from "../utils";
 
-
 export function oneSignalSdkInit() {
   incrementSdkLoadCount();
   if (getSdkLoadCount() > 1) {
@@ -10,22 +9,36 @@ export function oneSignalSdkInit() {
       `SDK only once on your page.`);
     Log.debug(`OneSignal: Exiting from SDK initialization to prevent double-initialization errors. ` +
       `Occurred ${getSdkLoadCount()} times.`);
-  } else {
-    // We're running in the host page, iFrame of the host page, or popup window
-    // Load OneSignal's web SDK
-    if (typeof OneSignal !== "undefined") {
-      var predefinedOneSignalPushes = OneSignal;
-    }
-
-    if (isPushNotificationsSupported()) {
-      (window as any).OneSignal = require('../OneSignal').default;
-    } else {
-      Log.debug('OneSignal: Push notifications are not supported. A stubbed version of the SDK will be initialized.');
-      (window as any).OneSignal = OneSignalStub;
-    }
-
-    if (predefinedOneSignalPushes)
-      for (var i = 0; i < predefinedOneSignalPushes.length; i++)
-        OneSignal.push(predefinedOneSignalPushes[i]);
+    return;
   }
+
+  // We're running in the host page, iFrame of the host page, or popup window
+  // Load OneSignal's web SDK
+  let predefinedOneSignalPushes: object[] | object | undefined | null;
+  if (typeof OneSignal !== "undefined")
+    predefinedOneSignalPushes = OneSignal;
+
+  if (isPushNotificationsSupported())
+    (window as any).OneSignal = require('../OneSignal').default;
+  else {
+    Log.debug('OneSignal: Push notifications are not supported. A stubbed version of the SDK will be initialized.');
+    (window as any).OneSignal = OneSignalStub;
+  }
+
+  if (predefinedOneSignalPushes) {
+    if (!Array.isArray(predefinedOneSignalPushes)) {
+      Log.error("'OneSignal' was not defined as an Array. Skipping pushes");
+      return;
+    }
+
+    for (const item of predefinedOneSignalPushes) {
+      try {
+        OneSignal.push(item);
+      } catch (e) {
+        // Catch and log error here so other elements still run
+        Log.error(e);
+      }
+    }
+  }
+
 }
