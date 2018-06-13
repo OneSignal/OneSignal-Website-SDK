c842a7115b89ed9bcf134ba2c56cbff574a90232
Josh Kasten <josh@onesignal.com>
2018-06-13 02:02:35 -0700

    Output build files pre-fixed to release folder
    * Also changed pathing to root level in dev builds to match production

--- build/scripts/publish.sh
+++ build/scripts/publish.sh
@@ -1,2 +1,26 @@
-# Create OneSignalSDKUpdaterWorker.js as a copy of OneSignalSDKWorker.js required for service worker updating 
-cp build/bundles/OneSignalSDKWorker.js build/bundles/OneSignalSDKUpdaterWorker.js
+#!/usr/bin/env bash
+getPrefix() {
+  if [ "$ENV" = "production" ]; then
+    echo ""
+  elif [ "$ENV" = "staging" ]; then
+    echo "Staging-"
+  else [ "$ENV" = "development" ]
+    echo "Dev-"
+  fi
+}
+
+PREFIX=$(getPrefix)
+set -x
+
+pwd
+
+mkdir build/releases
+
+cp build/bundles/OneSignalSDK.js build/releases/$PREFIX"OneSignalSDK.js"
+cp build/bundles/OneSignalSDK.js.map build/releases/$PREFIX"OneSignalSDK.js.map"
+cp build/bundles/OneSignalSDKWorker.js build/releases/$PREFIX"OneSignalSDKWorker.js"
+cp build/bundles/OneSignalSDKWorker.js.map build/releases/$PREFIX"OneSignalSDKWorker.js.map"
+cp build/bundles/OneSignalSDKWorker.js build/releases/$PREFIX"OneSignalSDKUpdaterWorker.js"
+cp build/bundles/OneSignalSDKWorker.js.map build/releases/$PREFIX"OneSignalSDKUpdaterWorker.js.map"
+cp build/bundles/OneSignalSDKStyles.css build/releases/$PREFIX"OneSignalSDKStyles.css"
+cp build/bundles/OneSignalSDKStyles.css.map build/releases/$PREFIX"OneSignalSDKStyles.css.map"
