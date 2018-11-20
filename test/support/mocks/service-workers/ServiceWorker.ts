90b24f9f1eadf294494421654d84dedcd4bed6f4
Josh Kasten <josh@onesignal.com>
2018-11-20 15:53:46 -0800

    Add null notification and app id handling
    * Also some other misc code clean up
    * Added test for null app id

--- test/support/mocks/service-workers/ServiceWorker.ts
+++ test/support/mocks/service-workers/ServiceWorker.ts
@@ -1,16 +1,10 @@
 import NotImplementedError from '../../../../src/errors/NotImplementedError';
 
-import { ServiceWorker as ServiceWorkerReal } from '../../../../src/service-worker/ServiceWorker';
-
 export default class ServiceWorker implements EventTarget {
   scriptURL: string;
   state: ServiceWorkerState;
 
-  postMessage(message: any, transfer: any = []): void {}
-
-  addEventListener = () => { throw new NotImplementedError() };
-  removeEventListener = () => { throw new NotImplementedError() };
-  dispatchEvent = () => { throw new NotImplementedError() };
-  // event
-  // EventHandler onstatechange;
+  addEventListener = () => { throw new NotImplementedError(); };
+  removeEventListener = () => { throw new NotImplementedError(); };
+  dispatchEvent = () => { throw new NotImplementedError(); };
 }
