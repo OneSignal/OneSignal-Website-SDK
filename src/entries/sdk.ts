9b6fdfd4cb502155548e2334771580ff4529ec68
Josh Kasten <josh@onesignal.com>
2019-02-28 11:33:53 -0800

    * Fixed ES5 Shim loading
    * TODO on tests
    * Fixed some comments and error logging

--- src/entries/sdk.ts
+++ src/entries/sdk.ts
@@ -3,8 +3,9 @@
  *   * This is an entry point for pages and older service workers.
  *   * Also we define a ES5 Stub for OneSignal for browsers that do not support push.
  * This is a shim to detect and load either;
- *   * ServiceWorkerSDK (ES6)                 - OneSignalSDKWorker.js
- *   * PageSDK (ES6)                          - OneSignalPageSDKES6.js
+ *   * ServiceWorkerSDK (ES6) - OneSignalSDKWorker.js
+ *   * PageSDK (ES6)          - OneSignalPageSDKES6.js
+ *   * Stub-PageSDK (ES5)     - OneSignalSDK.js (This File)
  */
 
 // NOTE: Careful if adding imports, ES5 targets can't clean up functions never called.
@@ -12,7 +13,7 @@ import { isPushNotificationsSupported } from "../utils/BrowserSupportsPush";
 import { OneSignalStubES6 } from "../utils/OneSignalStubES6";
 import { OneSignalStubES5 } from "../utils/OneSignalStubES5";
 
-// NOTE: scripts added won't start executing until all other existing scripts have been executed.
+// NOTE: scripts added won't start executing until
 function addScriptToPage(url: string): void {
   const scriptElement = document.createElement('script');
   scriptElement.src = url;
@@ -40,7 +41,7 @@ function serviceWorkerSupportsPush(): boolean {
   return (typeof self.registration !== "undefined");
 }
 
-function addOneSignalPageSDKStub(): void {
+function addOneSignalPageES6SDKStub(): void {
   const oneSignalExists = (typeof (window as any).OneSignal !== "undefined");
   const oneSignalIsArray = Array.isArray((window as any).OneSignal);
 
@@ -56,27 +57,22 @@ function addOneSignalPageSDKStub(): void {
 
   // 2. If this script was loaded with async or defer no need to create a stub.
   //    This means the site developer would have to be using OneSignal.push(...) already
-  // This technically isn't needed but due to the complexly of OneSignalStubES6
+  // This check technically isn't needed but due to the complexly of OneSignalStubES6
   //    would like to only load it when needed in case of any issues
-  const thisScript: any = document.currentScript; // TODO: Can drop any here after updating TypeScript
+  const thisScript: any = document.currentScript; // TODO: Can drop 'any' after updating TypeScript
   if (thisScript) {
     if (thisScript.async || thisScript.defer)
       return;
   }
 
-  // 3. Which stub should we load based on push support
-  isPushNotificationsSupported() ? addStubsForES6() : addStubsForES5();
-}
-
-// Stub out all functions but save calls into push(...), OneSignalPageSDKES6.js will load soon.
-function addStubsForES6(): void {
+  // 3. Stub out all functions but save calls into push(...), OneSignalPageSDKES6.js will load soon.
   console.error("Deprecation Warning!: Please load OneSignalSDK.js async and use 'var OneSignal = OneSignal || [];'" +
     "Direct calls to OneSignal without going through OneSignal.push(...) will be dropped in the future! ");
   (window as any).OneSignal = new OneSignalStubES6();
 }
 
 // Stub out all functions with default values. Unlike ES6 all ES5 support will be in OneSignalSDK.js
-function addStubsForES5(): void {
+function addOneSignalPageES5SDKStub(): void {
   (window as any).OneSignal = new OneSignalStubES5();
 }
 
@@ -85,9 +81,15 @@ function OneSignalShimMain(): void {
     if (serviceWorkerSupportsPush()) {
       (self as any).importScripts(`${getPathAndPrefix()}OneSignalSDKWorker.js?v=${__VERSION__}`);
     }
-  } else if (isPushNotificationsSupported()) {
+  }
+  else if (isPushNotificationsSupported()) {
     addScriptToPage(`${getPathAndPrefix()}OneSignalPageSDKES6.js?v=${__VERSION__}`);
-    addOneSignalPageSDKStub();
+    addOneSignalPageES6SDKStub();
+  }
+  else {
+    addOneSignalPageES5SDKStub();
+    // TODO: Move this to a new class to make sure this is all working!
+    // TODO: Also test for if .push!
   }
 }
 
