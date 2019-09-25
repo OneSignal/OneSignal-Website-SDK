7e02a11ba236d67248fec79e59b2ddb565b751c3
Iryna Trush <iryna@onesignal.com>
2019-09-24 17:27:03 -0700

    Updated outcomes config to be flexible per type of outcome

--- src/helpers/ConfigHelper.ts
+++ src/helpers/ConfigHelper.ts
@@ -381,11 +381,7 @@ export class ConfigHelper {
           allowLocalhostAsSecureOrigin: serverConfig.config.setupBehavior ?
             serverConfig.config.setupBehavior.allowLocalhostAsSecureOrigin : undefined,
           requiresUserPrivacyConsent: userConfig.requiresUserPrivacyConsent,
-          outcomes: {
-            ...serverConfig.config.outcomes,
-            influencedTimePeriodMin: serverConfig.config.outcomes.influencedTimePeriodMin,
-            influencedNotificationsLimit: serverConfig.config.outcomes.influencedNotificationsLimit,
-          }
+          outcomes: serverConfig.config.outcomes,
         };
       case IntegrationConfigurationKind.JavaScript:
         /*
@@ -412,11 +408,7 @@ export class ConfigHelper {
                 : 'OneSignalSDUpdaterKWorker.js',
             path: !!userConfig.path ? userConfig.path : '/'
           },
-          outcomes: {
-            ...serverConfig.config.outcomes,
-            influencedTimePeriodMin: serverConfig.config.outcomes.influencedTimePeriodMin,
-            influencedNotificationsLimit: serverConfig.config.outcomes.influencedNotificationsLimit,
-          }
+          outcomes: serverConfig.config.outcomes,
         };
 
         if (userConfig.hasOwnProperty("autoResubscribe")) {
--- src/models/AppConfig.ts
+++ src/models/AppConfig.ts
@@ -100,6 +100,21 @@ export interface AppUserConfig {
   allowLocalhostAsSecureOrigin?: boolean;
   requiresUserPrivacyConsent?: boolean;
   pageUrl?: string;
+  outcomes?: OutcomesConfig;
+}
+
+export interface OutcomesConfig {
+  direct: {
+    enabled: boolean;
+  }
+  indirect: {
+    enabled: boolean;
+    influencedTimePeriodMin: number;
+    influencedNotificationsLimit: number;
+  }
+  unattributed: {
+    enabled: boolean;
+  }
 }
 
 interface BasePromptOptions {
@@ -370,10 +385,7 @@ export interface ServerAppConfig {
         persist: boolean;
       };
     };
-    outcomes: {
-      influencedTimePeriodMin: number;
-      influencedNotificationsLimit: number;
-    },
+    outcomes: OutcomesConfig,
     vapid_public_key: string;
     onesignal_vapid_public_key: string;
     http_use_onesignal_com?: boolean;
