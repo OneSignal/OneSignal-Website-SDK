e353aa409d855c0ca5d0ae1c0342d3061f52ec87
Josh Kasten <josh@onesignal.com>
2019-09-29 00:54:04 -0700

    Fix isPush support on safari in an iframe
    * Unfortunately window.safari is not defined on Safari if in an iframe context
    * Due to this there is no other way than to have fallback logic to
       check navigator properties if we are on macOS Safari.

--- src/utils/BrowserSupportsPush.ts
+++ src/utils/BrowserSupportsPush.ts
@@ -3,27 +3,38 @@
 
 // Checks if the browser supports push notifications by checking if specific
 //   classes and properties on them exist
+export function isPushNotificationsSupported() {
+  return supportsVapidPush() || supportsSafariPush();
+}
 
+// Does the browser support Safari's proprietary push
+function supportsSafariPush(): boolean {
+  if (typeof window.safari !== "undefined" &&
+      typeof window.safari.pushNotification !== "undefined") {
+    return true;
+  }
+
+  // Fallback detection for Safari on macOS in an iframe context
+  return window.top !== window && // isContextIframe
+         navigator.vendor === "Apple Computer, Inc." && // isSafari
+         navigator.platform === "MacIntel"; // isMacOS
+}
+
+// Does the browser support the standard Push API
+function supportsVapidPush(): boolean {
+  return typeof PushSubscriptionOptions !== "undefined" &&
+         PushSubscriptionOptions.prototype.hasOwnProperty("applicationServerKey");
+}
+
+/* Notes on browser results which lead the logic of the functions above */
 // Safari
 //   macOS - typeof safari.pushNotification == "object"
+//         - iframe context - typeof safari == "undefined"
 //   iOS -  typeof safari == "undefined"
 // Chrome
 //   HTTP & HTTPS - typeof PushSubscriptionOptions == "function"
+//   HTTP - navigator.serviceWorker == "undefined"
 // Firefox
 //   Normal - typeof PushSubscriptionOptions == "function"
 //     HTTP & HTTPS - typeof PushSubscriptionOptions == "function"
 //   ESR - typeof PushSubscriptionOptions == "undefined"
-
-export function isPushNotificationsSupported() {
-  // window.safari will be present on macOS only
-  if (typeof window.safari !== "undefined") {
-    return typeof window.safari.pushNotification !== "undefined";
-  }
-
-  if (typeof PushSubscriptionOptions !== "undefined") {
-    // applicationServerKey required for VAPID
-    return PushSubscriptionOptions.prototype.hasOwnProperty("applicationServerKey");
-  }
-
-  return false;
-}
