5546489211605dab3a4ed5a2e2d8dcd6d9e6d4b0
Iryna Trush <iryna@onesignal.com>
2019-12-20 14:12:55 -0800

    New session initialization logic

--- src/models/Session.ts
+++ src/models/Session.ts
@@ -6,6 +6,9 @@ export enum SessionStatus {
 
 export interface Session {
   sessionKey: string;
+  appId: string;
+  deviceId: string;
+  deviceType: number;
   startTimestamp: number;
   accumulatedDuration: number;
   notificationId: string | null; // for direct clicks
@@ -15,3 +18,24 @@ export interface Session {
 }
 
 export const ONESIGNAL_SESSION_KEY = "oneSignalSession";
+
+export function initializeNewSession(
+  options: Partial<Session> & { deviceId: string; appId: string, deviceType: number; }
+): Session {
+  const currentTimestamp = new Date().getTime();
+  const sessionKey = options && options.sessionKey || ONESIGNAL_SESSION_KEY;
+  const notificationId = options && options.notificationId || null;
+
+  return {
+    sessionKey,
+    appId: options.appId,
+    deviceId: options.deviceId,
+    deviceType: options.deviceType,
+    startTimestamp: currentTimestamp,
+    accumulatedDuration: 0,
+    notificationId,
+    status: SessionStatus.Active,
+    lastDeactivatedTimestamp: null,
+    lastActivatedTimestamp: currentTimestamp,
+  }
+}
