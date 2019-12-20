b24c6dc0152b9c858be27e0919eee22d83515499
Iryna Trush <iryna@onesignal.com>
2019-12-20 14:20:09 -0800

    SessionManager: initial implementation of session tracking logic

--- src/managers/SessionManager.ts
+++ src/managers/SessionManager.ts
@@ -1,9 +1,56 @@
 import { ContextSWInterface } from "../models/ContextSW";
+import { PushDeviceRecord } from "../models/PushDeviceRecord";
+import { SessionPayload, SessionOrigin } from "../models/Session";
+import Log from "../libraries/Log";
+import { WorkerMessengerCommand } from "../libraries/WorkerMessenger";
 
 export class SessionManager {
   private context: ContextSWInterface;
 
   constructor(context: ContextSWInterface) {
     this.context = context;
+  }
+
+  public async notifySWToUpsertSession(
+    deviceId: string | undefined,
+    deviceRecord: PushDeviceRecord,
+    sessionOrigin: SessionOrigin
+  ): Promise<void> {
+    Log.debug("Notify SW to upsert session");
+    const payload: SessionPayload = {
+      deviceId,
+      deviceRecord: deviceRecord.serialize(),
+      sessionThreshold: OneSignal.config.sessionThreshold,
+      enableSessionDuration: OneSignal.config.enableSessionDuration,
+      sessionOrigin,
+    };
+    await this.context.workerMessenger.unicast(WorkerMessengerCommand.SessionUpsert, payload);
+  }
+
+  public async notifySWToDeactivateSession(
+    deviceId: string | undefined,
+    deviceRecord: PushDeviceRecord,
+    sessionOrigin: SessionOrigin
+  ): Promise<void> {
+    Log.debug("Notify SW to deactivate session");
+    const payload: SessionPayload = {
+      deviceId,
+      deviceRecord: deviceRecord.serialize(),
+      sessionThreshold: OneSignal.config.sessionThreshold,
+      enableSessionDuration: OneSignal.config.enableSessionDuration,
+      sessionOrigin,
+    };
+    await this.context.workerMessenger.unicast(WorkerMessengerCommand.SessionDeactivate, payload);
+  }
+
+  public async upsertSession(
+    deviceId: string,
+    deviceRecord: PushDeviceRecord,
+    sessionOrigin: SessionOrigin
+  ): Promise<void> {
+
+    const sessionPromise = this.notifySWToUpsertSession(deviceId, deviceRecord, sessionOrigin);
+
+    await sessionPromise;
   }
 }
