d7abb1be488924ea47b96cd7533990e61d49bd60
Rodrigo <rogomezpalacio@protonmail.com>
2019-12-20 17:33:20 -0600

    Addressed nits
    - moved colon in LocalStorage methods

--- src/utils/LocalStorage.ts
+++ src/utils/LocalStorage.ts
@@ -2,19 +2,19 @@ const IS_OPTED_OUT = "isOptedOut";
 const IS_PUSH_NOTIFICATIONS_ENABLED = "isPushNotificationsEnabled";
 
 export default class LocalStorage {
-    public static getIsOptedOut() : boolean {
+    public static getIsOptedOut(): boolean {
         return localStorage.getItem(IS_OPTED_OUT) === "true";
     }
 
-    public static getIsPushNotificationsEnabled() : boolean {
+    public static getIsPushNotificationsEnabled(): boolean {
         return localStorage.getItem(IS_PUSH_NOTIFICATIONS_ENABLED) === "true";
     }
 
-    public static setIsOptedOut(value: boolean) :void {
+    public static setIsOptedOut(value: boolean): void {
         localStorage.setItem(IS_OPTED_OUT, value.toString());
     }
 
-    public static setIsPushNotificationsEnabled(value: boolean) :void {
+    public static setIsPushNotificationsEnabled(value: boolean): void {
         localStorage.setItem(IS_PUSH_NOTIFICATIONS_ENABLED, value.toString());
     }
 }
