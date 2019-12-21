1ee6257dfe257e029557b655f700a7557414511a
Iryna Trush <iryna@onesignal.com>
2019-12-20 16:34:45 -0800

    Added page event for visibility change. It tracks switching to a different tab, fully covering page with another window, screen lock/unlock.

--- src/managers/SessionManager.ts
+++ src/managers/SessionManager.ts
@@ -1,6 +1,7 @@
 import { ContextSWInterface } from "../models/ContextSW";
 import { PushDeviceRecord } from "../models/PushDeviceRecord";
 import { SessionPayload, SessionOrigin } from "../models/Session";
+import MainHelper from "../helpers/MainHelper";
 import Log from "../libraries/Log";
 import { WorkerMessengerCommand } from "../libraries/WorkerMessenger";
 
@@ -43,14 +44,50 @@ export class SessionManager {
     await this.context.workerMessenger.unicast(WorkerMessengerCommand.SessionDeactivate, payload);
   }
 
-  public async upsertSession(
+  async handleVisibilityChange(): Promise<void> {
+    const visibilityState = document.visibilityState;
+
+    const [deviceId, deviceRecord] = await Promise.all([
+      MainHelper.getDeviceId(),
+      MainHelper.createDeviceRecord(this.context.appConfig.appId)
+    ]);
+
+    if (visibilityState === "visible") {
+      this.notifySWToUpsertSession(deviceId, deviceRecord, SessionOrigin.VisibilityVisible);
+      return;
+    }
+
+    if (visibilityState === "hidden") {
+      this.notifySWToDeactivateSession(deviceId, deviceRecord, SessionOrigin.VisibilityHidden);
+      return;
+    }
+
+    // it should never be anything else at this point
+    Log.warn("Unhandled visibility state happened", visibilityState);
+  }
+
+  async upsertSession(
     deviceId: string,
     deviceRecord: PushDeviceRecord,
     sessionOrigin: SessionOrigin
   ): Promise<void> {
-
     const sessionPromise = this.notifySWToUpsertSession(deviceId, deviceRecord, sessionOrigin);
 
+    // TODO: Possibly need to add handling for "pagehide" event. And review all the cases both fire in general
+    // https://github.com/w3c/page-visibility/issues/18
+    this.setupSessionEventListeners();
+
     await sessionPromise;
+  }
+
+  setupSessionEventListeners(): void {
+    // Page lifecycle events https://developers.google.com/web/updates/2018/07/page-lifecycle-api
+
+    // To make sure we add these event listeners only once.
+    if (!OneSignal.cache.visibilityChangeListener) {
+      // tracks switching to a different tab, fully covering page with another window, screen lock/unlock
+      document.addEventListener("visibilitychange", this.handleVisibilityChange.bind(this), true);
+      OneSignal.cache.visibilityChangeListener = true;
+    }
   }
 }
