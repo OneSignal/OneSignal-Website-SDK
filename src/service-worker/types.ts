955ef5f9b07ac63e7c7b7c3b0a571d2e57d93989
Iryna Trush <iryna@onesignal.com>
2019-12-23 13:58:30 -0800

    Checking if there are any active pages to determine whether to upsert   or deactivate the session.
    Currently done for https only. Http support to follow.
    
    Unfortunately, safari is special and would not give the correct
      focused status for its pages to the service worker as chrome does.
    Had to work around it with a messaging to the page:
    1. sending a request to all active pages to return their focused states
    2. setting a timeout of 500ms to wait for responses
    3. added a handler on the page and in SW to react to the new events
    4. after timeout goes off, check current clientsStatus in SW to trigger
      session update

--- src/service-worker/types.ts
+++ src/service-worker/types.ts
@@ -1,3 +1,23 @@
 export interface OSWindowClient extends WindowClient {
   isSubdomainIframe: boolean;
 }
+
+export interface ClientStatus {
+  timestamp: number;
+  sentRequestsCount: number;
+  receivedResponsesCount: number;
+  hasAnyActiveSessions: boolean;
+}
+
+export interface PageVisibilityRequest {
+  timestamp: number;
+}
+
+export interface PageVisibilityResponse extends PageVisibilityRequest {
+  focused: boolean;
+}
+
+export interface OSServiceWorkerFields { 
+  timerId?: number;
+  clientsStatus?: ClientStatus;
+}
