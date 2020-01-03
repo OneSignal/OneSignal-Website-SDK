cc8175d28199e38eead5693d16338290364a5905
Iryna Trush <iryna@onesignal.com>
2020-01-02 16:11:57 -0800

    Refactored build script for easy extensions - each option has a key and can be omitted - each option has defaults
    Updated package.json instructions to comply with the new format

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -1,36 +1,60 @@
 #!/bin/bash
 # codepath for all commands of the form `yarn build:<env>-<env>`
-ENV=$1
-API=$2
+# defaults
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
 
-if [ -z "$3" ]; then
-  # no origin provided
-  echo "No build origin provided";
-else
-  # custom origin provided. api env must be dev
-  if [ $ENV == "development" ]; then
-    BUILD_ORIGIN=$3
-  else
-    # empty, default to production
-    BUILD_ORIGIN=""
-  fi
-fi
+case $key in
+    -b|--build)
+    BUILD_ORIGIN="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -a|--api)
+    API_ORIGIN="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -f|--from)
+    ENV="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -t|--target)
+    API="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --http)
+    HTTPS=false
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
 
-if [ -z "$4" ]; then
-  # no origin provided
-  echo "No API origin provided";
-else
-  # custom origin provided. api env must be dev
-  if [ $API == "development" ]; then
-    API_ORIGIN=$4
-  else
-    # empty, default to production
-    API_ORIGIN=""
-  fi
-fi
+echo "unknown options -> ${POSITIONAL}"
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
-ENV=$ENV API=$API BUILD_ORIGIN=$BUILD_ORIGIN API_ORIGIN=$API_ORIGIN yarn transpile:sources 
-ENV=$ENV API=$API BUILD_ORIGIN=$BUILD_ORIGIN API_ORIGIN=$API_ORIGIN yarn bundle-sw 
-ENV=$ENV API=$API BUILD_ORIGIN=$BUILD_ORIGIN API_ORIGIN=$API_ORIGIN yarn bundle-sdk 
-ENV=$ENV API=$API BUILD_ORIGIN=$BUILD_ORIGIN API_ORIGIN=$API_ORIGIN yarn bundle-page-sdk-es6
-ENV=$ENV API=$API BUILD_ORIGIN=$BUILD_ORIGIN API_ORIGIN=$API_ORIGIN ./build/scripts/publish.sh
+BUILD_ORIGIN=${BUILD_ORIGIN:-"default-build-origin"}
+API_ORIGIN=${API_ORIGIN:-"default-api-origin"}
+ENV=${ENV:-"default-env"}
+API=${API:-"default-api"}
+HTTPS=${HTTPS:-true}
+
+echo "BUILD_ORIGIN = ${BUILD_ORIGIN}"
+echo "API_ORIGIN = ${API_ORIGIN}"
+echo "HTTPS = ${HTTPS}"
+echo "ENV = ${ENV}"
+echo "API = ${API}"
+
+ENV=$ENV API=$API BUILD_ORIGIN=$BUILD_ORIGIN API_ORIGIN=$API_ORIGIN HTTPS=$HTTPS yarn transpile:sources 
+ENV=$ENV API=$API BUILD_ORIGIN=$BUILD_ORIGIN API_ORIGIN=$API_ORIGIN HTTPS=$HTTPS yarn bundle-sw 
+ENV=$ENV API=$API BUILD_ORIGIN=$BUILD_ORIGIN API_ORIGIN=$API_ORIGIN HTTPS=$HTTPS yarn bundle-sdk 
+ENV=$ENV API=$API BUILD_ORIGIN=$BUILD_ORIGIN API_ORIGIN=$API_ORIGIN HTTPS=$HTTPS yarn bundle-page-sdk-es6
+ENV=$ENV API=$API BUILD_ORIGIN=$BUILD_ORIGIN API_ORIGIN=$API_ORIGIN HTTPS=$HTTPS ./build/scripts/publish.sh
