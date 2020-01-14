97101fc8e8c53ab06ac0668f2d6c6dc2ce7f7961
Rodrigo <rogomezpalacio@protonmail.com>
2020-01-14 15:57:06 -0800

    Made all EnvironmentInfo params required

--- src/context/browser/models/EnvironmentInfo.ts
+++ src/context/browser/models/EnvironmentInfo.ts
@@ -2,11 +2,11 @@ import { Browser } from './Browser';
 
 // for runtime environment info
 export interface EnvironmentInfo {
-    browserType?: Browser;
-    browserVersion?: string|number;
-    isHttps?: boolean;
-    isUsingSubscriptionWorkaround?: boolean;
-    isBrowserAndSupportsServiceWorkers?: boolean;
-    requiresUserInteraction?: boolean;
-    osVersion?: string|number; 
+    browserType: Browser;
+    browserVersion: string|number;
+    isHttps: boolean;
+    isUsingSubscriptionWorkaround: boolean;
+    isBrowserAndSupportsServiceWorkers: boolean;
+    requiresUserInteraction: boolean;
+    osVersion: string|number; 
 }
