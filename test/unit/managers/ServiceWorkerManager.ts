OneSignal/OneSignal-Website-SDK

commit 031400d8fd21f771c345237d0db746c0e136bef3

installWorker now takes care of upgrade check

src/managers/ServiceWorkerManager.ts
@@ -197,6 +197,9 @@ export class ServiceWorkerManager {
     if (!Environment.supportsServiceWorkers())
       return false;
 
+    if (!OneSignal.config)
+      return false;
+
     // No, if configured to use our subdomain (AKA HTTP setup) AND this is on their page (HTTP or HTTPS).
     if (OneSignal.config.subdomain && SdkEnvironment.getWindowEnv() == WindowEnvironmentKind.Host)
       return false;
@@ -206,7 +209,8 @@ export class ServiceWorkerManager {
     if (workerState !== ServiceWorkerActiveState.WorkerA && workerState !== ServiceWorkerActiveState.WorkerB) {
       return true;
     }
-    return false;
+
+    return this.workerNeedsUpdate();
   }
 
   async subscribeForPushNotifications(): Promise<Subscription> {
@@ -228,16 +232,12 @@ export class ServiceWorkerManager {
    * with a content-identical but differently named alternate service worker
    * file.
    */
-  async updateWorker() {
-    if (!Environment.supportsServiceWorkers()) {
-      return;
-    }
-
+  async workerNeedsUpdate(): Promise<boolean> {
     const workerState = await this.getActiveState();
     Log.info(`[Service Worker Update] Checking service worker version...`);
-    let workerVersion;
+    let workerVersion: number;
     try {
-      workerVersion = await Utils.timeoutPromise(this.getWorkerVersion(), 2000);
+      workerVersion = await Utils.timeoutPromise(this.getWorkerVersion(), 2_000);
     } catch (e) {
       Log.info(`[Service Worker Update] Worker did not reply to version query; assuming older version.`);
       workerVersion = 1;
@@ -248,15 +248,17 @@ export class ServiceWorkerManager {
       Log.debug(
         `[Service Worker Update] Not updating service worker, current active worker state is ${workerState}.`
       );
-      return;
+      return false;
     }
 
     if (workerVersion !== Environment.version()) {
       Log.info(`[Service Worker Update] Updating service worker from v${workerVersion} --> v${Environment.version()}.`);
-      await this.installWorker();
+      return true;
     } else {
       Log.info(`[Service Worker Update] Service worker version is current at v${workerVersion} (no update required).`);
     }
+
+    return false;
   }
 
   /**
@@ -304,7 +306,7 @@ export class ServiceWorkerManager {
    * considered subscribed.
    */
   public async installWorker() {
-    if (!await OneSignal.context.serviceWorkerManager.shouldInstallWorker()) {
+    if (!await this.shouldInstallWorker()) {
       return;
     }
 

test/unit/managers/ServiceWorkerManager.ts
@@ -35,6 +35,7 @@ import { MockServiceWorkerGlobalScope } from "../../support/mocks/service-worker
 import MockNotification from "../../support/mocks/MockNotification";
 import { setUserAgent } from "../../support/tester/browser";
 import { ConfigIntegrationKind } from "../../../src/models/AppConfig";
+import Environment from '../../../src/Environment';
 
 declare var self: MockServiceWorkerGlobalScope;
 
@@ -385,7 +386,7 @@ test('installWorker() installs worker A when a third party service worker exists
   t.is(await manager.getActiveState(), ServiceWorkerActiveState.WorkerA);
 });
 
-test('installWorker() installs Worker B and then A when Worker A exists', async t => {
+test('installWorker() installs Worker B and then A when Worker A is out of date', async t => {
   await TestEnvironment.initialize({
     httpOrHttps: HttpHttpsEnvironment.Https
   });
@@ -396,28 +397,35 @@ test('installWorker() installs Worker B and then A when Worker A exists', async 
     registrationOptions: { scope: '/' }
   });
 
+  // 1. Install ServiceWorker A and assert it was ServiceWorker A
   await manager.installWorker();
   t.is(await manager.getActiveState(), ServiceWorkerActiveState.WorkerA);
 
+  // 2. Simulate an upgraded page SDK 
+  const envVerStub = sandbox.stub(Environment, "version").returns(2);
   const spy = sandbox.spy(navigator.serviceWorker, 'register');
 
-  const appConfig = OneSignal.context.appConfig;
-
+  // 3. Attempt to install ServiceWorker which should detect an upgrade
   await manager.installWorker();
 
+  const appConfig = OneSignal.context.appConfig;
   const registerOptions =  { scope: `${location.origin}/` };
   const serviceWorkerAPath = `${location.origin}/Worker-A.js?appId=${appConfig.appId}`;
   const serviceWorkerBPath = `${location.origin}/Worker-B.js?appId=${appConfig.appId}`;
 
+  // 4. Ensure we installed ServiceWorker B, followed by an immediate install of A to switch back.
   t.true(spy.getCall(0).calledWithExactly(serviceWorkerBPath, registerOptions));
   t.true(spy.getCall(1).calledWithExactly(serviceWorkerAPath, registerOptions));
   t.is(await manager.getActiveState(), ServiceWorkerActiveState.WorkerA);
 
+  // 5. Ensure a 2nd upgrade is handled corretly in the same way
+  envVerStub.returns(3);
   await manager.installWorker();
   t.true(spy.getCall(2).calledWithExactly(serviceWorkerBPath, registerOptions));
   t.true(spy.getCall(3).calledWithExactly(serviceWorkerAPath, registerOptions));
   t.is(await manager.getActiveState(), ServiceWorkerActiveState.WorkerA);
 
+  // 6. Ensure we did not call more than 4 times
   t.is(spy.callCount, 4);
 });
 

src/helpers/InitHelper.ts
@@ -43,7 +43,7 @@ export default class InitHelper {
     Log.debug('Called %cinternalInit()', getConsoleStyle('code'));
 
     // Always check for an updated service worker
-    await OneSignal.context.serviceWorkerManager.updateWorker();
+    await OneSignal.context.serviceWorkerManager.installWorker();
 
     OneSignal.context.sessionManager.incrementPageViewCount();
 

src/managers/SubscriptionManager.ts
@@ -355,23 +355,21 @@ export class SubscriptionManager {
     }
 
     /* Now that permissions have been granted, install the service worker */
-    if (await this.context.serviceWorkerManager.shouldInstallWorker()) {
-        try {
-          await this.context.serviceWorkerManager.installWorker();
-        } catch(err) {
-          if (err instanceof ServiceWorkerRegistrationError) {
-            if (err.status === 403) {
-              await this.context.subscriptionManager.registerFailedSubscription(
-                SubscriptionStateKind.ServiceWorkerStatus403, 
-                this.context);
-            } else if (err.status === 404) {
-              await this.context.subscriptionManager.registerFailedSubscription(
-                SubscriptionStateKind.ServiceWorkerStatus404,
-                this.context);
-            } 
-          } 
-          throw err;
-        }
+    try {
+      await this.context.serviceWorkerManager.installWorker();
+    } catch(err) {
+      if (err instanceof ServiceWorkerRegistrationError) {
+        if (err.status === 403) {
+          await this.context.subscriptionManager.registerFailedSubscription(
+            SubscriptionStateKind.ServiceWorkerStatus403, 
+            this.context);
+        } else if (err.status === 404) {
+          await this.context.subscriptionManager.registerFailedSubscription(
+            SubscriptionStateKind.ServiceWorkerStatus404,
+            this.context);
+        } 
+      } 
+      throw err;
     }
       
 

src/utils/Utils.ts
@@ -4,6 +4,10 @@ interface IndexOfAble {
   indexOf(match:string): number;
 }
 
+export type RecursivePartial<T> = {
+  [P in keyof T]?: RecursivePartial<T[P]>;
+};
+
 export class Utils {
   /**
    * Returns true if match is in string; otherwise, returns false.

test/support/sdk/TestEnvironment.ts
@@ -24,6 +24,7 @@ import { MockPushManager } from "../mocks/service-workers/models/MockPushManager
 import { MockServiceWorkerContainer } from "../mocks/service-workers/models/MockServiceWorkerContainer";
 import { addServiceWorkerGlobalScopeToGlobal } from "../polyfills/polyfills";
 import deepmerge = require("deepmerge");
+import { RecursivePartial } from '../../../src/utils/Utils';
 
 // NodeJS.Global
 declare var global: any;

typings/globals/missing.d.ts
@@ -97,7 +97,3 @@ declare var __IS_ES6__: string;
 declare var __SRC_STYLESHEETS_MD5_HASH__: string;
 
 declare var __LOGGING__: boolean;
-
-type RecursivePartial<T> = {
-  [P in keyof T]?: RecursivePartial<T[P]>;
-};

commit 3f23f9ae30fad3c6640391e3f7c4a324e3125c1e

Only registers OS SW if notif permission exists* This prevents unnecessary install of the OneSignal ServiceWorker on first page load   - Now only does so after accepting notification permission of if it was previously accepted.* Regression bug most likely introduced in version 150711 by PR #571* Removed dead code function `ServiceWorkerManager.subscribeForPushNotifications`.

src/managers/ServiceWorkerManager.ts
@@ -193,7 +193,7 @@ export class ServiceWorkerManager {
     });
   }
 
-  async shouldInstallWorker(): Promise<boolean> {
+  private async shouldInstallWorker(): Promise<boolean> {
     if (!Environment.supportsServiceWorkers())
       return false;
 
@@ -205,26 +205,17 @@ export class ServiceWorkerManager {
       return false;
 
     const workerState = await this.getActiveState();
+    // If there isn't a SW or it isn't OneSignal's only install our SW if notification permissions are enabled
+    // This prevents an unnessary install which saves bandwidth
+    if (workerState === ServiceWorkerActiveState.None || workerState === ServiceWorkerActiveState.ThirdParty) {
+      const permission = await OneSignal.context.permissionManager.getNotificationPermission(
+        OneSignal.config!.safariWebId
+      );
 
-    if (workerState !== ServiceWorkerActiveState.WorkerA && workerState !== ServiceWorkerActiveState.WorkerB) {
-      return true;
+      return permission === "granted";
     }
 
     return this.workerNeedsUpdate();
-  }
-
-  async subscribeForPushNotifications(): Promise<Subscription> {
-    const workerState = await this.getActiveState();
-
-    if (workerState !== ServiceWorkerActiveState.WorkerA && workerState !== ServiceWorkerActiveState.WorkerB) {
-      throw new InvalidStateError(InvalidStateReason.ServiceWorkerNotActivated);
-    }
-    return new Promise<Subscription>(resolve => {
-      this.context.workerMessenger.once(WorkerMessengerCommand.Subscribe, subscription => {
-        resolve(Subscription.deserialize(subscription));
-      });
-      this.context.workerMessenger.unicast(WorkerMessengerCommand.Subscribe, this.context.appConfig);
-    });
   }
 
   /**
@@ -232,32 +223,22 @@ export class ServiceWorkerManager {
    * with a content-identical but differently named alternate service worker
    * file.
    */
-  async workerNeedsUpdate(): Promise<boolean> {
-    const workerState = await this.getActiveState();
-    Log.info(`[Service Worker Update] Checking service worker version...`);
+  private async workerNeedsUpdate(): Promise<boolean> {
+    Log.info("[Service Worker Update] Checking service worker version...");
     let workerVersion: number;
     try {
       workerVersion = await Utils.timeoutPromise(this.getWorkerVersion(), 2_000);
     } catch (e) {
-      Log.info(`[Service Worker Update] Worker did not reply to version query; assuming older version.`);
-      workerVersion = 1;
-    }
-
-    if (workerState !== ServiceWorkerActiveState.WorkerA && workerState !== ServiceWorkerActiveState.WorkerB) {
-      // Do not update 3rd party workers
-      Log.debug(
-        `[Service Worker Update] Not updating service worker, current active worker state is ${workerState}.`
-      );
-      return false;
+      Log.info("[Service Worker Update] Worker did not reply to version query; assuming older version and updating.");
+      return true;
     }
 
     if (workerVersion !== Environment.version()) {
-      Log.info(`[Service Worker Update] Updating service worker from v${workerVersion} --> v${Environment.version()}.`);
+      Log.info(`[Service Worker Update] Updating service worker from ${workerVersion} --> ${Environment.version()}.`);
       return true;
-    } else {
-      Log.info(`[Service Worker Update] Service worker version is current at v${workerVersion} (no update required).`);
     }
 
+    Log.info(`[Service Worker Update] Service worker version is current at ${workerVersion} (no update required).`);
     return false;
   }
 

test/unit/managers/ServiceWorkerManager.ts
@@ -358,6 +358,7 @@ test('installWorker() installs worker A with the correct file name and query par
   await TestEnvironment.initialize({
     httpOrHttps: HttpHttpsEnvironment.Https
   });
+  sandbox.stub(Notification, <any>"permission").value("granted");
 
   const manager = LocalHelpers.getServiceWorkerManager();
 
@@ -372,10 +373,24 @@ test('installWorker() installs worker A with the correct file name and query par
   }
 });
 
+test('installWorker() does NOT install ServiceWorker when permission has NOT been granted', async t => {
+  await TestEnvironment.initialize({
+    httpOrHttps: HttpHttpsEnvironment.Https
+  });
+
+  const manager = LocalHelpers.getServiceWorkerManager();
+
+  t.is(await manager.getActiveState(), ServiceWorkerActiveState.None);
+  await manager.installWorker();
+  t.is(await manager.getActiveState(), ServiceWorkerActiveState.None);
+  t.is(navigator.serviceWorker.controller, null);
+});
+
 test('installWorker() installs worker A when a third party service worker exists', async t => {
   await TestEnvironment.initialize({
     httpOrHttps: HttpHttpsEnvironment.Https
   });
+  sandbox.stub(Notification, <any>"permission").value("granted");
 
   await navigator.serviceWorker.register('/another-service-worker.js');
 
@@ -390,6 +405,7 @@ test('installWorker() installs Worker B and then A when Worker A is out of date'
   await TestEnvironment.initialize({
     httpOrHttps: HttpHttpsEnvironment.Https
   });
+  sandbox.stub(Notification, <any>"permission").value("granted");
 
   const manager = new ServiceWorkerManager(OneSignal.context, {
     workerAPath: new Path('/Worker-A.js'),
@@ -433,6 +449,7 @@ test('Server worker register URL correct when service worker path is a absolute 
   await TestEnvironment.initialize({
     httpOrHttps: HttpHttpsEnvironment.Https
   });
+  sandbox.stub(Notification, <any>"permission").value("granted");
 
   const manager = new ServiceWorkerManager(OneSignal.context, {
     workerAPath: new Path(`${location.origin}/Worker-A.js`),
@@ -454,6 +471,7 @@ test("Service worker failed to install due to 404 on host page. Send notificatio
   await TestEnvironment.initialize({
     httpOrHttps: HttpHttpsEnvironment.Https
   });
+  sandbox.stub(Notification, <any>"permission").value("granted");
 
   const context = OneSignal.context;
 
@@ -490,6 +508,7 @@ test("Service worker failed to install in popup. No handling.", async t => {
   await TestEnvironment.initialize({
     httpOrHttps: HttpHttpsEnvironment.Https
   });
+  sandbox.stub(Notification, <any>"permission").value("granted");
 
   const context = OneSignal.context;
 