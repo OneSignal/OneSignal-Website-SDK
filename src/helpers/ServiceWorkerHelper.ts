0024325222c832ed3b5756021b83d1204c441439
Iryna Trush <iryna@onesignal.com>
2020-02-20 23:25:35 -0800

    Minor refactor of ServiceWorkerHelper Started splitting it for page and service worker Will later move all interfaces into a separate file

--- src/helpers/ServiceWorkerHelper.ts
+++ src/helpers/ServiceWorkerHelper.ts
@@ -6,18 +6,11 @@ import { InvalidStateError, InvalidStateReason } from "../errors/InvalidStateErr
 import { OneSignalUtils } from "../utils/OneSignalUtils";
 import Database from "../services/Database";
 import { SerializedPushDeviceRecord } from "../models/PushDeviceRecord";
+import PageServiceWorkerHelper from "./page/ServiceWorkerHelper";
 
 export default class ServiceWorkerHelper {
-  // Gets details on the service-worker (if any) that controls the current page
   public static async getRegistration(): Promise<ServiceWorkerRegistration | null | undefined> {
-    try {
-      // location.href is used for <base> tag compatibility when it is set to a different origin
-      return await navigator.serviceWorker.getRegistration(location.href);
-    } catch (e) {
-      // This could be null in an HTTP context or error if the user doesn't accept cookies
-      Log.warn("[Service Worker Status] Error Checking service worker registration", location.href, e);
-      return null;
-    }
+    return await PageServiceWorkerHelper.getRegistration();
   }
 
   public static getServiceWorkerHref(
