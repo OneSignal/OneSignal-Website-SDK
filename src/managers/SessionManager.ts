79ed3164373fac24f9401051f220c7a86e7ec0d5
Iryna Trush <iryna@onesignal.com>
2020-03-26 22:30:21 -0700

    Send old-school on_session from page if SW not supported and not using subscription workaround and first page view

--- src/managers/SessionManager.ts
+++ src/managers/SessionManager.ts
@@ -1,15 +1,19 @@
-import { ContextSWInterface } from "../models/ContextSW";
+import { ContextInterface } from "../models/Context";
 import { PushDeviceRecord } from "../models/PushDeviceRecord";
 import { UpsertSessionPayload, DeactivateSessionPayload, SessionOrigin } from "../models/Session";
 import MainHelper from "../helpers/MainHelper";
 import Log from "../libraries/Log";
 import { WorkerMessengerCommand } from "../libraries/WorkerMessenger";
 import { OneSignalUtils } from "../utils/OneSignalUtils";
+import { SubscriptionStateKind } from "../models/SubscriptionStateKind";
+import OneSignalApiShared from "../OneSignalApiShared";
+import Database from "../services/Database";
 
 export class SessionManager {
-  private context: ContextSWInterface;
+  private context: ContextInterface;
+  private onSessionSent: boolean = false;
 
-  constructor(context: ContextSWInterface) {
+  constructor(context: ContextInterface) {
     this.context = context;
   }
 
@@ -30,12 +34,18 @@ export class SessionManager {
       isSafari: OneSignalUtils.isSafari(),
       outcomesConfig: this.context.appConfig.userConfig.outcomes!,
     };
-    if (isHttps) {
+    if (
+      this.context.environmentInfo.isBrowserAndSupportsServiceWorkers &&
+        !this.context.environmentInfo.isUsingSubscriptionWorkaround
+    ) {
       Log.debug("Notify SW to upsert session");
       await this.context.workerMessenger.unicast(WorkerMessengerCommand.SessionUpsert, payload);
-    } else {
+    } else if (this.context.environmentInfo.isUsingSubscriptionWorkaround) {
       Log.debug("Notify iframe to notify SW to upsert session");
       await OneSignal.proxyFrameHost.runCommand(OneSignal.POSTMAM_COMMANDS.SESSION_UPSERT, payload);
+    } else { // http w/o our iframe
+      // we probably shouldn't even be here
+      Log.debug("Notify upsert: do nothing");
     }
   }
 
@@ -55,12 +65,18 @@ export class SessionManager {
       isSafari: OneSignalUtils.isSafari(),
       outcomesConfig: this.context.appConfig.userConfig.outcomes!,
     };
-    if (isHttps) {
+    if (
+      this.context.environmentInfo.isBrowserAndSupportsServiceWorkers &&
+        !this.context.environmentInfo.isUsingSubscriptionWorkaround
+    ) {
       Log.debug("Notify SW to deactivate session");
       await this.context.workerMessenger.unicast(WorkerMessengerCommand.SessionDeactivate, payload);
-    } else {
+    } else if (this.context.environmentInfo.isUsingSubscriptionWorkaround) {
       Log.debug("Notify SW to deactivate session");
       await OneSignal.proxyFrameHost.runCommand(OneSignal.POSTMAM_COMMANDS.SESSION_DEACTIVATE, payload);
+    } else { // http w/o our iframe
+      // we probably shouldn't even be here
+      Log.debug("Notify deactivate: do nothing");
     }
   }
 
@@ -174,7 +190,11 @@ export class SessionManager {
 
     if (OneSignalUtils.isHttps()) {
       this.setupSessionEventListeners();
-    } else {
+    } else if (
+      !this.context.environmentInfo.isBrowserAndSupportsServiceWorkers &&
+      !this.context.environmentInfo.isUsingSubscriptionWorkaround
+    ) {
+      this.onSessionSent = sessionOrigin === SessionOrigin.PlayerCreate;
       OneSignal.emitter.emit(OneSignal.EVENTS.SESSION_STARTED);
     }
 
@@ -231,5 +251,46 @@ export class SessionManager {
     }
 
     OneSignal.context.sessionManager.setupSessionEventListeners();
+  }
+
+  // If user has been subscribed before, send the on_session update to our backend on the first page view.
+  async sendOnSessionUpdateFromPage(deviceRecord?: PushDeviceRecord): Promise<void> {
+    if (this.onSessionSent) {
+      return;
+    }
+
+    if (!this.context.pageViewManager.isFirstPageView()) {
+      return;
+    }
+
+    const existingUser = await this.context.subscriptionManager.isAlreadyRegisteredWithOneSignal();
+    if (!existingUser) {
+      Log.debug("Not sending the on session because user is not registered with OneSignal (no device id)");
+      return;
+    }
+
+    const deviceId = await MainHelper.getDeviceId();
+    if (!deviceRecord) {
+      deviceRecord = await MainHelper.createDeviceRecord(this.context.appConfig.appId);
+    }
+
+    if (deviceRecord.subscriptionState !== SubscriptionStateKind.Subscribed &&
+      OneSignal.config.enableOnSession !== true) {
+      return;
+    }
+
+    try {
+      const newPlayerId = await OneSignalApiShared.updateUserSession(deviceId!, deviceRecord);
+      this.onSessionSent = true;
+
+      // If the returned player id is different, save the new id.
+      if (newPlayerId !== deviceId) {
+        const subscription = await Database.getSubscription();
+        subscription.deviceId = newPlayerId;
+        await Database.setSubscription(subscription);
+      }
+    } catch(e) {
+      Log.error(`Failed to update user session. Error "${e.message}" ${e.stack}`);
+    }
   }
 }
