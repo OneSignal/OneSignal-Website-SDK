2311d3fe186080d710b7152e96bd174c131a025b
Iryna Trush <iryna@onesignal.com>
2020-03-26 22:27:29 -0700

    Changing initialization order to expose environmentInfo in context

--- src/models/Context.ts
+++ src/models/Context.ts
@@ -12,15 +12,18 @@ import ContextHelper from "../helpers/ContextHelper";
 import { UpdateManager } from "../managers/UpdateManager";
 import { PromptsManager } from "../managers/PromptsManager";
 import { SessionManager } from "../managers/SessionManager";
+import { EnvironmentInfo } from "../context/browser/models/EnvironmentInfo";
 
 export interface ContextInterface extends ContextSWInterface {
   dynamicResourceLoader: DynamicResourceLoader;
   cookieSyncer: CookieSyncer;
   metricsManager: MetricsManager;
+  environmentInfo: EnvironmentInfo;
 }
 
 export default class Context implements ContextInterface {
   public appConfig: AppConfig;
+  public environmentInfo: EnvironmentInfo;
   public dynamicResourceLoader: DynamicResourceLoader;
   public subscriptionManager: SubscriptionManager;
   public serviceWorkerManager: ServiceWorkerManager;
@@ -35,6 +38,7 @@ export default class Context implements ContextInterface {
 
   constructor(appConfig: AppConfig) {
     this.appConfig = appConfig;
+    this.environmentInfo = OneSignal.environmentInfo;
     this.subscriptionManager = ContextHelper.getSubscriptionManager(this);
     this.serviceWorkerManager = ContextHelper.getServiceWorkerManager(this);
     this.pageViewManager = new PageViewManager();
