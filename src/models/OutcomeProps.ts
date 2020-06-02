d1239f70e390eb1e0ac4ca33ab38f464db459b97
Rodrigo <rogomezpalacio@protonmail.com>
2020-06-02 14:00:18 -0500

    More arguments to OutcomesHelper constr for cleaner code and reuse

--- src/models/OutcomeProps.ts
+++ src/models/OutcomeProps.ts
@@ -3,6 +3,5 @@ import { OutcomeAttributionType } from './Outcomes';
 export default interface OutcomeProps {
   type: OutcomeAttributionType;
   notificationIds: string[];
-  isUnique: boolean;
   weight?: number|undefined;
 }
