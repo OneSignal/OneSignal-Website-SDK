f1c07d8ae0b6dd2fc571e7b2aec520630e6bfe0d
Rodrigo <rogomezpalacio@protonmail.com>
2020-07-07 16:37:04 -0500

    Remove tagHelperWithRetries unit test

--- test/unit/utils/TagUtils.ts
+++ test/unit/utils/TagUtils.ts
@@ -26,11 +26,3 @@ test('check conversion from tag boolean values to numbers', t => {
     const converted = TagUtils.convertTagBooleanValuesToNumbers({ tag1: false, tag2: true });
     t.true(_.isEqual(converted, { tag1: 0, tag2: 1 }));
 });
-
-test('check tagHelperWithRetries makes correct number of tries', async t => {
-    const getTagsSpy = sinonSandbox.spy(OneSignal, "getTags");
-    sinonSandbox.stub(OneSignalApiBase, "get").rejects();
-    sinonSandbox.stub(Database, "getSubscription");
-    await TagUtils.tagHelperWithRetries(OneSignal.getTags, 100, 3);
-    t.is(getTagsSpy.callCount, 4);
-});
