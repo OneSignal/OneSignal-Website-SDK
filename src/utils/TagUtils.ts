46840ca65da4a155e003a956961808fe0e298e05
Rodrigo <rogomezpalacio@protonmail.com>
2020-07-06 23:28:47 -0500

    Remove Tag Helper with Retries

--- src/utils/TagUtils.ts
+++ src/utils/TagUtils.ts
@@ -3,22 +3,6 @@ import { TagsObject } from '../models/Tags';
 import _ from "lodash";
 
 export default class TagUtils {
-    static async tagHelperWithRetries(callback:Function, ms: number, maxTries: number): Promise<Object> {
-        return  new Promise(async resolve => {
-            callback().then(resolve)
-            .catch( () => {
-                if(!maxTries) {
-                    resolve();
-                } else {
-                    setTimeout(() => {
-                        Log.debug("Retrying tag request");
-                        this.tagHelperWithRetries(callback, ms, maxTries-1).then(resolve);
-                    }, ms);
-                }
-            });
-        });
-    }
-
     static convertTagBooleanValuesToNumbers(tags: TagsObject): TagsObject {
         tags = JSON.parse(JSON.stringify(tags));
         Object.keys(tags).forEach(key => {
