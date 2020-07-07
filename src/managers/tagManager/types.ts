577d3cfbd9efc759ee608103ce8eadcec8b601d7
Rodrigo <rogomezpalacio@protonmail.com>
2020-07-07 01:32:02 -0500

    Refactoring. Pass in isInUpdateMode to sendTags
    Avoid sending tags if no Category is chosen

--- src/managers/tagManager/types.ts
+++ src/managers/tagManager/types.ts
@@ -1,7 +1,7 @@
 import { TagsObject } from '../../models/Tags';
 
 export interface ITagManager {
-    sendTags: () => Promise<TagsObject|null>;
+    sendTags: (isInUpdateMode: boolean) => Promise<TagsObject|null>;
     storeTagValuesToUpdate: (tags: TagsObject) => void;
     playerTags: TagsObject;
 }
