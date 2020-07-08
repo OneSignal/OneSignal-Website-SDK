5de0e313fb665ef6757d3ea1fcd43e830f675017
Rodrigo <rogomezpalacio@protonmail.com>
2020-07-08 13:35:22 -0500

    Unit tests for string->number conversion helper and object difference helper

--- test/unit/utils/TagUtils.ts
+++ test/unit/utils/TagUtils.ts
@@ -5,6 +5,7 @@ import sinon, { SinonSandbox } from "sinon";
 import { TestEnvironment } from '../../support/sdk/TestEnvironment';
 import OneSignalApiBase from '../../../src/OneSignalApiBase';
 import Database from '../../../src/services/Database';
+import { TagsObject } from 'src/models/Tags';
 
 const sinonSandbox: SinonSandbox = sinon.sandbox.create();
 
@@ -25,4 +26,27 @@ test('check conversion from tag number values to booleans', t => {
 test('check conversion from tag boolean values to numbers', t => {
     const converted = TagUtils.convertTagBooleanValuesToNumbers({ tag1: false, tag2: true });
     t.true(_.isEqual(converted, { tag1: 0, tag2: 1 }));
+});
+
+test('check conversion from tag string values to numbers', t => {
+    const converted = TagUtils.convertTagStringValuesToNumbers({ tag1: "1", tag2: "0" });
+    t.true(_.isEqual(converted, { tag1: 1, tag2: 0 }));
+});
+
+test('check correct object difference is returned', t => {
+    let localTags: TagsObject = { tag1: 1, tag2: 0 };
+    let playerTags: TagsObject = { tag1: 0 };
+    const diff1 = TagUtils.getObjectDifference(localTags, playerTags);
+
+    localTags = { tag1: 0, tag2: 1 };
+    playerTags = { tag1: 1, tag2: 1 };
+    const diff2 = TagUtils.getObjectDifference(localTags, playerTags);
+
+    localTags = { tag1: 1 };
+    playerTags = { tag2: 1 };
+    const diff3 = TagUtils.getObjectDifference(localTags, playerTags);
+
+    t.true(_.isEqual(diff1, ["tag1"]));
+    t.true(_.isEqual(diff2, ["tag1"]));
+    t.true(_.isEqual(diff3, ["tag1"]));
 });
