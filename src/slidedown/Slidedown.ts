7c8087158dd78265a471758631560f965a1c4e12
Iryna Trush <iryna@onesignal.com>
2020-07-09 23:44:54 -0700

    Extend Slidedown
    Added loading and error states support.
    Initializing tagging container options.
    Preparing to support re-showing via isInUpdateMode flag.
    And some minor refactoring.

--- src/slidedown/Slidedown.ts
+++ src/slidedown/Slidedown.ts
@@ -2,17 +2,32 @@ import bowser from 'bowser';
 
 import Event from '../Event';
 import MainHelper from '../helpers/MainHelper';
-import { addCssClass, addDomElement, getPlatformNotificationIcon, once, removeDomElement } from '../utils';
+import {
+  addCssClass,
+  addDomElement,
+  getPlatformNotificationIcon,
+  once,
+  removeDomElement,
+  removeCssClass,
+  getDomElementOrStub
+} from '../utils';
 import { SlidedownPermissionMessageOptions } from '../models/Prompts';
 import { SERVER_CONFIG_DEFAULTS_SLIDEDOWN } from '../config';
 import { getLoadingIndicatorWithColor } from './LoadingIndicator';
-import { getSlidedownHtml } from './SlidedownHtml';
+import { getDialogHtml } from './DialogHtml';
 import { getRetryIndicator } from './RetryIndicator';
 import { SlidedownCssClasses, SlidedownCssIds, COLORS } from "./constants";
+import { Categories } from 'src/models/Tags';
+import Log from 'src/libraries/Log';
+import TagUtils from 'src/utils/TagUtils';
 
 export default class Slidedown {
   public options: SlidedownPermissionMessageOptions;
   public notificationIcons: NotificationIcons | null;
+  public isShowingFailureState: boolean;
+
+  private isInSaveState: boolean;
+  private categoryOptions: Categories|undefined;
 
   static get EVENTS() {
     return {
@@ -28,23 +43,42 @@ export default class Slidedown {
         options = MainHelper.getSlidedownPermissionMessageOptions(OneSignal.config.userConfig.promptOptions);
     }
     this.options = options;
+    this.categoryOptions = options.categories;
     this.options.actionMessage = options.actionMessage.substring(0, 90);
     this.options.acceptButtonText = options.acceptButtonText.substring(0, 15);
     this.options.cancelButtonText = options.cancelButtonText.substring(0, 15);
-
     this.notificationIcons = null;
+    this.isInSaveState = false;
+    this.isShowingFailureState = false;
+
+    if (!!this.categoryOptions) {
+      this.categoryOptions.positiveUpdateButton = this.categoryOptions.positiveUpdateButton.substring(0, 16),
+      this.categoryOptions.negativeUpdateButton = this.categoryOptions.negativeUpdateButton.substring(0, 16),
+      this.categoryOptions.updateMessage = this.categoryOptions.updateMessage.substring(0, 90),
+
+        // NOTE: will be configurable in the future
+      this.categoryOptions.savingButtonText = SERVER_CONFIG_DEFAULTS_SLIDEDOWN.savingText;
+      this.categoryOptions.errorButtonText = this.categoryOptions.positiveUpdateButton;
+    }
   }
 
-  async create() {
+  async create(isInUpdateMode?: boolean) {
+    // TODO: dynamically change btns depending on if its first or repeat display of slidedown (subscribe vs update)
     if (this.notificationIcons === null) {
       const icons = await MainHelper.getNotificationIcons();
 
       this.notificationIcons = icons;
 
       // Remove any existing container
-      if (this.container) {
-        removeDomElement('#onesignal-popover-container');
+      if (this.container.className.includes(SlidedownCssClasses.container)) {
+          removeDomElement(`#${SlidedownCssIds.container}`);
       }
+      const positiveButtonText = isInUpdateMode && !!this.categoryOptions ?
+        this.categoryOptions.positiveUpdateButton : this.options.acceptButtonText;
+      const negativeButtonText = isInUpdateMode && !!this.categoryOptions ?
+        this.categoryOptions.negativeUpdateButton : this.options.cancelButtonText;
+      const messageText = isInUpdateMode && !!this.categoryOptions ?
+        this.categoryOptions.updateMessage : this.options.actionMessage;
 
       const icon = this.getPlatformNotificationIcon();
       const slidedownHtml = getSlidedownHtml({
@@ -55,14 +89,15 @@ export default class Slidedown {
       });
 
       // Insert the container
-      addDomElement('body', 'beforeend',
-          '<div id="onesignal-popover-container" class="onesignal-popover-container onesignal-reset"></div>');
+      addDomElement('body', 'beforeend', `<div id="${SlidedownCssIds.container}"` +
+        `class="${SlidedownCssClasses.container} ${SlidedownCssClasses.reset}"></div>`);
       // Insert the dialog
       addDomElement(this.container, 'beforeend',
-          `<div id="${SlidedownCssIds.dialog}" class="${SlidedownCssClasses.dialog}">${slidedownHtml}</div>`);
+          `<div id="${SlidedownCssIds.dialog}" class="${SlidedownCssClasses.dialog}">${dialogHtml}</div>`);
 
       // Animate it in depending on environment
       addCssClass(this.container, bowser.mobile ? 'slide-up' : 'slide-down');
+
       // Add click event handlers
       this.allowButton.addEventListener('click', this.onSlidedownAllowed.bind(this));
       this.cancelButton.addEventListener('click', this.onSlidedownCanceled.bind(this));
@@ -85,31 +120,90 @@ export default class Slidedown {
       if (event.target === this.dialog &&
           (event.animationName === 'slideDownExit' || event.animationName === 'slideUpExit')) {
           // Uninstall the event listener for animationend
-          removeDomElement('#onesignal-popover-container');
+          removeDomElement(`#${SlidedownCssIds.container}`);
           destroyListenerFn();
           Event.trigger(Slidedown.EVENTS.CLOSED);
       }
     }, true);
   }
 
+  /**
+   * only used with Category Slidedown
+   */
+  toggleSaveState() {
+    if (!this.categoryOptions) {
+      Log.debug("Slidedown private category options are not defined");
+      return;
+    }
+
+    if (!this.isInSaveState) {
+      // note: savingButtonText is hardcoded in constructor. TODO: pull from config & set defaults for future release
+      this.allowButton.innerHTML = this.getIndicatorHolderHtmlWithText(this.categoryOptions!.savingButtonText!);
+      addDomElement(this.buttonIndicatorHolder, 'beforeend',
+        getLoadingIndicatorWithColor(COLORS.whiteLoadingIndicator));
+      (<HTMLButtonElement>this.allowButton).disabled = true;
+      addCssClass(this.allowButton, 'disabled');
+      addCssClass(this.allowButton, SlidedownCssClasses.savingStateButton);
+    } else {
+      // positiveUpdateButton should be defined as written in MainHelper.getSlidedownPermissionMessageOptions
+      this.allowButton.innerHTML = this.categoryOptions!.positiveUpdateButton!;
+      removeDomElement(`#${SlidedownCssClasses.buttonIndicatorHolder}`);
+      (<HTMLButtonElement>this.allowButton).disabled = false;
+      removeCssClass(this.allowButton, 'disabled');
+      removeCssClass(this.allowButton, SlidedownCssClasses.savingStateButton);
+    }
+    this.isInSaveState = !this.isInSaveState;
+  }
+
+  toggleFailureState() {
+    if (!this.categoryOptions) {
+      Log.debug("Slidedown private category options are not defined");
+      return;
+    }
+
+    if (!this.isShowingFailureState) {
+      // note: errorButtonText is hardcoded in constructor. TODO: pull from config & set defaults for future release
+      this.allowButton.innerHTML = this.getIndicatorHolderHtmlWithText(this.categoryOptions!.errorButtonText);
+      addDomElement(this.buttonIndicatorHolder, 'beforeend', getRetryIndicator());
+      addCssClass(this.allowButton, 'onesignal-error-state-button');
+    } else {
+      removeDomElement('#onesignal-button-indicator-holder');
+      removeCssClass(this.allowButton, 'onesignal-error-state-button');
+    }
+    this.isShowingFailureState = !this.isShowingFailureState;
+  }
+
   getPlatformNotificationIcon(): string {
     return getPlatformNotificationIcon(this.notificationIcons);
   }
 
+  getIndicatorHolderHtmlWithText(text: string) {
+    return `${text}<div id="${SlidedownCssIds.buttonIndicatorHolder}"` +
+      `class="${SlidedownCssClasses.buttonIndicatorHolder}"></div>`;
+  }
+
   get container() {
-    return document.querySelector('#onesignal-popover-container');
+    return getDomElementOrStub(`#${SlidedownCssIds.container}`);
   }
 
   get dialog() {
-    return document.querySelector('#onesignal-popover-dialog');
+    return getDomElementOrStub(`#${SlidedownCssIds.dialog}`);
   }
 
   get allowButton() {
-    return document.querySelector('#onesignal-popover-allow-button');
+    return getDomElementOrStub(`#${SlidedownCssIds.allowButton}`);
   }
 
   get cancelButton() {
-    return document.querySelector('#onesignal-popover-cancel-button');
+    return getDomElementOrStub(`#${SlidedownCssIds.cancelButton}`);
+  }
+
+  get buttonIndicatorHolder() {
+    return getDomElementOrStub(`#${SlidedownCssIds.buttonIndicatorHolder}`);
+  }
+
+  get slidedownFooter() {
+    return getDomElementOrStub(`#${SlidedownCssIds.footer}`);
   }
 }
 
