955c49e9f151dc2a30da08dcd8cf2628de59ac9f
Iryna Trush <iryna@onesignal.com>
2020-07-09 22:24:36 -0700

    Rename Popover to Slidedown
    Mostly just changed word to word Popover to Slidedown.
    Changed the class name and subsequently all events and
    functions but only their internal representation.
    
    For css had to introduce double selectors to make sure
    the rename is made in a backwards compatible way.

---
+++ test/unit/prompts/Slidedown.ts
@@ -0,0 +1,182 @@
+import test from 'ava';
+import sinon, { SinonSandbox } from 'sinon';
+import Slidedown from '../../../src/slidedown/Slidedown';
+import { SlidedownPermissionMessageOptions } from '../../../src/models/Prompts';
+import "../../support/polyfills/polyfills";
+import { TestEnvironment, HttpHttpsEnvironment, BrowserUserAgent } from '../../support/sdk/TestEnvironment';
+import { setUserAgent } from '../../support/tester/browser';
+
+const sandbox: SinonSandbox = sinon.sandbox.create();
+
+const options: SlidedownPermissionMessageOptions = {
+  actionMessage : '',
+  acceptButtonText : '',
+  cancelButtonText : '',
+  enabled : true
+};
+
+test.beforeEach(async () => {
+  (global as any).BrowserUserAgent = BrowserUserAgent;
+  await TestEnvironment.stubDomEnvironment({
+    httpOrHttps: HttpHttpsEnvironment.Https
+  });
+});
+
+test.afterEach(function () {
+  sandbox.restore();
+});
+
+/**
+ * chrome tests
+ */
+test('slidedown: uses chrome by default on mac', async t => {
+  setUserAgent(BrowserUserAgent.ChromeMacSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { chrome: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+test('slidedown: uses chrome by default on tablet', async t => {
+  setUserAgent(BrowserUserAgent.ChromeTabletSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { chrome: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+test('slidedown: uses chrome by default on linux', async t => {
+  setUserAgent(BrowserUserAgent.ChromeLinuxSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { chrome: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+test('slidedown: uses chrome by default on windows', async t => {
+  setUserAgent(BrowserUserAgent.ChromeWindowsSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { chrome: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+test('slidedown: uses chrome by default on Android', async t => {
+  setUserAgent(BrowserUserAgent.ChromeAndroidSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { chrome: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+/**
+ * firefox tests
+ */
+test('slidedown: uses firefox by default on mobile', async t => {
+  setUserAgent(BrowserUserAgent.FirefoxMobileSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { firefox: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+test('slidedown: uses firefox by default on tablet', async t => {
+  setUserAgent(BrowserUserAgent.FirefoxTabletSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { firefox: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+test('slidedown: uses firefox by default on windows', async t => {
+  setUserAgent(BrowserUserAgent.FirefoxWindowsSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { firefox: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+test('slidedown: uses firefox by default on mac', async t => {
+  setUserAgent(BrowserUserAgent.FirefoxMacSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { firefox: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+test('slidedown: uses firefox by default on linux', async t => {
+  setUserAgent(BrowserUserAgent.FirefoxLinuxSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { firefox: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+/**
+ * edge test
+ */
+test('slidedown: uses edge by default', async t => {
+  setUserAgent(BrowserUserAgent.EdgeSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { chrome: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+/**
+ * samsung test
+ */
+test('slidedown: uses samsung browser by default, icon url defined', async t => {
+  setUserAgent(BrowserUserAgent.SamsungBrowserSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = { chrome: "http://url.com" };
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "http://url.com");
+});
+
+/**
+ * default catch tests
+ */
+test('slidedown: uses samsung browser by default, icon undefined', async t => {
+  setUserAgent(BrowserUserAgent.SamsungBrowserSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = null;
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "default-icon");
+});
+
+test('slidedown: uses samsung browser by default, icon empty', async t => {
+  setUserAgent(BrowserUserAgent.SamsungBrowserSupported);
+  const slidedown = new Slidedown(options);
+
+  slidedown.notificationIcons = {};
+  const icon = slidedown.getPlatformNotificationIcon();
+
+  t.is(icon, "default-icon");
+});
--- test/unit/public-sdk-apis/onSession.ts
+++ test/unit/public-sdk-apis/onSession.ts
@@ -11,7 +11,7 @@ import OneSignalApiBase from "../../../src/OneSignalApiBase";
 import {
   stubMessageChannel, mockIframeMessaging, mockWebPushAnalytics, InitTestHelper
 } from '../../support/tester/utils';
-import Popover from "../../../src/popover/Popover";
+import Slidedown from "../../../src/slidedown/Slidedown";
 import OneSignalEvent from "../../../src/Event";
 import { DynamicResourceLoader, ResourceLoadState } from "../../../src/services/DynamicResourceLoader";
 import { ServiceWorkerManager } from "../../../src/managers/ServiceWorkerManager";
@@ -23,10 +23,9 @@ import { SubscriptionManager } from "../../../src/managers/SubscriptionManager";
 import InitHelper from "../../../src/helpers/InitHelper";
 import {
   markUserAsOptedOut, markUserAsSubscribed, markUserAsSubscribedOnHttp,
-  stubServiceWorkerInstallation, 
+  stubServiceWorkerInstallation,
 } from "../../support/tester/sinonSandboxUtils";
 import { createSubscription } from "../../support/tester/utils";
-import { PromptsManager } from '../../../src/managers/PromptsManager';
 
 
 const sinonSandbox: SinonSandbox = sinon.sandbox.create();
@@ -841,14 +840,14 @@ async function beforeTest(
 }
 
 function simulateSlidedownAllowAfterShown() {
-  OneSignal.on(Popover.EVENTS.SHOWN, () => {
-    OneSignalEvent.trigger(Popover.EVENTS.ALLOW_CLICK);
+  OneSignal.on(Slidedown.EVENTS.SHOWN, () => {
+    OneSignalEvent.trigger(Slidedown.EVENTS.ALLOW_CLICK);
   });
 }
 
 function simulateSlidedownDismissAfterShown() {
-  OneSignal.on(Popover.EVENTS.SHOWN, () => {
-    OneSignalEvent.trigger(Popover.EVENTS.CANCEL_CLICK);
+  OneSignal.on(Slidedown.EVENTS.SHOWN, () => {
+    OneSignalEvent.trigger(Slidedown.EVENTS.CANCEL_CLICK);
   });
 }
 
