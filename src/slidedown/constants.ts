8099661aca4a6bfc1ca7bbdda66745c4d8ce4720
Iryna Trush <iryna@onesignal.com>
2020-07-09 23:18:25 -0700

    Creating constants with css classes and ids
    To be used in slidedown and tagging container

---
+++ src/slidedown/constants.ts
@@ -0,0 +1,53 @@
+export const SlidedownCssClasses = {
+  allowButton: "onesignal-slidedown-allow-button",
+  body: "slidedown-body",
+  buttonIndicatorHolder: "onesignal-button-indicator-holder",
+  cancelButton: "onesignal-slidedown-cancel-button",
+  container: "onesignal-slidedown-container",
+  dialog: "onesignal-slidedown-dialog",
+  footer: "slidedown-footer",
+  reset: "onesignal-reset",
+  savingStateButton: "onesignal-saving-state-button"
+};
+
+export const SlidedownCssIds = {
+  allowButton: "onesignal-slidedown-allow-button",
+  body: "slidedown-body",
+  buttonIndicatorHolder: "onesignal-button-indicator-holder",
+  cancelButton: "onesignal-slidedown-cancel-button",
+  container: "onesignal-slidedown-container",
+  dialog: "onesignal-slidedown-dialog",
+  footer: "slidedown-footer",
+};
+
+export const TaggingContainerCssClasses = {
+  categoryLabelInput: "onesignal-category-label-input",
+  categoryLabelText: "onesignal-category-label-text",
+  categoryLabel: "onesignal-category-label",
+  checkmark: "onesignal-checkmark",
+  taggingContainer: "tagging-container",
+  taggingContainerCol: "tagging-container-col",
+  loadingContainer: "onesignal-loading-container",
+  loadingMessage: "onesignal-loading-message"
+};
+
+export const TaggingContainerCssIds = {
+  loadingContainer: "onesignal-loading-container",
+};
+
+export const DEFAULT_ICON = `data:image/svg+xml,%3Csvg fill='none' xmlns='http://www.w3.org/2000/svg'` +
+  `viewBox='0 0 40 40'%3E%3Cg clip-path='url(%23clip0)'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd'` +
+  `d='M33.232 28.434a2.5 2.5 0 001.768.733 1.667 1.667 0 010 3.333H5a1.667 1.667 0 110-3.333 2.5 2.5 0 00` +
+  `2.5-2.5v-8.104A13.262 13.262 0 0118.333 5.122V1.667a1.666 1.666 0 113.334 0v3.455A13.262 13.262 0 0132` +
+  `.5 18.563v8.104a2.5 2.5 0 00.732 1.767zM16.273 35h7.454a.413.413 0 01.413.37 4.167 4.167 0 11-8.28 0 .` +
+  `417.417 0 01.413-.37z' fill='%23BDC4CB'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='clip0'%3E%3Cpath fill='%2` +
+  `3fff' d='M0 0h40v40H0z'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E`;
+
+export const COLORS = {
+  greyLoadingIndicator: "#95A1AC",
+  whiteLoadingIndicator: "#FFFFFF"
+};
+
+export const TaggingContainerStrings = {
+  fetchingPreferences : "Fetching your preferences"
+};
