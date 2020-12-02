f295af0af563b3f4a49f97b3f1d615e85f078f99
Rodrigo <rogomezpalacio@protonmail.com>
2020-12-01 19:24:33 -0600

    Fix other invocations of `OneSignalApi.updatePlayer`
    Motivation: since we update the functions to accept the UpdatePlayerOptions model, we should fix other invocations of this function. To do this, we update the `UpdatePlayerOptions` to expect the parameters used.

--- src/models/UpdatePlayerOptions.ts
+++ src/models/UpdatePlayerOptions.ts
@@ -1,8 +1,22 @@
+import { SerializedPushDeviceRecord } from './PushDeviceRecord';
+import { DeliveryPlatformKind } from './DeliveryPlatformKind';
 import { SubscriptionStateKind } from 'src/models/SubscriptionStateKind';
 
 export interface UpdatePlayerOptions {
+    web_auth                    ?: string;
+    web_p256                    ?: string;
+    language                    ?: string;
+    timezone                    ?: number;
+    device_os                   ?: number;
+    sdk                         ?: string;
+    device_model                ?: string;
+    app_id                      ?: string;
     tags                        ?: Object | null;
     external_user_id_auth_hash  ?: string | null;
     identifier_auth_hash        ?: string | null;
-    notification_types          ?: SubscriptionStateKind;
+    email                       ?: string | null;
+    parent_player_id            ?: string | null;
+    identifier                  ?: string | null;
+    device_type                 ?: DeliveryPlatformKind;
+    notification_types          ?: SubscriptionStateKind | undefined;
 }
