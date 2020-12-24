7379d842c8f86b1d0effc62a9db2827ee4b8fe13
Rodrigo <rogomezpalacio@protonmail.com>
2020-12-23 20:03:19 -0600

    Nits: spaces, semi-colons, and newlines
    Motivation: small fixes regarding missing or additional spaces, semi-colons, and newlines

--- src/errors/InvalidStateError.ts
+++ src/errors/InvalidStateError.ts
@@ -48,7 +48,8 @@ export class InvalidStateError extends OneSignalError {
     /**
      * Important! Required to make sure the correct error type is detected during instanceof checks.
      * Same applies to all derived classes.
-     * https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
+     * https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md
+     * #extending-built-ins-like-error-array-and-map-may-no-longer-work
      */
     Object.setPrototypeOf(this, InvalidStateError.prototype);
   }
