3a8965a3dea7d3330c7a6b35aee1b64aba7b69cf
Rodrigo <rogomezpalacio@protonmail.com>
2021-01-21 15:32:11 -0600

    ConfigHelper Implementation
    Motivation:
    - Config version converter
    - Update default config injection
    
    Config Version Converter:
    We want to convert config version 1 (old) to the new version 2 that includes changes to how the slidedown prompt model. The new model (server model and sdk model now much more similar). The slidedown prompts model is now an array of individual slidedown configurations.
    
    Config Default Injection:
    Rename `ServerAppConfigPrompt` to `ServerAppPromptConfig` for clarity. Update defaults injection to use new model.

--- src/helpers/PromptsHelper.ts
+++ src/helpers/PromptsHelper.ts
@@ -1,9 +1,31 @@
 import { ContextInterface } from '../../src/models/Context';
 
 export default class PromptsHelper {
-  static isCategorySlidedownConfigured(context: ContextInterface): boolean {
-    const { promptOptions } = context.appConfig.userConfig;
-    if (!promptOptions || !promptOptions.slidedown || !promptOptions.slidedown.categories) {
+  static isCategorySlidedownConfigured(prompts: SlidedownPromptOptions[]): boolean {
+    if (!prompts) return false;
+
+    const options = PromptsHelper.getSlidedownPromptOptionsWithType(prompts, DelayedPromptType.Category);
+    if (!!options) {
+      return (!!options.categories && options.categories.length > 0);
+    }
+    return false;
+  }
+  /**
+   * Is Category Slidedown Configured (version 1 config schema)
+   * @param  {SlidedownOptionsVersion1} options
+   * @returns boolean
+   */
+  static isCategorySlidedownConfiguredVersion1(options: SlidedownOptionsVersion1): boolean {
+    return (options?.categories?.tags?.length || 0) > 0;
+  }
+
+  static getSlidedownPromptOptionsWithType(prompts: SlidedownPromptOptions[] | undefined, type: DelayedPromptType):
+    SlidedownPromptOptions | undefined {
+      return prompts ? prompts.filter(options => options.type === type)[0] : undefined;
+    }
+
+  static isSlidedownAutoPromptConfigured(prompts: SlidedownPromptOptions[]) : boolean {
+    if (!prompts) {
       return false;
     }
 
