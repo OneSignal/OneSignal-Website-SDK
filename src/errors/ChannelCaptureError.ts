eed13fdb4c4107a6560950771cbe24c61107f81e
Rodrigo <rogomezpalacio@protonmail.com>
2021-03-24 14:29:52 -0500

    Spacing nits, use ampersand in string

--- src/errors/ChannelCaptureError.ts
+++ src/errors/ChannelCaptureError.ts
@@ -20,22 +20,22 @@ export class ChannelCaptureError extends OneSignalError {
         errorMessage = `Invalid sms`;
         break;
       case InvalidChannelInputField.InvalidEmailAndSms:
-        errorMessage = `Invalid email, sms`;
+        errorMessage = `Invalid email & sms`;
         break;
       default:
         break;
     }
 
-    super(errorMessage);
-    this.description = InvalidChannelInputField[reason];
-    this.reason = reason;
+  super(errorMessage);
+  this.description = InvalidChannelInputField[reason];
+  this.reason = reason;
 
-    /**
-     * Important! Required to make sure the correct error type is detected during instanceof checks.
-     * Same applies to all derived classes.
-     * https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md
-     * #extending-built-ins-like-error-array-and-map-may-no-longer-work
-     */
-    Object.setPrototypeOf(this, ChannelCaptureError.prototype);
+  /**
+   * Important! Required to make sure the correct error type is detected during instanceof checks.
+   * Same applies to all derived classes.
+   * https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md
+   * #extending-built-ins-like-error-array-and-map-may-no-longer-work
+   */
+  Object.setPrototypeOf(this, ChannelCaptureError.prototype);
   }
 }
