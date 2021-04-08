4b8230338f3697f3a180abcb063a9088a5b4c5cf
Rodrigo <rogomezpalacio@protonmail.com>
2021-04-08 17:49:59 -0500

    International Telephone Input Script Enums
    Includes related hashes

---
+++ src/slidedown/InternationalTelephoneInput.ts
@@ -0,0 +1,11 @@
+export enum ItiScriptURLs {
+    Stylesheet = "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/css/intlTelInput.min.css",
+    Main       = "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/js/intlTelInput.min.js",
+    Utils      = "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/js/utils.js"
+}
+
+export enum ItiScriptURLHashes {
+    Stylesheet  = "sha512-yye/u0ehQsrVrfSd6biT17t39Rg9kNc+vENcCXZuMz2a+LWFGvXUnYuWUW6pbfYj1jcBb/C39UZw2ciQvwDDvg==",
+    Main        = "sha512-OnkjbJ4TwPpgSmjXACCb5J4cJwi880VRe+vWpPDlr8M38/L3slN5uUAeOeWU2jN+4vN0gImCXFGdJmc0wO4Mig==",
+    Utils       = "sha512-bUcJxlqkiGA3cmoYPuZaLRsyc5ChG9APG4ajom2AXKSlBtOmx4kLV3c8uv/6uSz43FMjI4Q2QI21+D223rT76w=="
+}
