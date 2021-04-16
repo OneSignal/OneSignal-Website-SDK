89e149a90c9f21779c194a51352f433f440cde5e
Josh Kasten <josh@onesignal.com>
2021-04-16 07:22:20 +0000

    Clean up "setExternalUserId after email,..." test
    * Utilized NockOneSignalHelper to clean up test.
    * Also utilized setupFakePlayerId

--- test/unit/public-sdk-apis/setExternalUserIdWithSecondaryChannel.ts
+++ test/unit/public-sdk-apis/setExternalUserIdWithSecondaryChannel.ts
@@ -1,8 +1,8 @@
 import test from "ava";
-import nock from "nock";
 import Database from "../../../src/services/Database";
 import { TestEnvironment } from "../../support/sdk/TestEnvironment";
-import Random from "../../support/tester/Random";
+import { NockOneSignalHelper } from "../../support/tester/NockOneSignalHelper";
+import { setupFakePlayerId } from "../../support/tester/utils";
 
 test.beforeEach(async _t => {
   await TestEnvironment.initialize();
@@ -11,46 +11,24 @@ test.beforeEach(async _t => {
 });
 
 test("setExternalUserId after email, makes PUT call to update Email record", async t => {
-  const emailPlayerId = Random.getRandomUuid();
   // 1. Nock out email create
-  let lastRequestBody: any;
-  const nockScopePostPlayer =
-    nock('https://onesignal.com')
-    .post(`/api/v1/players`)
-    .reply(200, (_uri: string, requestBody: any) => {
-      lastRequestBody = requestBody;
-      return { success : true, id: emailPlayerId };
-    });
-
+  const emailPostNock = NockOneSignalHelper.nockPlayerPost();
   await OneSignal.setEmail("test@test.com");
-  t.true(nockScopePostPlayer.isDone());
+  const emailPlayerId = (await emailPostNock.result).response.body.id;
 
   // 2. Create a push player id in the DB
-  const pushPlayerId = Random.getRandomUuid();
-  const subscription = await Database.getSubscription();
-  subscription.deviceId = pushPlayerId;
-  await Database.setSubscription(subscription);
+  const pushPlayerId = await setupFakePlayerId();
 
   // 3. Nock out push player set external user id, ignore it
-  nock('https://onesignal.com')
-   .put(`/api/v1/players/${pushPlayerId}`)
-   .reply(200, (_uri: string, _requestBody: any) => {
-     return { success : true };
-  });
+  NockOneSignalHelper.nockPlayerPut(pushPlayerId);
 
   // 4. Call OneSignal.setExternalUserId and ensure email is updated
-  let lastEmailPutRequestBody: any;
-  nock('https://onesignal.com')
-   .put(`/api/v1/players/${emailPlayerId}`)
-   .reply(200, (_uri: string, requestBody: any) => {
-     lastEmailPutRequestBody = JSON.parse(requestBody);
-     return { success : true };
-   });
+  const externalUserIdUpdateOnEmail = NockOneSignalHelper.nockPlayerPut(emailPlayerId);
   const testExternalUserId = "myExtId";
   await OneSignal.setExternalUserId(testExternalUserId);
 
   t.deepEqual(
-    lastEmailPutRequestBody, {
+    (await externalUserIdUpdateOnEmail.result).request.body, {
       app_id: OneSignal.context.appConfig.appId,
       external_user_id: testExternalUserId
     }
