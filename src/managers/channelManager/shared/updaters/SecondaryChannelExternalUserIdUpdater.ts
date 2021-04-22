17dcd547b6d1a3a456d65db8c469ffd838bc79a8
Josh Kasten <josh@onesignal.com>
2021-04-22 01:44:07 +0000

    Added a comment why we await on a subscriptionId

--- src/managers/channelManager/shared/updaters/SecondaryChannelExternalUserIdUpdater.ts
+++ src/managers/channelManager/shared/updaters/SecondaryChannelExternalUserIdUpdater.ts
@@ -8,6 +8,10 @@ export class SecondaryChannelExternalUserIdUpdater {
   }
 
   async setExternalUserId(id: string, authHash?: string): Promise<void> {
+    // We wait until we have a subscriptionId. We do this instead of an early return
+    //   so we do not lose this value and the update can then be made as soon as possible.
+    // Note: This promise may never resolve if the channel identifier is never set.
+    //   - Example if OneSignal.setEmail(""..."") is never called
     const subscriptionId = await this.profileProvider.getSubscriptionId();
 
     const { appId } = await Database.getAppConfig();
