9d7a7919c243298386c4829c23333a011a193495
Josh Kasten <josh@onesignal.com>
2021-04-28 06:17:57 +0000

    Add SMS to SecondaryChannelManager
    * Added SMS to SecondaryChannelManager in a similar as email.
    * Added SetSMSOptions that was untilized by OneSignal.setSMSNumber
    in the last commit.

--- src/managers/channelManager/shared/SecondaryChannelManager.ts
+++ src/managers/channelManager/shared/SecondaryChannelManager.ts
@@ -7,14 +7,18 @@ import { SecondaryChannelExternalUserIdUpdater } from "./updaters/SecondaryChann
 import { SecondaryChannelFocusUpdater } from "./updaters/SecondaryChannelFocusUpdater";
 import { SecondaryChannelSessionUpdater } from "./updaters/SecondaryChannelSessionUpdater";
 import { SecondaryChannelProfileProviderEmail } from "./providers/SecondaryChannelProfileProviderEmail";
+import { SecondaryChannelProfileProviderSMS } from "./providers/SecondaryChannelProfileProviderSMS";
+import { SecondaryChannelSMS } from "./SecondaryChannelSMS";
 
 export class SecondaryChannelManager {
   public readonly synchronizer: SecondaryChannelSynchronizer;
   public readonly email: SecondaryChannel;
+  public readonly sms: SecondaryChannel;
 
   constructor() {
     this.synchronizer = new SecondaryChannelSynchronizer();
 
+    // Add Email
     const emailProfileProvider = new SecondaryChannelProfileProviderEmail();
     const emailChannel = new SecondaryChannelEmail(
       new SecondaryChannelIdentifierUpdater(emailProfileProvider),
@@ -25,5 +29,17 @@ export class SecondaryChannelManager {
     );
     this.email = emailChannel;
     this.synchronizer.registerChannel(emailChannel);
+
+    // Add SMS
+    const smsProfileProvider = new SecondaryChannelProfileProviderSMS();
+    const smsChannel = new SecondaryChannelSMS(
+      new SecondaryChannelIdentifierUpdater(smsProfileProvider),
+      new SecondaryChannelExternalUserIdUpdater(smsProfileProvider),
+      new SecondaryChannelTagsUpdater(smsProfileProvider),
+      new SecondaryChannelSessionUpdater(smsProfileProvider),
+      new SecondaryChannelFocusUpdater(smsProfileProvider),
+    );
+    this.sms = smsChannel;
+    this.synchronizer.registerChannel(smsChannel);
   }
 }
