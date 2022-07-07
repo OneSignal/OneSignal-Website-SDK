4bbb6de0d284d9ccadba907c4585f40b156daad4
Rodrigo <rogomezpalacio@protonmail.com>
2022-07-07 18:36:46 -0500

    Move page context-specific files into `page` directory

---
+++ src/page/bell/Launcher.ts
@@ -0,0 +1,118 @@
+import { addCssClass, contains, hasCssClass, nothing, once, removeCssClass } from '../../shared/utils/utils';
+import ActiveAnimatedElement from './ActiveAnimatedElement';
+import Bell from './Bell';
+import { InvalidStateError, InvalidStateReason } from '../../shared/errors/InvalidStateError';
+import { BellSize } from '../../shared/models/Prompts';
+import Log from '../../shared/libraries/Log';
+
+
+export default class Launcher extends ActiveAnimatedElement {
+
+  public bell: any;
+  public wasInactive: boolean;
+
+  constructor(bell: Bell) {
+    super('.onesignal-bell-launcher','onesignal-bell-launcher-active', undefined, undefined,
+          'onesignal-bell-launcher-inactive', 'hidden', 'active');
+
+    this.bell = bell;
+    this.wasInactive = false;
+  }
+
+  async resize(size: BellSize) {
+    if (!this.element) {
+      // Notify button doesn't exist
+      throw new InvalidStateError(InvalidStateReason.MissingDomElement);
+    }
+
+    // If the size is the same, do nothing and resolve an empty promise
+    if ((size === 'small' && hasCssClass(this.element, 'onesignal-bell-launcher-sm')) ||
+        (size === 'medium' && hasCssClass(this.element, 'onesignal-bell-launcher-md')) ||
+        (size === 'large' && hasCssClass(this.element, 'onesignal-bell-launcher-lg'))) {
+      return Promise.resolve(this);
+    }
+    removeCssClass(this.element, 'onesignal-bell-launcher-sm');
+    removeCssClass(this.element, 'onesignal-bell-launcher-md');
+    removeCssClass(this.element, 'onesignal-bell-launcher-lg');
+    if (size === 'small') {
+      addCssClass(this.element, 'onesignal-bell-launcher-sm');
+    }
+    else if (size === 'medium') {
+      addCssClass(this.element, 'onesignal-bell-launcher-md');
+    }
+    else if (size === 'large') {
+      addCssClass(this.element, 'onesignal-bell-launcher-lg');
+    }
+    else {
+      throw new Error('Invalid OneSignal bell size ' + size);
+    }
+    if (!this.shown) {
+      return this;
+    }
+    else {
+      return await new Promise(resolve => {
+        // Once the launcher has finished shrinking down
+        if (this.targetTransitionEvents.length == 0) {
+          return resolve(this);
+        } else {
+          var timerId = setTimeout(() => {
+            Log.debug(`Launcher did not completely resize (state: ${this.state}, activeState: ${this.activeState}).`);
+          }, this.transitionCheckTimeout);
+          once(this.element, 'transitionend', (event: Event, destroyListenerFn: Function) => {
+            if (event.target === this.element &&
+              contains(this.targetTransitionEvents, (event as any).propertyName)) {
+              clearTimeout(timerId);
+              // Uninstall the event listener for transitionend
+              destroyListenerFn();
+              return resolve(this);
+            }
+          }, true);
+        }
+      });
+    }
+  }
+
+  activateIfInactive() {
+    if (this.inactive) {
+      this.wasInactive = true;
+      return this.activate();
+    }
+    else return nothing();
+  }
+
+  inactivateIfWasInactive() {
+    if (this.wasInactive) {
+      this.wasInactive = false;
+      return this.inactivate();
+    }
+    else return nothing();
+  }
+
+  clearIfWasInactive() {
+    this.wasInactive = false;
+  }
+
+  inactivate() {
+    return this.bell.message.hide()
+      .then(() => {
+        if (this.bell.badge.content.length > 0) {
+          return this.bell.badge.hide()
+            .then(() => Promise.all([super.inactivate(), this.resize('small')]))
+            .then(() => this.bell.badge.show());
+        }
+        else {
+          return Promise.all([super.inactivate(), this.resize('small')]);
+        }
+      });
+  }
+
+  activate() {
+    if (this.bell.badge.content.length > 0) {
+      return this.bell.badge.hide()
+        .then(() => Promise.all([super.activate(), this.resize(this.bell.options.size)]));
+    }
+    else {
+      return Promise.all([super.activate(), this.resize(this.bell.options.size)]);
+    }
+  }
+}
---
+++ src/page/errors/ChannelCaptureError.ts
@@ -0,0 +1,41 @@
+import OneSignalError from "../../shared/errors/OneSignalError";
+
+export enum InvalidChannelInputField {
+  InvalidSms,
+  InvalidEmail,
+  InvalidEmailAndSms
+}
+
+export class ChannelCaptureError extends OneSignalError {
+  description: string;
+  reason: InvalidChannelInputField;
+
+  constructor(reason: InvalidChannelInputField) {
+    let errorMessage;
+    switch (reason) {
+      case InvalidChannelInputField.InvalidEmail:
+        errorMessage = `Invalid email`;
+        break;
+      case InvalidChannelInputField.InvalidSms:
+        errorMessage = `Invalid sms`;
+        break;
+      case InvalidChannelInputField.InvalidEmailAndSms:
+        errorMessage = `Invalid email & sms`;
+        break;
+      default:
+        break;
+    }
+
+    super(errorMessage);
+    this.description = InvalidChannelInputField[reason];
+    this.reason = reason;
+
+    /**
+     * Important! Required to make sure the correct error type is detected during instanceof checks.
+     * Same applies to all derived classes.
+     * https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md
+     * #extending-built-ins-like-error-array-and-map-may-no-longer-work
+     */
+    Object.setPrototypeOf(this, ChannelCaptureError.prototype);
+  }
+}
