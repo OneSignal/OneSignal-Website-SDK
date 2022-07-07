7d6c2e67d69ffcd084514e1084bd9fbad775a251
Rodrigo <rogomezpalacio@protonmail.com>
2022-07-07 15:38:28 -0500

    Moved shared files into `shared` directory
    Motivation: these files are shared by both the page context and sw context

---
+++ src/shared/models/AppState.ts
@@ -0,0 +1,20 @@
+import { Timestamp } from "./Timestamp";
+
+export interface ClickedNotifications {
+    [key: string]: [Notification, Timestamp];
+}
+
+class AppState {
+    defaultNotificationUrl: string | undefined;
+    defaultNotificationTitle: string | undefined;
+
+    /**
+     * Whether the user is currently completely subscribed, including not opted out. Database cached version of
+     * isPushNotificationsEnabled().
+     */
+    lastKnownPushEnabled: boolean | undefined;
+
+    clickedNotifications: ClickedNotifications | undefined;
+}
+
+export { AppState };
---
+++ src/shared/utils/BrowserUtils.ts
@@ -0,0 +1,21 @@
+import Environment from "../helpers/Environment";
+
+export class BrowserUtils {
+  private static decodeTextArea: HTMLTextAreaElement | null = null;
+  public static decodeHtmlEntities(text: string) {
+    if (Environment.isBrowser()) {
+      if (!BrowserUtils.decodeTextArea) {
+        BrowserUtils.decodeTextArea = document.createElement("textarea");
+      }
+    }
+    if (BrowserUtils.decodeTextArea) {
+      BrowserUtils.decodeTextArea.innerHTML = text;
+      return BrowserUtils.decodeTextArea.value;
+    } else {
+      // Not running in a browser environment, text cannot be decoded
+      return text;
+    }
+  }
+}
+
+export default BrowserUtils;
