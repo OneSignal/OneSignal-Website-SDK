1dfb1426e3d706a4ed01d1e6daf7e8a3528c318a
Rodrigo <rogomezpalacio@protonmail.com>
2022-07-21 14:08:57 -0500

    OperationRepo: set up delta queue & processes
    Motivation: first we subscribe via `setup()` the operation repo to the broadcasting of new deltas from the `modelRepo` model change observer and push those deltas to a new `deltaQueue` array we will process.
    
    We then process the deltas in order (based on timestamp) and aggregate all changes into an operation. Which we will process in the operations queue.

--- src/system/operationRepo/OperationRepo.ts
+++ src/system/operationRepo/OperationRepo.ts
@@ -1,11 +1,59 @@
-
+import { ConfigModel, Delta, IdentityModel, Model, Operation, PropertiesModel, SessionModel, SubscriptionsModel } from "../types";
 import OneSignal from "../../OneSignal";
 import Subscribable from "../Subscribable";
 import { EnhancedSet } from "../EnhancedSet";
 
+const DELTAS_BATCH_PROCESSING_TIME = 1;
+
+type DeltaAggregator = {
+  [Model.Config]: ConfigModel,
+  [Model.Identity]: IdentityModel,
+  [Model.Properties]: PropertiesModel,
+  [Model.Session]: SessionModel,
+  [Model.Subscriptions]: SubscriptionsModel
+};
+
 export default class OperationRepo extends Subscribable<Operation> {
+  public deltaQueue: Delta[] = [];
   private operationQueue: EnhancedSet<Operation> = new EnhancedSet();
   constructor(private oneSignal: OneSignal) {
     super();
+    setInterval(this.processDeltaQueue.bind(this), DELTAS_BATCH_PROCESSING_TIME * 1_000);
+  }
+
+  public setup(): void {
+    this.oneSignal.system.modelRepo.subscribe((delta: Delta) => {
+      this.deltaQueue.push(delta);
+    });
+  }
+
+  private processDeltaQueue(): void {
+    if (!this.deltaQueue.length) {
+      return;
+    }
+
+    this.sortByTimestamp();
+    const deltaAggregator: DeltaAggregator = this.initDeltaAggregator();
+
+    while(this.deltaQueue.length > 0) {
+      const { model, property, newValue } = this.deltaQueue.shift() as Delta;
+      deltaAggregator[model][property] = newValue;
+    }
+  }
+
+  private initDeltaAggregator(): DeltaAggregator {
+    return {
+      [Model.Config]: {},
+      [Model.Identity]: {},
+      [Model.Properties]: {},
+      [Model.Session]: {},
+      [Model.Subscriptions]: {}
+    };
+  }
+
+  private sortByTimestamp(): void {
+    this.deltaQueue.sort((x, y) => {
+      return x.timestamp - y.timestamp;
+    });
   }
 }
