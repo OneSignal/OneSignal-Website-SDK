1976cf4368feeb0421b99da7f7b4ac753d72b2d1
Rodrigo <rogomezpalacio@protonmail.com>
2022-07-26 14:54:47 -0500

    Operation Repo
    Motivation: first we subscribe via `setup()` the operation repo to the broadcasting of new deltas from the `modelRepo` model change observer and push those deltas to a new `deltaQueue` array we will process.
    
    Individual operations will be batched and processed at a fixed time interval (can be set by remote params in future).
    
    Operations are pushed onto the ops queue after processing the delta queue and aggregating all individual deltas into one large delta (delta aggregator), separated by the model the delta corresponds to.
    
    Once the operation is processed we will broadcast the operation.
    
    TO DO: hook into request service

--- src/core/OperationRepo.ts
+++ src/core/OperationRepo.ts
@@ -1,1 +1,102 @@
-export default class OperationRepo {}
+import { Delta, DeltaAggregator, Model, Operation } from "./types";
+import Subscribable from "./utils/Subscribable";
+import { EnhancedSet } from "./utils/EnhancedSet";
+import OperationCache from "./cache/OperationCache";
+import ModelRepo from "./ModelRepo";
+
+const DELTAS_BATCH_PROCESSING_TIME = 1;
+const OPERATIONS_BATCH_PROCESSING_TIME = 5;
+
+export default class OperationRepo extends Subscribable<Operation> {
+  public deltaQueue: Delta[] = [];
+  private operationQueue: EnhancedSet<Operation> = new EnhancedSet();
+  private onlineStatus: boolean = true;
+
+  constructor(private modelRepo: ModelRepo, private operationCache: OperationCache) {
+    super();
+
+    setInterval(this.processDeltaQueue.bind(this), DELTAS_BATCH_PROCESSING_TIME * 1_000);
+    setInterval(this.processOperationQueue.bind(this), OPERATIONS_BATCH_PROCESSING_TIME * 1_000);
+
+    window.addEventListener('online', () => { this.onlineStatus = true; });
+    window.addEventListener('offline', () => { this.onlineStatus = false; });
+
+    this.modelRepo.subscribe((delta: Delta) => {
+      this.deltaQueue.push(delta);
+    });
+  }
+
+  private processDeltaQueue(): void {
+    if (!this.deltaQueue.length) {
+      return;
+    }
+
+    this.sortByTimestamp();
+    const deltaAggregator: DeltaAggregator = this.initDeltaAggregator();
+
+    while(this.deltaQueue.length > 0) {
+      const { model, property, newValue } = this.deltaQueue.shift() as Delta;
+      deltaAggregator[model][property] = newValue;
+    }
+
+    this.pushToOperationsQueue(deltaAggregator);
+  }
+
+  private pushToOperationsQueue(deltaAggregator: DeltaAggregator): void {
+    const modelKeys = Object.keys(deltaAggregator) as Model[];
+
+    for (let i=0; i<modelKeys.length; i++) {
+      const key = modelKeys[i];
+      const delta = deltaAggregator[key];
+
+      if (Object.keys(delta).length) {
+
+        const newOperation: Operation = {
+          model: key,
+          delta: delta,
+        };
+        this.operationQueue.add(newOperation);
+      }
+    }
+  }
+
+  private async getCachedOperations(): Promise<void> {
+    const cachedOps: Operation[] = await this.operationCache.getAll();
+    this.operationQueue.unshift(...cachedOps || []);
+  }
+
+  private async processOperationQueue(): Promise<void> {
+    if (!this.onlineStatus) {
+      return;
+    }
+
+    await this.getCachedOperations();
+    if (!this.operationQueue.size) {
+      return;
+    }
+
+    while(this.operationQueue.size > 0) {
+      const operation = this.operationQueue.shift();
+      /*
+      const res = RequestService.request(operation);
+      this.broadcast(res);
+      */
+    }
+  }
+
+  private initDeltaAggregator(): DeltaAggregator {
+    return {
+      [Model.Config]: {},
+      [Model.Identity]: {},
+      [Model.Properties]: {},
+      [Model.Session]: {},
+      [Model.Subscriptions]: {}
+    };
+  }
+
+  private sortByTimestamp(): void {
+    this.deltaQueue.sort((x, y) => {
+      return x.timestamp - y.timestamp;
+    });
+  }
+}
