f2474a163a70c82a4a4ff073de67a239d93a2455
Rodrigo <rogomezpalacio@protonmail.com>
2022-07-26 14:57:27 -0500

    HydratorBus
    Motivation: the hydrator bus imports both the operation repo and the model repo in order to form a communication channel between them. When the operation repo broadcasts a request result as a result of a call to the static request service, they hydrator bus will hydrate the appropriate model and set the new value in the model repo.

--- src/core/HydratorBus.ts
+++ src/core/HydratorBus.ts
@@ -1,1 +1,37 @@
-export default class HydratorBus {}
+import ModelRepo from "./ModelRepo";
+import OperationRepo from "./OperationRepo";
+import { ConfigModel, IdentityModel, Model, PropertiesModel, SessionModel, SubscriptionsModel } from "./types";
+
+type ServerResponse = object;
+
+export class HydratorBus {
+  constructor(private modelRepo: ModelRepo, private operationRepo: OperationRepo) {
+    this.operationRepo.subscribe((res: ServerResponse) => {
+      this.hydrate(res);
+    });
+  }
+
+  private hydrate(res: ServerResponse): void {
+    console.log("hydrating:", res);
+  }
+
+  private hydrateIdentity(res: ServerResponse): void {
+    this.modelRepo.set<IdentityModel>(Model.Identity, res);
+  }
+
+  private hydrateProperties(res: ServerResponse): void {
+    this.modelRepo.set<PropertiesModel>(Model.Properties, res);
+  }
+
+  private hydrateSession(res: ServerResponse): void {
+    this.modelRepo.set<SessionModel>(Model.Session, res);
+  }
+
+  private hydrateConfig(res: ServerResponse): void {
+    this.modelRepo.set<ConfigModel>(Model.Config, res);
+  }
+
+  private hydrateSubscriptions(res: ServerResponse): void {
+    this.modelRepo.set<SubscriptionsModel>(Model.Subscriptions, res);
+  }
+}
