ab12d014060e6e89b8c18420785107ac429f629f
Rodrigo <rogomezpalacio@protonmail.com>
2022-07-26 14:35:23 -0500

    Set up Proxy Observers on Model Repo using ObservableSlim
    Motivation: we use the observable-slim library to set up `Proxy` observers on our public model class properties.

--- src/core/ModelRepo.ts
+++ src/core/ModelRepo.ts
@@ -1,7 +1,21 @@
 import {
+  ConfigModel,
   Delta,
+  IdentityModel,
+  Model,
+  ObservableSlimChange,
+  PropertiesModel,
+  SessionModel,
+  SubscriptionsModel
 } from "./types";
+import ObservableSlim from 'observable-slim';
 import Subscribable from "./utils/Subscribable";
+import equal from 'fast-deep-equal/es6';
+import ModelCache from "./cache/ModelCache";
+
+interface StringIndexable {
+  [key: string]: any;
+}
 
 export default class ModelRepo extends Subscribable<Delta> {
   public session: SessionModel = {};
@@ -10,7 +24,68 @@ export default class ModelRepo extends Subscribable<Delta> {
   public subscriptions: SubscriptionsModel = {};
   public config: ConfigModel = {};
 
+  private _session: SessionModel = {};
+  private _identity: IdentityModel = {};
+  private _properties: PropertiesModel = {};
+  private _subscriptions: SubscriptionsModel = {};
+  private _config: ConfigModel = {};
+
   constructor(private modelCache: ModelCache) {
     super();
+  }
+
+  /**
+   * IMPORTANT: Must be called after instantiation to complete setup process
+   *  * loads models from cache
+   *  * creates Proxy observers assigned to public top-level class model properties
+   *  * observers will trigger writes to cache immediately upon object mutation (see `createObserver`)
+   */
+  public async setup(): Promise<void> {
+    console.log("setting up");
+    // ES5 limitation: object properties must be known at creation time
+    // TO DO: full model must be stored (e.g: with null values for empty props)
+    this._session = await this.modelCache.get(Model.Session); // might need to pull from session service
+    this._identity = await this.modelCache.get(Model.Identity);
+    this._properties = await this.modelCache.get(Model.Properties); // might need to pull from remote
+    this._subscriptions = await this.modelCache.get(Model.Subscriptions);
+    this._config = await this.modelCache.get(Model.Config);
+
+    this.session = this.createObserver(this._session, Model.Session);
+    this.identity = this.createObserver(this._identity, Model.Identity);
+    this.properties = this.createObserver(this._properties, Model.Properties);
+    this.subscriptions = this.createObserver(this._subscriptions, Model.Subscriptions);
+    this.config = this.createObserver(this._config, Model.Config);
+  }
+
+  /**
+   * Uses ObservableSlim lib to detect object changes
+   * https://github.com/ElliotNB/observable-slim
+   * Observer triggers a write to cache
+   * @param  {object} target - the object to observe
+   * @param  {Model} model - the model the change corresponds to
+   * @returns object
+   */
+  private createObserver(target: object, model: Model): object {
+    /**
+     * @param {object} target - the object we want to observe
+     * @param {boolean} delay - if true, observed changes are batched every 10ms
+     * @param {function} observer - the observer callback
+     */
+    return ObservableSlim.create(target, true, async (changes: ObservableSlimChange[]) => {
+      console.log("Observed change");
+      changes.forEach(change => {
+        if (equal(change.previousValue, change.newValue)) {
+          return;
+        }
+
+        const delta: Delta = {
+          model,
+          property: change.property,
+          newValue: change.newValue,
+          timestamp: Date.now()
+        };
+        this.broadcast(delta);
+      });
+    });
   }
 }
