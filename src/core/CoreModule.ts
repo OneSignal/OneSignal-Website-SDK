a8619be609b82f065343ec688373b533750af0d4
Rodrigo <rogomezpalacio@protonmail.com>
2022-07-26 16:15:57 -0500

    CoreModule

---
+++ src/core/CoreModule.ts
@@ -0,0 +1,29 @@
+import ModelCache from "./cache/ModelCache";
+import OperationCache from "./cache/OperationCache";
+import { HydratorBus } from "./HydratorBus";
+import ModelRepo from "./ModelRepo";
+import OperationRepo from "./OperationRepo";
+
+/**
+ * @class CoreModule
+ * Initializes core module class instances.
+ */
+
+export class CoreModule {
+  private modelRepo: ModelRepo;
+  private operationRepo: OperationRepo;
+  private modelCache: ModelCache;
+  private operationCache: OperationCache;
+
+  constructor() {
+    this.modelCache = new ModelCache();
+    this.modelRepo = new ModelRepo(this.modelCache);
+    this.operationCache = new OperationCache();
+    this.operationRepo = new OperationRepo(this.modelRepo, this.operationCache);
+    new HydratorBus(this.modelRepo, this.operationRepo);
+  }
+
+  public async setup(): Promise<void> {
+    await this.modelRepo.setup();
+  }
+}
