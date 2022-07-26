aeb9a0891b220be8cd5051200e4e902f0f72378b
Rodrigo <rogomezpalacio@protonmail.com>
2022-07-26 14:32:43 -0500

    Create model repo with public model properties
    Motivation: the public model properties on the model repo will be the "in memory" versions of the models.

--- src/core/ModelRepo.ts
+++ src/core/ModelRepo.ts
@@ -1,1 +1,16 @@
-export default class ModelRepo {}
+import {
+  Delta,
+} from "./types";
+import Subscribable from "./utils/Subscribable";
+
+export default class ModelRepo extends Subscribable<Delta> {
+  public session: SessionModel = {};
+  public identity: IdentityModel = {};
+  public properties: PropertiesModel = {};
+  public subscriptions: SubscriptionsModel = {};
+  public config: ConfigModel = {};
+
+  constructor(private modelCache: ModelCache) {
+    super();
+  }
+}
