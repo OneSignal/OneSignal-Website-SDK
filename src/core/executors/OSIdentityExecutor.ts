c6707b31db58783ceb1cb0311db128de976ccf6c
Rodrigo <rogomezpalacio@protonmail.com>
2022-09-14 17:24:04 -0500

    Model-specific Executor Implementations
    Motivation: each executor will have model-specific logic (e.g: user properties executor should only process update operations).

---
+++ src/core/executors/OSIdentityExecutor.ts
@@ -0,0 +1,51 @@
+import { CoreChangeType } from "../models/CoreChangeType";
+import { PropertyDelta } from "../models/CoreDeltas";
+import { OSExecutorConfig } from "../models/OSExecutorConfig";
+import { ModelName } from "../models/SupportedModels";
+import { Operation } from "../operationRepo/Operation";
+import { isPropertyDelta } from "../utils/typePredicates";
+import OSExecutorBase from "./OSExecutorBase";
+
+export class OSIdentityExecutor<Model> extends OSExecutorBase<Model> {
+  constructor(executorConfig: OSExecutorConfig<Model>) {
+    super(executorConfig);
+  }
+
+  public processDeltaQueue(): void {
+    if (this._deltaQueue.length === 0) {
+      return;
+    }
+
+    const addDeltas: PropertyDelta<Model>[] = [];
+    const removeDeltas: PropertyDelta<Model>[] = [];
+    const updateDeltas: PropertyDelta<Model>[] = [];
+
+    this._deltaQueue.forEach(delta => {
+      if (!isPropertyDelta(delta)) {
+        return;
+      }
+
+      const deltaChangeType = this._getChangeType(delta.oldValue, delta.newValue);
+
+      if (deltaChangeType === CoreChangeType.Add) {
+        addDeltas.push(delta);
+      } else if (deltaChangeType === CoreChangeType.Remove) {
+        removeDeltas.push(delta);
+      } else if (deltaChangeType === CoreChangeType.Update) {
+        updateDeltas.push(delta);
+      }
+    });
+
+    if (addDeltas.length > 0) {
+      this._enqueueOperation(new Operation(CoreChangeType.Add, ModelName.Identity, addDeltas));
+    }
+    if (removeDeltas.length > 0) {
+      this._enqueueOperation(new Operation(CoreChangeType.Remove, ModelName.Identity, removeDeltas));
+    }
+    if (updateDeltas.length > 0) {
+      this._enqueueOperation(new Operation(CoreChangeType.Update, ModelName.Identity, updateDeltas));
+    }
+
+    this._flushDeltas();
+  }
+}
