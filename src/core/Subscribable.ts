64095cd50775c781909dba9f568b624e6a908254
Rodrigo <rogomezpalacio@protonmail.com>
2022-09-14 16:49:14 -0500

    Subscribable Class
    Motivation: to be used to communicate between core module components

---
+++ src/core/Subscribable.ts
@@ -0,0 +1,24 @@
+export default class Subscribable<MessageType> {
+  protected subscribers = new Set<(msg: MessageType) => void>();
+
+  constructor() {}
+
+  /**
+   * Subscribe to the message stream.
+   * @param  {(msg:MessageType)=>void} callback
+   */
+  public subscribe(callback: (msg: MessageType) => void): () => void {
+    this.subscribers.add(callback);
+    return () => this.subscribers.delete(callback);
+  }
+
+  /**
+   * Publish a message to the message stream.
+   * @param msg The message to publish
+   */
+  public broadcast(msg: MessageType) {
+    this.subscribers.forEach(callback => {
+      callback(msg);
+    });
+  }
+}
