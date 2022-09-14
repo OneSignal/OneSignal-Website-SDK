c2372b50f087937041f967e0af61dcda335aeaa2
Rodrigo <rogomezpalacio@protonmail.com>
2022-09-14 16:50:59 -0500

    OSModel Implementation
    Motivation: a class that encapsulates an individual model instance and its relevant functions.

---
+++ src/core/modelRepo/OSModel.ts
@@ -0,0 +1,52 @@
+import { OSModelUpdatedArgs } from "./OSModelUpdatedArgs";
+import Subscribable from "../Subscribable";
+import EncodedModel from "../caching/EncodedModel";
+import { StringKeys } from "../models/StringKeys";
+import { SupportedModel, ModelName } from "../models/SupportedModels";
+
+export class OSModel<Model> extends Subscribable<OSModelUpdatedArgs<Model>> {
+  public id: string;
+  public data?: Model;
+
+  constructor(public modelName: ModelName, id?: string, data?: Model) {
+    super();
+    this.id = id ?? Math.random().toString(36).substring(2);
+    this.modelName = modelName;
+    this.data = data;
+  }
+
+  /**
+   * We use this method to update the model data.
+   * Results in a broadcasted update event.
+   */
+  public set(property: StringKeys<Model>, newValue: any): void {
+    let oldValue;
+
+    if (this.data) {
+      oldValue = this.data[property];
+      this.data[property] = newValue;
+    }
+
+    this.broadcast(new OSModelUpdatedArgs(this, property, oldValue, newValue));
+  }
+
+  /**
+   * Updates the entire model data.
+   * To be called when updating the data with a remote sync.
+   */
+  public hydrate(data: Model): void {
+    this.data = data;
+  }
+
+
+  public encode(): EncodedModel {
+    const modelId = this.id as string;
+    const modelName = this.modelName;
+    return { modelId, modelName, ...this.data };
+  }
+
+  static decode(encodedModel: EncodedModel): OSModel<SupportedModel> {
+    const { modelId: id, modelName, ...data } = encodedModel;
+    return new OSModel(modelName, id, data);
+  }
+}
