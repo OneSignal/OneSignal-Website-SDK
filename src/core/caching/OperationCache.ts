30ce6661476b7c19de4a3afc832f0c7a4d9bf2b3
Rodrigo <rogomezpalacio@protonmail.com>
2022-09-14 17:27:36 -0500

    OperationCache
    Static operation cache class with enqueue, getOperations, delete, and flush functions.

---
+++ src/core/caching/OperationCache.ts
@@ -0,0 +1,27 @@
+import { Operation } from "../operationRepo/Operation";
+
+export default class OperationCache {
+  static enqueue<Model>(operation: Operation<Model>): void {
+    const fromCache = localStorage.getItem("operationCache");
+    const operations: { [key: string]: any } = fromCache ? JSON.parse(fromCache) : {};
+    operations[operation.id] = operation;
+    localStorage.setItem("operationCache", JSON.stringify(operations));
+  }
+
+  static getOperations<Model>(): Operation<Model>[] {
+    const fromCache = localStorage.getItem("operationCache");
+    const operations: Operation<Model>[] = fromCache ? Object.values(JSON.parse(fromCache)) : [];
+    return operations.sort((a, b) => a.timestamp - b.timestamp);
+  }
+
+  static delete(id: string): void {
+    const fromCache = localStorage.getItem("operationCache");
+    const operations: { [key: string]: any } = fromCache ? JSON.parse(fromCache) : {};
+    delete operations[id];
+    localStorage.setItem("operationCache", JSON.stringify(operations));
+  }
+
+  static flushOperations(): void {
+    localStorage.removeItem("operationCache");
+  }
+}
