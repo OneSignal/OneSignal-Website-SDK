068a04a46807099d81057e7ce1fbead91649e5b0
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-10-07 19:14:54 -0500

    Fixes ServiceWorker Build Script
    Previously wasn't taking the port into consideration

--- build/scripts/buildServiceWorker.sh
+++ build/scripts/buildServiceWorker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Builds the local service worker files for express_webpack for use in local dev env
-echo "importScripts(\"https://${1}/sdks/Dev-OneSignalSDKWorker.js\");" > express_webpack/push/onesignal/OneSignalSDKWorker.js
-echo "importScripts(\"https://${1}/sdks/Dev-OneSignalSDKWorker.js\");" > express_webpack/OneSignalSDKWorker.js
+echo "Building local service worker file with arguments: ${1} ${2}"
+echo "importScripts(\"https://${1}${2}/sdks/Dev-OneSignalSDKWorker.js\");" > express_webpack/push/onesignal/OneSignalSDKWorker.js
+echo "importScripts(\"https://${1}${2}/sdks/Dev-OneSignalSDKWorker.js\");" > express_webpack/OneSignalSDKWorker.js
