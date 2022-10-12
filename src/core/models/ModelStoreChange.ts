30a4830aa4f6aa45dd45470a1b46eeb98d972120
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-10-11 21:17:19 -0500

    Rename `id` to `modelId` for clarity in `ModelStoreChange`

--- src/core/models/ModelStoreChange.ts
+++ src/core/models/ModelStoreChange.ts
@@ -4,22 +4,23 @@ import { CoreChangeType } from "./CoreChangeType";
 
 export interface ModelStoreChange<Model> {
   type: CoreChangeType;
-  id: string;
+  modelId: string;
   noRemoteSync: boolean;
   payload: OSModel<Model> | OSModelUpdatedArgs<Model>;
 }
 
 export class ModelStoreAdded<Model> implements ModelStoreChange<Model> {
   type: CoreChangeType.Add = CoreChangeType.Add;
-  constructor(public id: string, public payload: OSModel<Model>, public noRemoteSync: boolean = false) {}
+  constructor(public modelId: string, public payload: OSModel<Model>, public noRemoteSync: boolean = false) {}
 }
 
 export class ModelStoreRemoved<Model> implements ModelStoreChange<Model> {
   type: CoreChangeType.Remove = CoreChangeType.Remove;
-  constructor(public id: string, public payload: OSModel<Model>, public noRemoteSync: boolean = false) {}
+  constructor(public modelId: string, public payload: OSModel<Model>, public noRemoteSync: boolean = false) {}
 }
 
 export class ModelStoreUpdated<Model> implements ModelStoreChange<Model> {
   type: CoreChangeType.Update = CoreChangeType.Update;
   constructor(public id: string, public payload: OSModelUpdatedArgs<Model>, public noRemoteSync: boolean = false) {}
+  constructor(public modelId: string, public payload: OSModelUpdatedArgs<Model>) {}
 }
