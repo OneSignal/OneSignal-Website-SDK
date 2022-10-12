c8f2605eb65da4913f6607c92de97e6ebcc4b42f
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-10-11 19:56:18 -0500

    User Instance Class Implementation
    Motivation: this class contains references to all models for current users. Also contains logic to create a new user

---
+++ src/onesignal/User.ts
@@ -0,0 +1,42 @@
+import Environment from "../../src/shared/helpers/Environment";
+import { OSModel } from "../../src/core/modelRepo/OSModel";
+import { IdentityModel } from "../../src/core/models/IdentityModel";
+import { SupportedSubscription } from "../../src/core/models/SubscriptionModels";
+import { ModelName, SupportedModel } from "../../src/core/models/SupportedModels";
+import { UserPropertiesModel } from "../../src/core/models/UserPropertiesModel";
+import OneSignal from "./OneSignal";
+
+export default class User {
+  constructor(
+    public identity?: OSModel<IdentityModel>,
+    public userProperties?: OSModel<UserPropertiesModel>,
+    public pushSubscriptions?: OSModel<SupportedSubscription>,
+    public smsSubscriptions?: { [key: string]: OSModel<SupportedSubscription> },
+    public emailSubscriptions?: { [key: string]: OSModel<SupportedSubscription> },
+  ) {
+    // initialize new user
+    if (!identity) {
+      this.createNewUser();
+    }
+
+    if (!userProperties) {
+      this.userProperties = new OSModel<UserPropertiesModel>(ModelName.Properties, undefined, {
+        language: Environment.getLanguage(),
+        timezone_id: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      });
+      OneSignal.coreDirector.add(ModelName.Properties, this.userProperties as OSModel<SupportedModel>);
+    }
+  }
+
+  private createNewUser(): void {
+    // TO DO: create user and get fresh onesignalId
+    const data = {
+      onesignalId: "123", // mock data
+    };
+
+    this.identity = new OSModel<IdentityModel>(ModelName.Identity, undefined, data);
+    OneSignal.coreDirector.add(ModelName.Identity, this.identity as OSModel<SupportedModel>);
+
+    // TO DO: populate subscription models also
+  }
+}
