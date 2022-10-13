3c126256d0f4fac0c3bfcb71cca0e3f1a1e515ee
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-10-13 18:48:05 -0500

    Remove `noRemoteSync`
    Not actually needed since we added the hydration change type

--- src/core/models/ModelStoreChange.ts
+++ src/core/models/ModelStoreChange.ts
@@ -5,30 +5,25 @@ import { CoreChangeType } from "./CoreChangeType";
 export interface ModelStoreChange<Model> {
   type: CoreChangeType;
   modelId: string;
-  noRemoteSync: boolean;
   payload: OSModel<Model> | OSModelUpdatedArgs<Model>;
 }
 
 export class ModelStoreAdded<Model> implements ModelStoreChange<Model> {
   type: CoreChangeType.Add = CoreChangeType.Add;
-  noRemoteSync: boolean = false;
   constructor(public modelId: string, public payload: OSModel<Model>) {}
 }
 
 export class ModelStoreRemoved<Model> implements ModelStoreChange<Model> {
   type: CoreChangeType.Remove = CoreChangeType.Remove;
-  noRemoteSync: boolean = false;
   constructor(public modelId: string, public payload: OSModel<Model>) {}
 }
 
 export class ModelStoreUpdated<Model> implements ModelStoreChange<Model> {
   type: CoreChangeType.Update = CoreChangeType.Update;
-  noRemoteSync: boolean = false;
   constructor(public modelId: string, public payload: OSModelUpdatedArgs<Model>) {}
 }
 
 export class ModelStoreHydrated<Model> implements ModelStoreChange<Model> {
   type: CoreChangeType.Hydrate = CoreChangeType.Hydrate;
-  noRemoteSync: boolean = true;
   constructor(public modelId: string, public payload: OSModel<Model>) {}
 }
