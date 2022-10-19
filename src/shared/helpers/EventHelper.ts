1c59be2be760e0db13b2d06ea27ac8ed309bced4
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-10-19 13:57:37 -0500

    Nits

--- src/shared/helpers/EventHelper.ts
+++ src/shared/helpers/EventHelper.ts
@@ -68,7 +68,7 @@ export default class EventHelper {
       const { deviceId } = await Database.getSubscription();
       const { appId } = await Database.getAppConfig();
 
-      const welcome_notification_opts = OneSignal.config.userConfig.welcomeNotification;
+      const welcome_notification_opts = OneSignal.config?.userConfig.welcomeNotification;
       const welcome_notification_disabled =
         welcome_notification_opts !== undefined && welcome_notification_opts['disable'] === true;
       let title =
