0af8e2540d14f18f610fa809e0ed5746748f2214
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-10-25 16:15:01 -0500

    ExecutorBase & ExecutorConfig changes
    Motivation:
    1. we need to await the result of the calls: -> make asynchronous
    2. Add retry logic stopping condition

--- src/core/models/ExecutorConfig.ts
+++ src/core/models/ExecutorConfig.ts
@@ -1,12 +1,12 @@
+import ExecutorResult from "../executors/ExecutorResult";
 import { Operation } from "../operationRepo/Operation";
-import { ExecutorResult } from "./ExecutorResult";
 import { ModelName, SupportedModel } from "./SupportedModels";
 
 export type ExecutorConfig<Model> = {
   modelName: ModelName;
-  add?: (operation: Operation<Model>) => ExecutorResult;
-  remove?: (operation: Operation<Model>) => ExecutorResult;
-  update?: (operation: Operation<Model>) => ExecutorResult;
+  add?: (operation: Operation<Model>) => Promise<ExecutorResult>;
+  remove?: (operation: Operation<Model>) => Promise<ExecutorResult>;
+  update?: (operation: Operation<Model>) => Promise<ExecutorResult>;
 };
 
 export type ExecutorConfigMap = {
