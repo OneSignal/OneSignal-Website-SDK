b849c9461eb00e4063b7d61b3344dd68df0d6b45
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-11-07 17:55:03 -0600

    Update OneSignal API implementation
    Motivation: update existing `OneSignalApi` classes to use new `OneSignalApiBaseResult` type (includes status code).

--- src/shared/api/OneSignalApiBase.ts
+++ src/shared/api/OneSignalApiBase.ts
@@ -1,86 +1,90 @@
 import Utils from "../context/Utils";
 import { OneSignalApiError, OneSignalApiErrorKind } from "../errors/OneSignalApiError";
 import Environment from "../helpers/Environment";
-import Log from "../libraries/Log";
 import SdkEnvironment from "../managers/SdkEnvironment";
+import OneSignalApiBaseResult from "./OneSignalApiBaseResult";
 
 type Headers = any[] & {[key: string]: any};
 type SupportedMethods = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
 
 export class OneSignalApiBase {
-  static get(action: string, data?: any, headers?: Headers | undefined) {
+  static get(action: string, data?: any, headers?: Headers | undefined): Promise<OneSignalApiBaseResult | undefined> {
     return OneSignalApiBase.call('GET', action, data, headers);
   }
 
-  static post(action: string, data?: any, headers?: Headers | undefined) {
+  static post(action: string, data?: any, headers?: Headers | undefined): Promise<OneSignalApiBaseResult | undefined> {
     return OneSignalApiBase.call('POST', action, data, headers);
   }
 
-  static put(action: string, data?: any, headers?: Headers | undefined) {
+  static put(action: string, data?: any, headers?: Headers | undefined): Promise<OneSignalApiBaseResult | undefined> {
     return OneSignalApiBase.call('PUT', action, data, headers);
   }
 
-  static delete(action: string, data?: any, headers?: Headers | undefined) {
-    return OneSignalApiBase.call('DELETE', action, data, headers);
+  static delete(action: string, data?: any, headers?: Headers | undefined):
+    Promise<OneSignalApiBaseResult | undefined> {
+      return OneSignalApiBase.call('DELETE', action, data, headers);
   }
 
-  static patch(action: string, data?: any, headers?: Headers | undefined) {
+  static patch(action: string, data?: any, headers?: Headers | undefined): Promise<OneSignalApiBaseResult | undefined> {
     return OneSignalApiBase.call('PATCH', action, data, headers);
   }
 
-  private static call(method: SupportedMethods, action: string, data: any, headers: Headers | undefined): Promise<any> {
-    if (method === "GET") {
-      if (action.indexOf("players") > -1 && action.indexOf("app_id=") === -1) {
-        console.error("Calls to player api are not permitted without app_id");
-        return Promise.reject(new OneSignalApiError(OneSignalApiErrorKind.MissingAppId));
+  private static call(method: SupportedMethods, action: string, data: any, headers: Headers | undefined):
+    Promise<OneSignalApiBaseResult | undefined> {
+      if (method === "GET") {
+        if (action.indexOf("players") > -1 && action.indexOf("app_id=") === -1) {
+          console.error("Calls to player api are not permitted without app_id");
+          return Promise.reject(new OneSignalApiError(OneSignalApiErrorKind.MissingAppId));
+        }
+      } else {
+        if (action.indexOf("players") > -1 && (!data || !data["app_id"])) {
+          console.error("Calls to player api are not permitted without app_id");
+          return Promise.reject(new OneSignalApiError(OneSignalApiErrorKind.MissingAppId));
+        }
       }
+
+      const callHeaders: any = new Headers();
+      callHeaders.append("Origin", SdkEnvironment.getOrigin());
+      callHeaders.append('SDK-Version', `onesignal/web/${Environment.version()}`);
+      callHeaders.append('Content-Type', 'application/json;charset=UTF-8');
+      if (headers) {
+        for (const key of Object.keys(headers)) {
+          callHeaders.append(key, headers[key]);
+        }
+      }
+
+      const contents: RequestInit = {
+        method: method || 'NO_METHOD_SPECIFIED',
+        headers: callHeaders,
+        cache: 'no-cache'
+      };
+      if (data)
+        contents.body = JSON.stringify(data);
+
+      const url = `${SdkEnvironment.getOneSignalApiUrl(undefined, action).toString()}/${action}`;
+
+      return OneSignalApiBase.executeFetch(url, contents);
+  }
+
+  private static async executeFetch(url: string, contents: RequestInit): Promise<OneSignalApiBaseResult | undefined> {
+    const response = await fetch(url, contents);
+    const { status } = response;
+    const json = await response.json();
+
+    if (status >= 200 && status < 300) {
+      return {
+        result: json,
+        status
+      };
+    }
+
+    const error = OneSignalApiBase.identifyError(json);
+    if (error === 'no-user-id-error') {
+      // TODO: This returns undefined
     } else {
-      if (action.indexOf("players") > -1 && (!data || !data["app_id"])) {
-        console.error("Calls to player api are not permitted without app_id");
-        return Promise.reject(new OneSignalApiError(OneSignalApiErrorKind.MissingAppId));
-      }
+      return Promise.reject(json);
     }
-
-    const callHeaders: any = new Headers();
-    callHeaders.append("Origin", SdkEnvironment.getOrigin());
-    callHeaders.append('SDK-Version', `onesignal/web/${Environment.version()}`);
-    callHeaders.append('Content-Type', 'application/json;charset=UTF-8');
-    if (headers) {
-      for (const key of Object.keys(headers)) {
-        callHeaders.append(key, headers[key]);
-      }
-    }
-
-    const contents: RequestInit = {
-      method: method || 'NO_METHOD_SPECIFIED',
-      headers: callHeaders,
-      cache: 'no-cache'
-    };
-    if (data)
-      contents.body = JSON.stringify(data);
-
-    let status: number;
-    return fetch(SdkEnvironment.getOneSignalApiUrl(undefined, action).toString() + '/' + action, contents)
-        .then(response => {
-          status = response.status;
-          return response.json();
-        })
-        .then(json => {
-          if (status >= 200 && status < 300)
-            return json;
-          else {
-            const error = OneSignalApiBase.identifyError(json);
-            if (error === 'no-user-id-error') {
-              // TODO: This returns undefined
-            } else {
-              return Promise.reject(json);
-            }
-          }
-        })
-        .catch(err => {
-          Log.warn(`Could not complete request to /${action}`, err);
-          return Promise.reject(err);
-        });
+    return undefined;
   }
 
   private static identifyError(error: any) {
