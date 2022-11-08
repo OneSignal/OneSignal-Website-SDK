763342d4b3b53cf521d5b24de72bd505dae85ca8
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-11-08 16:46:49 -0600

    Ability to resubscribe operation repo to model repo
    Motivation: after resetting the user and clearing the model repo to achieve a blank temporary user and related models during the login process, we need to resubscribe the operation repo to the newly instantiated model repo.

--- src/core/operationRepo/OperationRepo.ts
+++ src/core/operationRepo/OperationRepo.ts
@@ -6,18 +6,27 @@ import { logMethodCall } from "../../shared/utils/utils";
 
 export class OperationRepo {
   public executorStore: ExecutorStore;
+  private _unsubscribeFromModelRepo: () => void;
 
   constructor(private modelRepo: ModelRepo) {
     this.executorStore = new ExecutorStore();
 
-    this.modelRepo.subscribe((delta: CoreDelta<SupportedModel>) => {
-      this.processDelta(delta);
+    this._unsubscribeFromModelRepo = this.modelRepo.subscribe((delta: CoreDelta<SupportedModel>) => {
+      this._processDelta(delta);
     });
   }
 
-  private processDelta(delta: CoreDelta<SupportedModel>): void {
+  private _processDelta(delta: CoreDelta<SupportedModel>): void {
     logMethodCall("processDelta", { delta });
     const { modelName } = delta.model;
     this.executorStore[modelName]?.enqueueDelta(delta);
+  }
+
+  public setModelRepoAndResubscribe(modelRepo: ModelRepo) {
+    this.modelRepo = modelRepo;
+    this._unsubscribeFromModelRepo();
+    this._unsubscribeFromModelRepo = this.modelRepo.subscribe((delta: CoreDelta<SupportedModel>) => {
+      this._processDelta(delta);
+    });
   }
 }
