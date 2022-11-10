35cd9a4ded85a787e9201151f7a094573bc475dd
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-11-10 13:41:13 -0600

    New model: `FutureIdentityModel`
    Motivation: we should support not having a `onesignalId` in case it hasn't returned from the server.
    
    Useful when creating a temporary blank anonymous user that doesn't need a `onesignalId` since it will be replaced anyway with updated user data from a fetch call

--- src/core/models/IdentityModel.ts
+++ src/core/models/IdentityModel.ts
@@ -1,5 +1,10 @@
-export interface IdentityModel {
+export interface IdentityModel extends FutureIdentityModel {
   onesignalId: string;
+}
+
+export interface FutureIdentityModel {
   externalId?: string;
   [key: string]: string | undefined;
 }
+
+export type SupportedIdentity = IdentityModel | FutureIdentityModel;
