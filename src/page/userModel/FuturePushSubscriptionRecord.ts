df518b20fc859be5fd56291c122a9f46e3b387aa
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-11-16 13:10:05 -0600

    `FuturePushSubscriptionRecord` Implementation
    Motivation: class that takes a `RawPushSubscription` and returns a  populated`FuturePushSubscriptionRecord`

---
+++ src/page/userModel/FuturePushSubscriptionRecord.ts
@@ -0,0 +1,67 @@
+import { Serializable } from "../models/Serializable";
+import { FutureSubscriptionModel, SubscriptionType } from "../../core/models/SubscriptionModels";
+import { EnvironmentInfoHelper } from "../helpers/EnvironmentInfoHelper";
+import { RawPushSubscription } from "src/shared/models/RawPushSubscription";
+import OneSignalUtils from "../../shared/utils/OneSignalUtils";
+
+export default class FuturePushSubscriptionRecord implements Serializable {
+  readonly type: SubscriptionType;
+  readonly token?: string; // maps to legacy player.identifier
+  readonly enabled?: boolean;
+  readonly notificationTypes?: number;
+  readonly sdk: string;
+  readonly deviceModel: string;
+  readonly deviceOs: number;
+  readonly webAuth?: string;
+  readonly webp256?: string;
+
+  constructor(rawPushSubscription: RawPushSubscription) {
+    const environment = EnvironmentInfoHelper.getEnvironmentInfo();
+
+    this.token = this._getToken(rawPushSubscription);
+    this.type = this._getSubscriptionType();
+    this.sdk = __VERSION__;
+    this.deviceModel = navigator.platform;
+    this.deviceOs = isNaN(environment.browserVersion) ? -1 : environment.browserVersion;
+    this.webAuth = rawPushSubscription.w3cAuth;
+    this.webp256 = rawPushSubscription.w3cP256dh;
+  }
+
+  private _getToken(subscription: RawPushSubscription): string | undefined {
+    const browser = OneSignalUtils.redetectBrowserUserAgent();
+    const isLegacySafari = browser.safari && browser.version < 16;
+    return isLegacySafari ? subscription.safariDeviceToken : subscription.w3cEndpoint ?
+      subscription.w3cEndpoint.toString() : undefined;
+  }
+
+  private _getSubscriptionType(): SubscriptionType {
+    const browser = OneSignalUtils.redetectBrowserUserAgent();
+    if (browser.firefox) {
+      return SubscriptionType.FirefoxPush;
+    }
+    if (browser.safari && browser.version >= 16) {
+      return SubscriptionType.SafariPush;
+    }
+    if (browser.safari && browser.version < 16) {
+      return SubscriptionType.SafariLegacyPush;
+    }
+    if (browser.msedge) {
+      return SubscriptionType.WindowPush;
+    }
+    return SubscriptionType.ChromePush;
+  }
+
+  serialize(): FutureSubscriptionModel {
+    return {
+      type: this.type,
+      token: this.token,
+      enabled: this.enabled,
+      notification_types: this.notificationTypes,
+      sdk: this.sdk,
+      device_model: this.deviceModel,
+      device_os: this.deviceOs,
+      web_auth: this.webAuth,
+      web_p256: this.webp256,
+    };
+  }
+}
