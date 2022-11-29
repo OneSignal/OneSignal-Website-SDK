e5c2a1018c607155463c505d3789ac54886144ec
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-11-29 00:02:03 -0600

    Add `RequestMetaData` to encapsulate app and subscription ids
    Motivation: reduce argument number to RequestService functions

--- src/core/requestService/RequestService.ts
+++ src/core/requestService/RequestService.ts
@@ -7,73 +7,96 @@ import { isIdentityObject } from "../utils/typePredicates";
 import AliasPair from "./AliasPair";
 import { UpdateUserPayload } from "./UpdateUserPayload";
 import UserData from "../models/UserData";
+import { RequestMetadata } from "../models/RequestMetadata";
 
 export class RequestService {
   /* U S E R   O P E R A T I O N S */
 
-  static createUser(requestBody: UserData): Promise<OneSignalApiBaseResponse>{
-    return OneSignalApiBase.post(`user`, requestBody);
+  /**
+   * Creates a new user
+   * @param requestMetadata - { appId }
+   * @param requestBody - The user's properties, identity, and subscriptions
+   */
+  static createUser(requestMetadata: RequestMetadata, requestBody: UserData): Promise<OneSignalApiBaseResponse> {
+    const { appId } = requestMetadata;
+    return OneSignalApiBase.post(`${appId}/user`, requestBody);
   }
 
   /**
    * Returns the user's properties, aliases, and subscriptions
-   * @param alias - alias label & id
-   * @returns user properties object, identity object, and subscription objects
+   * @param requestMetadata - { appId }
+   * @param alias - The user's alias
+   * @returns - A promise that resolves with the user's properties, identity, and subscriptions
    */
-  static getUser(alias: AliasPair): Promise<OneSignalApiBaseResponse>{
-    return OneSignalApiBase.get(`user/by/${alias.label}/${alias.id}`);
+  static getUser(requestMetadata: RequestMetadata, alias: AliasPair): Promise<OneSignalApiBaseResponse> {
+    const { appId } = requestMetadata;
+    return OneSignalApiBase.get(`${appId}/user/by/${alias.label}/${alias.id}`);
   }
 
   /**
    * Updates an existing user's properties
    *  - Aliases and subscriptions are managed via other endpoints
+   * @param requestMetadata - { appId }
    * @param alias - alias label & id
-   * @param refreshDeviceMetaData - if true, updates ip, country, & last active
-   * @returns properties object
+   * @param payload - update user payload
+   * @returns no body
    */
-  static updateUser(alias: AliasPair, payload: UpdateUserPayload): Promise<OneSignalApiBaseResponse>{
-    return OneSignalApiBase.patch(`user/by/${alias.label}/${alias.id}`, payload);
+  static updateUser(requestMetadata: RequestMetadata, alias: AliasPair, payload: UpdateUserPayload)
+    : Promise<OneSignalApiBaseResponse>
+    {
+      const { appId, subscriptionId } = requestMetadata;
+      const header = subscriptionId ? [{ "OneSignal-Subscription-Id": subscriptionId }] : undefined;
+      return OneSignalApiBase.patch(`${appId}/user/by/${alias.label}/${alias.id}`, payload, header);
   }
 
   /**
    * Removes the user identified by the given alias pair, and all subscriptions and aliases
+   * @param requestMetadata - { appId }
    * @param alias - alias label & id
    */
-  static deleteUser(alias: AliasPair): Promise<OneSignalApiBaseResponse>{
-    return OneSignalApiBase.delete(`user/by/${alias.label}/${alias.id}`);
+  static deleteUser(requestMetadata: RequestMetadata, alias: AliasPair): Promise<OneSignalApiBaseResponse> {
+    const { appId } = requestMetadata;
+    return OneSignalApiBase.delete(`${appId}/user/by/${alias.label}/${alias.id}`);
   }
 
   /* I D E N T I T Y   O P E R A T I O N S */
 
   /**
    * Upserts one or more aliases for the user identified by the given alias pair
+   * @param requestMetadata - { appId }
    * @param alias - alias label & id
-   * @param identity - identity object
-   * @returns identity object
+   * @param identity - identity label & id
    */
-  static identifyUser(alias: AliasPair, identity: SupportedIdentity): Promise<OneSignalApiBaseResponse>{
-    return OneSignalApiBase.put(`user/by/${alias.label}/${alias.id}/identity`, {
-      identity
-    });
+  static identifyUser(requestMetadata: RequestMetadata, alias: AliasPair, identity: SupportedIdentity)
+    : Promise<OneSignalApiBaseResponse>
+    {
+      const { appId } = requestMetadata;
+      return OneSignalApiBase.put(`${appId}/user/by/${alias.label}/${alias.id}/identity`, {
+        identity
+      });
   }
 
   /**
    * Lists all aliases for the user identified by the given alias pair
+   * @param requestMetadata - { appId }
    * @param alias - alias label & id
-   * @returns identity object
    */
-  static getUserIdentity(alias: AliasPair): Promise<OneSignalApiBaseResponse>{
-    return OneSignalApiBase.get(`user/by/${alias.label}/${alias.id}/identity`);
+  static getUserIdentity(requestMetadata: RequestMetadata, alias: AliasPair): Promise<OneSignalApiBaseResponse> {
+    const { appId } = requestMetadata;
+    return OneSignalApiBase.get(`${appId}/user/by/${alias.label}/${alias.id}/identity`);
   }
 
   /**
    * Deletes an alias for the user identified by the given alias pair
+   * @param requestMetadata - { appId }
    * @param alias - alias label & id
-   * @param labelToRemove - alias label to remove
-   * @returns identity object
+   * @param labelToRemove - label of identity to remove
    */
-  static deleteAlias(alias: AliasPair, labelToRemove: string): Promise<OneSignalApiBaseResponse>{
-    const identity = OneSignalApiBase.delete(`user/by/${alias.label}/${alias.id}/identity/${labelToRemove}`);
+  static deleteAlias(requestMetadata: RequestMetadata, alias: AliasPair, labelToRemove: string)
+  : Promise<OneSignalApiBaseResponse>
+  {
+    const { appId } = requestMetadata;
+    const identity = OneSignalApiBase.delete(`${appId}/user/by/${alias.label}/${alias.id}/identity/${labelToRemove}`);
 
     if (isIdentityObject(identity)) {
       return identity;
@@ -86,70 +109,95 @@ export class RequestService {
   /**
    * Creates a new subscription for the user identified by the given alias pair
    * Useful to add email or SMS subscriptions to a user
+   * @param requestMetadata - { appId }
    * @param alias - alias label & id
-   * @param subscription - subscription object
-   * @returns subscription object
+   * @param subscription - subscription label & id
    */
-  static createSubscription(alias: AliasPair, subscription: FutureSubscriptionModel):
-    Promise<OneSignalApiBaseResponse>{
-      return OneSignalApiBase.post(`user/by/${alias.label}/${alias.id}/subscription`, subscription);
+  static createSubscription(requestMetadata: RequestMetadata, alias: AliasPair, subscription: FutureSubscriptionModel):
+    Promise<OneSignalApiBaseResponse> {
+      // TO DO: remove temporary mock response
+      return new Promise(resolve => {
+        resolve({
+          status: 200,
+          result: {
+            id: "11111111-1111-1111-1111-111111111111",
+            ...subscription
+          }
+        });
+      });
+
+      const { appId } = requestMetadata;
+      return OneSignalApiBase.post(`${appId}/user/by/${alias.label}/${alias.id}/subscription`, subscription);
   }
+
 
   /**
    * Updates an existing Subscription’s properties.
+   * @param requestMetadata - { appId }
    * @param subscriptionId - subscription id
-   * @param subscription - partial or full subscription object
-   * @returns subscription object
+   * @param subscription - subscription object
    */
-  static updateSubscription(subscriptionId: string, subscription: Partial<SubscriptionModel>):
-    Promise<OneSignalApiBaseResponse>{
-      return OneSignalApiBase.patch(`subscriptions/${subscriptionId}`, subscription);
+  static updateSubscription(
+    requestMetadata: RequestMetadata,
+    subscriptionId: string,
+    subscription: Partial<SubscriptionModel>)
+    : Promise<OneSignalApiBaseResponse> {
+      const { appId } = requestMetadata;
+      return OneSignalApiBase.patch(`${appId}/subscriptions/${subscriptionId}`, subscription);
   }
 
   /**
    * Deletes the subscription.
    * Creates an "orphan" user record if the user has no other subscriptions.
+   * @param requestMetadata - { appId }
    * @param subscriptionId - subscription id
    */
-  static deleteSubscription(subscriptionId: string): Promise<OneSignalApiBaseResponse>{
-    return OneSignalApiBase.delete(`subscriptions/${subscriptionId}`);
+  static deleteSubscription(requestMetadata: RequestMetadata, subscriptionId: string)
+    : Promise<OneSignalApiBaseResponse> {
+      const { appId } = requestMetadata;
+      return OneSignalApiBase.delete(`${appId}/subscriptions/${subscriptionId}`);
   }
 
   /**
    * Lists all aliases for the user identified by the given subscription id
+   * @param requestMetadata - { appId }
    * @param subscriptionId - subscription id
-   * @returns identity object
    */
-  static fetchAliasesForSubscription(subscriptionId: string): Promise<OneSignalApiBaseResponse>{
-    return OneSignalApiBase.get(`subscriptions/${subscriptionId}/identity`);
+  static fetchAliasesForSubscription(requestMetadata: RequestMetadata, subscriptionId: string)
+    : Promise<OneSignalApiBaseResponse> {
+      const { appId } = requestMetadata;
+      return OneSignalApiBase.get(`${appId}/subscriptions/${subscriptionId}/identity`);
   }
 
   /**
    * Upserts one or more aliases for the user identified by the given subscription id
+   * @param requestMetadata - { appId }
    * @param subscriptionId - subscription id
-   * @param identity - identity object
-   * @returns identity object
+   * @param identity - identity label & id
    */
-  static identifyUserForSubscription(subscriptionId: string, identity: IdentityModel):
-    Promise<OneSignalApiBaseResponse>{
-      return OneSignalApiBase.put(`user/by/subscriptions/${subscriptionId}/identity`, identity);
+  static identifyUserForSubscription(requestMetadata: RequestMetadata, subscriptionId: string, identity: IdentityModel):
+    Promise<OneSignalApiBaseResponse> {
+      const { appId } = requestMetadata;
+      return OneSignalApiBase.put(`${appId}/user/by/subscriptions/${subscriptionId}/identity`, identity);
   }
 
   /**
    * Transfers this Subscription to the User identified by the identity in the payload.
+   * @param requestMetadata - { appId }
    * @param subscriptionId - subscription id
-   * @param identity - identity object
+   * @param identity - identity label & id
    * @param retainPreviousOwner - if true *AND* subscription is last subscription for the previous
    * user, an orphan user will be created. Otherwise, the previous user will be deleted. Useful when going
    * from a guest (anonymous) user to an identified user. If the previous owner had other subscriptions,
    * nothing will happen to the previous user.
-   * @returns identity object
    */
   static transferSubscription(
+    requestMetadata: RequestMetadata,
     subscriptionId: string,
     identity: SupportedIdentity,
-    retainPreviousOwner: boolean): Promise<OneSignalApiBaseResponse>{
-      return OneSignalApiBase.put(`subscriptions/${subscriptionId}/owner`, {
+    retainPreviousOwner: boolean): Promise<OneSignalApiBaseResponse> {
+      const { appId } = requestMetadata;
+      return OneSignalApiBase.put(`${appId}/subscriptions/${subscriptionId}/owner`, {
         identity,
         retain_previous_owner: retainPreviousOwner
       });
