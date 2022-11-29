fedd8b3b07d6b5c67c6e1048ee99c7f63ed6164d
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-11-28 23:26:50 -0600

    Make `properties` optional in case we only want to send `deltas`

--- src/core/requestService/UpdateUserPayload.ts
+++ src/core/requestService/UpdateUserPayload.ts
@@ -1,7 +1,7 @@
 import { UserPropertiesModel } from "../models/UserPropertiesModel";
 
 export type UpdateUserPayload = {
-  properties: UserPropertiesModel,
+  properties?: UserPropertiesModel,
   refresh_device_metadata?: boolean,
   // TO DO: narrow deltas type to include only session deltas
   deltas?: Partial<UserPropertiesModel>,
