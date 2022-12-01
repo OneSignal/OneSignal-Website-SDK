ec7dde8e338580313b31627b5b7814e6e00ecb66
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-12-01 14:35:10 -0600

    Remove Device Id from Session Data
    Motivation: with user-model we are dropping the device model in favor of user centric model

--- src/sw/managers/sessionManager/SessionManager.ts
+++ src/sw/managers/sessionManager/SessionManager.ts
@@ -1,5 +1,4 @@
 import { ContextSWInterface } from "../../../shared/models/ContextSW";
-import { PushDeviceRecord } from "../../../shared/models/PushDeviceRecord";
 import { ISessionManager } from "../../../shared/managers/sessionManager/types";
 import { SessionOrigin } from "../../../shared/models/Session";
 
@@ -7,10 +6,12 @@ export class SessionManager implements ISessionManager {
   constructor(_context: ContextSWInterface) { }
 
   async upsertSession(
-    _deviceId: string,
-    _deviceRecord: PushDeviceRecord,
     _sessionOrigin: SessionOrigin
   ): Promise<void> {
     // TODO: how should it be implemented if called from inside of service worker???
+  }
+
+  async setupSessionEventListeners(): Promise<void> {
+    // TO DO
   }
 }
