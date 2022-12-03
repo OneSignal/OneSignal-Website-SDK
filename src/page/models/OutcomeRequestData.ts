cbc5996598cef591570fd9201ea7df0030d01035
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-12-02 18:56:01 -0600

    Update `OutcomeRequestData`
    * Remove `device_type`. We will rely on `subscription.type`
    * Add `session_time`, `subscription`, `onesignal_id`

--- src/page/models/OutcomeRequestData.ts
+++ src/page/models/OutcomeRequestData.ts
@@ -1,8 +1,12 @@
+import { SubscriptionModel } from "../../core/models/SubscriptionModels";
+
 export interface OutcomeRequestData {
   app_id: string;
   id: string;
-  device_type: number;
   direct?: boolean;
   notification_ids?: string[];
   weight?: number;
+  session_time?: number;
+  subscription?: SubscriptionModel;
+  onesignal_id?: string;
 }
