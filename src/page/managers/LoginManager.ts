0aa2be5f17a1686378fbaf117d5288ea5da9de31
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-12-07 17:12:03 -0600

    Remove login stubs

--- src/page/managers/LoginManager.ts
+++ src/page/managers/LoginManager.ts
@@ -119,25 +119,10 @@ export default class LoginManager {
   static async fetchAndHydrate(onesignalId: string): Promise<void> {
     logMethodCall("LoginManager.fetchAndHydrate", { onesignalId });
 
-    /* TO DO: uncomment
     const fetchUserResponse = await RequestService.getUser(
-      new AliasPair("onesignalId", onesignalId)
+      { appId: await MainHelper.getAppId() },
+      new AliasPair("onesignal_id", onesignalId)
     );
-    */
-    const fetchUserResponse = {
-      result: {
-        identity: {
-          onesignalId,
-          externalId: "99999",
-        },
-        properties: {
-          tags: {
-            tag1: "tag1",
-          },
-        },
-        subscriptions: [],
-      },
-    };
 
     await OneSignal.coreDirector.hydrateUser(fetchUserResponse?.result).catch(e => {
       Log.error("Error hydrating user models", e);
