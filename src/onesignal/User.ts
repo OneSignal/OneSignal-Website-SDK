41777fc280393caa392111882c16e0c129807be1
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2022-12-08 18:42:28 -0600

    Nits

--- src/onesignal/User.ts
+++ src/onesignal/User.ts
@@ -1,14 +1,13 @@
 import Environment from "../shared/helpers/Environment";
 import { OSModel } from "../core/modelRepo/OSModel";
 import { IdentityModel, SupportedIdentity } from "../core/models/IdentityModel";
-import { FutureSubscriptionModel, SupportedSubscription } from "../core/models/SubscriptionModels";
+import { SupportedSubscription } from "../core/models/SubscriptionModels";
 import { ModelName, SupportedModel } from "../core/models/SupportedModels";
 import { UserPropertiesModel } from "../core/models/UserPropertiesModel";
 import OneSignal from "./OneSignal";
 import Log from "../shared/libraries/Log";
 import { RequestService } from "../core/requestService/RequestService";
 import UserData from "../core/models/UserData";
-import FuturePushSubscriptionRecord from "../page/userModel/FuturePushSubscriptionRecord";
 import MainHelper from "../shared/helpers/MainHelper";
 import { isIdentityObject } from "../core/utils/typePredicates";
 import OneSignalError from "../shared/errors/OneSignalError";
