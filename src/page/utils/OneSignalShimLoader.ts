dbe67cd04347bc5891bbbd560c3b00cc0aff3a3f
Josh Kasten <josh@onesignal.com>
2022-12-14 17:53:41 -0500

    Simplified shim loader
    Greatly simplified the page SDK loading, this new behavior means we
    don't need function stubs anymore
    These are not needed as we expect everyone to use
    `OneSignalDeferred.push(function(onesignal) {});`.
    Direct calls without going through the new deferred API won't be
    supported in the major release.
    
    We also removed the check for a pre-defined OneSignal, and didn't
    translate this into OneSignalDeferred as that isn't something a
    developer would clobber by mistake.
    
    In a follow up commit as part of this PR we will be removing all the
    stub code itself and it's usages.

--- src/page/utils/OneSignalShimLoader.ts
+++ src/page/utils/OneSignalShimLoader.ts
@@ -1,7 +1,4 @@
 import { isPushNotificationsSupported } from "./BrowserSupportsPush";
-import { OneSignalStubES6 } from "./OneSignalStubES6";
-import { OneSignalStubES5 } from "./OneSignalStubES5";
-import { PossiblePredefinedOneSignal } from "./OneSignalStub";
 // NOTE: Careful if adding imports, ES5 targets can't clean up functions never called.
 
 // See sdk.ts for what entry points this handles
@@ -41,38 +38,18 @@ export class OneSignalShimLoader {
     }
   }
 
-  private static addOneSignalPageES6SDKStub(): void {
-    const predefinedOneSignal: PossiblePredefinedOneSignal = (<any>window).OneSignal;
-    const oneSignalIsArray = Array.isArray(predefinedOneSignal);
-
-    // Do NOT replace window.OneSignal if it's something else other than an Array.
-    if (!!predefinedOneSignal && !oneSignalIsArray) {
-      console.error(
-        `window.OneSignal already defined as '${typeof OneSignal}'!
-         Please make sure to define as 'window.OneSignal = window.OneSignal || [];'`,
-        OneSignal
-      );
-      return;
-    }
-  }
-
-  // Stub out all functions with default values.
-  // OneSignalStubES5 class is bundled into the production OneSignalSDK.js so other .js files are loaded.
-  private static addOneSignalPageES5SDKStub(): void {
-    console.log("OneSignal: Using fallback ES5 Stub for backwards compatibility.");
-    const predefinedOneSignal: PossiblePredefinedOneSignal = (<any>window).OneSignal;
-    (<any>window).OneSignal = new OneSignalStubES5(predefinedOneSignal);
+  private static loadFullPageSDK(): void {
+    OneSignalShimLoader.addScriptToPage(
+      `${OneSignalShimLoader.getPathAndPrefix()}OneSignalPageSDKES6.js?v=${OneSignalShimLoader.VERSION}`
+    );
   }
 
   public static start(): void {
     if (isPushNotificationsSupported()) {
-      OneSignalShimLoader.addScriptToPage(
-        `${OneSignalShimLoader.getPathAndPrefix()}OneSignalPageSDKES6.js?v=${OneSignalShimLoader.VERSION}`
-      );
-      OneSignalShimLoader.addOneSignalPageES6SDKStub();
+      OneSignalShimLoader.loadFullPageSDK();
     }
     else {
-      OneSignalShimLoader.addOneSignalPageES5SDKStub();
+      console.log("OneSignal: SDK is not compatible with this browser.");
     }
   }
 }
