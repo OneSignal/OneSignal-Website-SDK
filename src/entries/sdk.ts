62a811e4ea68ba958e4befd83a1773e706cb95e5
Josh Kasten <josh@onesignal.com>
2022-12-15 15:15:24 -0500

    Clean up comments in sdk.ts
    Simplified comments

--- src/entries/sdk.ts
+++ src/entries/sdk.ts
@@ -1,11 +1,9 @@
 /**
  * This is OneSignalSDK.js (ES5)
  *   * This is an entry point for pages
- *       - Developers are instructed to use OneSignalSDKWorker.js in their Service Worker instead
- *   * Also we define a ES5 Stub for OneSignal for browsers that do not support push.
- * This is a shim to detect and load either;
- *   * PageSDK (ES6)          - OneSignalPageSDKES6.js
- *   * Stub-PageSDK (ES5)     - OneSignalSDK.js (This File)
+ * This is a shim to detect if the browser supports the full OneSignal SDK before loading it.
+ *   * Full PageSDK (ES6) - OneSignalPageSDKES6.js
+ *   * Shim PageSDK (ES5) - OneSignalSDK.js (This File)
  */
 
 // NOTE: Careful if adding imports, ES5 targets can't clean up functions never called.
