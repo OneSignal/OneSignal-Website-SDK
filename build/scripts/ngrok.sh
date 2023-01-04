701da0c094251aef7958ac002df89ff4c5682e8a
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-01-03 19:23:14 -0600

    Add `ngrok` script to faciliate easily serving localhost to the web
    Motivation: Problem: you need a quick and easy way to test your changes on a different machine or share the sandbox environment with others.
    
    Solution: ngrok is a programmable reverse proxy that lets you instantly serve your localhost environment to the web.

---
+++ build/scripts/ngrok.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Check if ngrok is installed
+if ! [ -x "$(command -v ngrok)" ]; then
+  echo 'ngrok is not installed.'
+  echo 'Please install ngrok to use this script.'
+  exit 1
+fi
+
+echo 'ngrok is installed. Continuing with script execution.'
+
+# Kill any existing ngrok processes
+killall ngrok
+
+# Start ngrok
+ngrok http https://localhost:4001 --log=ngrok.log > /dev/null &
+echo "starting ngrok..."
+
+# Wait for ngrok to start
+sleep 5
+
+# Get the ngrok url
+read -r rawurl < <(grep -o "https://[^ ]*\.ngrok\.io" ngrok.log)
+url=$(echo $rawurl | sed 's/https:\/\///')
+echo "ngrok url: $url"
+
+rm ngrok.log
+
+# Build the service worker file
+echo "building service worker file"
+./build/scripts/buildServiceWorker.sh $url
+
+# Build the SDK
+echo "building SDK with build origin $url"
+docker-compose exec onesignal-web-sdk-dev yarn build:dev-prod -b $url --no-port
+echo "Done."
+
+echo "Last step: update the SDK script src to https://$url/sdks/Dev-OneSignalSDK.js"
+
+# Open the ngrok url
+open "https://$url"
