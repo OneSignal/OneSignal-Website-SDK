18baf8a1cf0334fa28c4c01f5ab7b097bcc56044
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-01-05 17:52:38 -0600

    Make CoreDirector functions synchronous
    Motivation: will help avoid race condition due to having to await on core director functions. Example: if we add a tag then login, due to the fact that we have to await on the core director functions, the the tag update will not happen in time before the login function force-flushes the delta queue.

--- src/core/CoreModuleDirector.ts
+++ src/core/CoreModuleDirector.ts
@@ -21,7 +21,7 @@ export class CoreModuleDirector {
 
   constructor(private core: CoreModule) {}
 
-  public async generatePushSubscriptionModel(rawPushSubscription: RawPushSubscription): Promise<void> {
+  public generatePushSubscriptionModel(rawPushSubscription: RawPushSubscription): void {
     logMethodCall("CoreModuleDirector.generatePushSubscriptionModel", { rawPushSubscription });
     // new subscription
     const pushModel = new OSModel<SupportedSubscription>(
@@ -34,19 +34,18 @@ export class CoreModuleDirector {
       pushModel.setOneSignalId(user.onesignalId);
     }
     // don't propagate since we will be including the subscription in the user create call
-    await OneSignal.coreDirector.add(ModelName.PushSubscriptions, pushModel as OSModel<SupportedModel>, false);
+    OneSignal.coreDirector.add(ModelName.PushSubscriptions, pushModel as OSModel<SupportedModel>, false);
   }
 
   public async resetModelRepoAndCache(): Promise<void> {
     await this.core.resetModelRepoAndCache();
   }
 
-  public async hydrateUser(user: UserData): Promise<void> {
+  public hydrateUser(user: UserData): void {
     logMethodCall("CoreModuleDirector.hydrateUser", { user });
     try {
-      await this.initPromise;
-      const identity = await this.getIdentityModel();
-      const properties = await this.getPropertiesModel();
+      const identity = this.getIdentityModel();
+      const properties = this.getPropertiesModel();
 
       const { onesignal_id: onesignalId } = user.identity;
 
@@ -64,23 +63,20 @@ export class CoreModuleDirector {
 
       // subscriptions are duplicable, so we hydrate them separately
       // when hydrating, we should have the full subscription object (i.e. include ID from server)
-      this._hydrateSubscriptions(user.subscriptions as SubscriptionModel[], onesignalId).catch(e => {
-        throw new OneSignalError(`Error hydrating subscriptions: ${e}`);
-      });
+      this._hydrateSubscriptions(user.subscriptions as SubscriptionModel[], onesignalId);
     } catch (e) {
       Log.error(`Error hydrating user: ${e}`);
     }
   }
 
-  private async _hydrateSubscriptions(subscriptions: SubscriptionModel[], onesignalId: string): Promise<void> {
+  private _hydrateSubscriptions(subscriptions: SubscriptionModel[], onesignalId: string): void {
     logMethodCall("CoreModuleDirector._hydrateSubscriptions", { subscriptions });
 
     if (!subscriptions) {
       return;
     }
 
-    await this.initPromise;
-    const modelStores = await this.getModelStores();
+    const modelStores = this.getModelStores();
 
     const getModelName = (subscription: SupportedSubscription) => {
       if (subscription.type === "Email") {
@@ -99,7 +95,7 @@ export class CoreModuleDirector {
        * We don't want to create a new model in this case, so we use the token to identify the model.
        */
       const existingSubscription = !!subscription.token ?
-        await this.getSubscriptionOfTypeWithToken(modelName, subscription.token) :
+        this.getSubscriptionOfTypeWithToken(modelName, subscription.token) :
         undefined;
 
       if (existingSubscription) {
@@ -122,49 +118,44 @@ export class CoreModuleDirector {
 
   /* O P E R A T I O N S */
 
-  public async add(modelName: ModelName, model: OSModel<SupportedModel>, propagate: boolean = true): Promise<void> {
+  public add(modelName: ModelName, model: OSModel<SupportedModel>, propagate: boolean = true): void {
     logMethodCall("CoreModuleDirector.add", { modelName, model });
-    const modelStores = await this.getModelStores();
+    const modelStores = this.getModelStores();
     modelStores[modelName].add(model, propagate);
   }
 
-  public async remove(modelName: ModelName, modelId: string): Promise<void> {
+  public remove(modelName: ModelName, modelId: string): void {
     logMethodCall("CoreModuleDirector.remove", { modelName, modelId });
-    const modelStores = await this.getModelStores();
+    const modelStores = this.getModelStores();
     modelStores[modelName].remove(modelId);
   }
 
   /* G E T T E R S */
 
-  public async getModelByTypeAndId(modelName: ModelName, modelId: string):
-    Promise<OSModel<SupportedModel> | undefined> {
+  public getModelByTypeAndId(modelName: ModelName, modelId: string): OSModel<SupportedModel> | undefined {
       logMethodCall("CoreModuleDirector.getModelByTypeAndId", { modelName, modelId });
-      await this.initPromise;
-      const modelStores = await this.getModelStores();
+      const modelStores = this.getModelStores();
       return modelStores[modelName].models[modelId];
   }
 
-  public async getEmailSubscriptionModels(): Promise<{ [key: string]: OSModel<SupportedSubscription> }> {
+  public getEmailSubscriptionModels(): { [key: string]: OSModel<SupportedSubscription> } {
     logMethodCall("CoreModuleDirector.getEmailSubscriptionModels");
-    await this.initPromise;
-    const modelStores = await this.getModelStores();
+    const modelStores = this.getModelStores();
     return modelStores.emailSubscriptions.models as { [key: string]: OSModel<SupportedSubscription> };
   }
 
-  public async getSmsSubscriptionModels(): Promise<{ [key: string]: OSModel<SupportedSubscription> }> {
+  public getSmsSubscriptionModels(): { [key: string]: OSModel<SupportedSubscription> } {
     logMethodCall("CoreModuleDirector.getSmsSubscriptionModels");
-    await this.initPromise;
-    const modelStores = await this.getModelStores();
+    const modelStores = this.getModelStores();
     return modelStores.smsSubscriptions.models as { [key: string]: OSModel<SupportedSubscription> };
   }
 
   /**
    * Returns all push subscription models, including push subscriptions from other browsers.
    */
-  public async getAllPushSubscriptionModels(): Promise<{ [key: string]: OSModel<SupportedSubscription> }> {
+  public getAllPushSubscriptionModels(): { [key: string]: OSModel<SupportedSubscription> } {
     logMethodCall("CoreModuleDirector.getAllPushSubscriptionModels");
-    await this.initPromise;
-    const modelStores = await this.getModelStores();
+    const modelStores = this.getModelStores();
     return modelStores.pushSubscriptions.models as { [key: string]: OSModel<SupportedSubscription> };
   }
 
@@ -172,9 +163,9 @@ export class CoreModuleDirector {
    * Gets the current push subscription model for the current browser.
    * @returns The push subscription model for the current browser, or undefined if no push subscription exists.
    */
+  // TO DO: make synchronous by making getting the current push token synchronous
   public async getCurrentPushSubscriptionModel(): Promise<OSModel<SupportedSubscription> | undefined> {
     logMethodCall("CoreModuleDirector.getPushSubscriptionModel");
-    await this.initPromise;
     const pushToken = await MainHelper.getCurrentPushToken();
 
     if (!pushToken) {
@@ -184,27 +175,24 @@ export class CoreModuleDirector {
     return this.getSubscriptionOfTypeWithToken(ModelName.PushSubscriptions, pushToken);
   }
 
-  public async getIdentityModel(): Promise<OSModel<SupportedIdentity> | undefined> {
+  public getIdentityModel(): OSModel<SupportedIdentity> | undefined {
     logMethodCall("CoreModuleDirector.getIdentityModel");
-    await this.initPromise;
-    const modelStores = await this.getModelStores();
+    const modelStores = this.getModelStores();
     const modelKeys = Object.keys(modelStores.identity.models);
     return modelStores.identity.models[modelKeys[0]] as OSModel<SupportedIdentity>;
   }
 
-  public async getPropertiesModel(): Promise<OSModel<UserPropertiesModel> | undefined> {
+  public getPropertiesModel(): OSModel<UserPropertiesModel> | undefined {
     logMethodCall("CoreModuleDirector.getPropertiesModel");
-    await this.initPromise;
-    const modelStores = await this.getModelStores();
+    const modelStores = this.getModelStores();
     const modelKeys = Object.keys(modelStores.properties.models);
     return modelStores.properties.models[modelKeys[0]] as OSModel<UserPropertiesModel>;
   }
 
   public async getAllSubscriptionsModels(): Promise<OSModel<SupportedSubscription>[]> {
     logMethodCall("CoreModuleDirector.getAllSubscriptionsModels");
-    await this.initPromise;
-    const emailSubscriptions = await this.getEmailSubscriptionModels();
-    const smsSubscriptions = await this.getSmsSubscriptionModels();
+    const emailSubscriptions = this.getEmailSubscriptionModels();
+    const smsSubscriptions = this.getSmsSubscriptionModels();
     const pushSubscription = await this.getCurrentPushSubscriptionModel();
 
     const subscriptions = Object.values(emailSubscriptions)
@@ -213,20 +201,18 @@ export class CoreModuleDirector {
     return subscriptions;
   }
 
-  public async getSubscriptionOfTypeWithToken(type: ModelName, token: string):
-    Promise<OSModel<SupportedSubscription> | undefined>
+  public getSubscriptionOfTypeWithToken(type: ModelName, token: string): OSModel<SupportedSubscription> | undefined
     {
       logMethodCall("CoreModuleDirector.getSubscriptionOfTypeWithToken", { type, token });
-      await this.initPromise;
       switch (type) {
         case ModelName.EmailSubscriptions:
-          const emailSubscriptions = await this.getEmailSubscriptionModels();
+          const emailSubscriptions = this.getEmailSubscriptionModels();
           return Object.values(emailSubscriptions).find(subscription => subscription.data.token === token);
         case ModelName.SmsSubscriptions:
-          const smsSubscriptions = await this.getSmsSubscriptionModels();
+          const smsSubscriptions = this.getSmsSubscriptionModels();
           return Object.values(smsSubscriptions).find(subscription => subscription.data.token === token);
         case ModelName.PushSubscriptions:
-          const pushSubscriptions = await this.getAllPushSubscriptionModels();
+          const pushSubscriptions = this.getAllPushSubscriptionModels();
           return Object.values(pushSubscriptions).find(subscription => subscription.data.token === token);
         default:
           return undefined;
@@ -235,8 +221,7 @@ export class CoreModuleDirector {
 
   /* P R I V A T E */
 
-  private async getModelStores(): Promise<ModelStoresMap<SupportedModel>> {
-    await this.initPromise;
+  private getModelStores(): ModelStoresMap<SupportedModel> {
     return (this.core.modelRepo?.modelStores as ModelStoresMap<SupportedModel>);
   }
 }
