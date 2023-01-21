12ec31d23352f1364a37d2e80b4c40f73016672a
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-01-20 18:12:39 -0600

    Add blank CoreModule test files

---
+++ __test__/core/modelCache.test.ts
@@ -0,0 +1,11 @@
+describe('ModelCache tests', () => {
+  test('ModelCache adds model to IndexedDB', async () => {});
+  test('ModelCache adds multiple models to IndexedDB', async () => {});
+  test('ModelCache updates model in IndexedDB', async () => {});
+  test('ModelCache removes model from IndexedDB', async () => {});
+  test('ModelCache `decode` returns undefined when no models exist', async () => {});
+  test('ModelCache `decode` returns model when one model exists', async () => {});
+  test('ModelCache `decode` returns models when multiple models exist (same name)', async () => {});
+  test('ModelCache `decode` returns models when multiple models exist (different name)', async () => {});
+  test('Multiple ModelCache updates result in correct IndexedDB state (mutex working)', async () => {});
+});
---
+++ __test__/core/osModel.test.ts
@@ -0,0 +1,7 @@
+describe('OSModel tests', () => {
+  test('Set function updates data', async () => {});
+  test('Set function broadcasts update event', async () => {});
+  test('Hydrate function updates data', async () => {});
+  test('Encode function returns encoded model', async () => {});
+  test('Decode function returns decoded model', async () => {});
+});
