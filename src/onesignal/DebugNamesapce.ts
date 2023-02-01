266d984b1a7539e234170fe1bdd26d9823afa53c
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-01-31 18:38:09 -0600

    Debug namespace with logging function
    Motivation: Debug namespace will match other SDKs and provide a place for future potential debug-related functions
    
    Move the `setLogLevel` function to the new namespace

---
+++ src/onesignal/DebugNamesapce.ts
@@ -0,0 +1,11 @@
+import Log from "../shared/libraries/Log";
+
+export default class DebugNamespace {
+  /**
+   * @PublicApi
+   * @param logLevel - string
+   */
+  setLogLevel(logLevel: string) {
+    Log.setLevel(logLevel);
+  }
+}
