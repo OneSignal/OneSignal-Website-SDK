73d31b17d921bea357865d7f2a495ce801c18fcb
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-02-21 18:53:22 -0600

    Jest configuration files and types
    Motivation: includes jets config and types for the custom `stub` function on the `It` object (`test`) for easier stubbing

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @returns {Promise<import('jest').Config>} */
+module.exports = async () => {
+  return {
+    verbose: true,
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    // Run these files after jest has been
+    // installed in the environment
+    setupFilesAfterEnv: ["<rootDir>/__test__/jest/jest.setup.ts"], // use .js if you prefer JavaScript,
+    // hide type errors: https://stackoverflow.com/questions/73687337/disable-type-checking-stopped-working-after-ts-jest-upgrade-to-29-0
+    globals: {
+      'ts-jest': {
+        diagnostics: {
+          exclude: ['**'],
+        },
+      },
+    },
+  };
+};
