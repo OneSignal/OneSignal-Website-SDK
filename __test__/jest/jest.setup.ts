6cf37067c3f0318c9f48e205615019d5f2ae4dfa
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-02-23 16:46:34 -0600

    Add `fail` function to jest customization file
    Motivation: we can use `fail` in the future to force-fail tests.

--- __test__/jest/jest.setup.ts
+++ __test__/jest/jest.setup.ts
@@ -4,6 +4,10 @@ test.stub = (obj: any, method: string, returnValue?: any) => {
   return stub;
 };
 
+test.fail = () => {
+  expect(true).toBe(false);
+}
+
 // workaround for open upstream issue: https://github.com/inrupt/solid-client-authn-js/issues/1676
 import { TextEncoder, TextDecoder } from 'util';
 (global as any).TextEncoder = TextEncoder;
