1073d3c373e56c86c37be6a3d5c5c9fa23b253ea
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-03-03 17:50:33 -0600

    Add ability to set the port in the service worker file

--- build/scripts/buildServiceWorker.sh
+++ build/scripts/buildServiceWorker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Builds the local service worker file for express_webpack for use in local dev env
-echo "importScripts(\"https://${1}/sdks/Dev-OneSignalSDKWorker.js\");" > express_webpack/push/onesignal/OneSignalSDKWorker.js
+echo "importScripts(\"https://${1}${2}/sdks/Dev-OneSignalSDKWorker.js\");" > express_webpack/push/onesignal/OneSignalSDKWorker.js
