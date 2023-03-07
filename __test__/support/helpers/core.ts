51d67213852893e3432f1eba60d29193051f9ff0
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-03-07 17:33:04 -0600

    Update core helper file with new constants, type fixes

--- __test__/support/helpers/core.ts
+++ __test__/support/helpers/core.ts
@@ -1,8 +1,10 @@
+import { SupportedIdentity } from "../../../src/core/models/IdentityModel";
 import { OSModel } from "../../../src/core/modelRepo/OSModel";
 import { CoreChangeType } from "../../../src/core/models/CoreChangeType";
 import { CoreDelta } from "../../../src/core/models/CoreDeltas";
 import { SupportedSubscription, SubscriptionType } from "../../../src/core/models/SubscriptionModels";
 import { ModelName, SupportedModel } from "../../../src/core/models/SupportedModels";
+import { DUMMY_MODEL_ID, DUMMY_PUSH_TOKEN, DUMMY_SUBSCRIPTION_ID } from "../constants";
 
 
 export function generateNewSubscription(modelId = '0000000000') {
@@ -20,14 +22,22 @@ export function generateNewSubscription(modelId = '0000000000') {
 export function getMockDeltas(): CoreDelta<SupportedModel>[] {
   return [
     {
-      model: getDummyIdentityOSModel(),
+      model: getDummyIdentityOSModel() as OSModel<SupportedModel>,
       changeType: CoreChangeType.Add,
     }
   ];
 }
 
-export function getDummyIdentityOSModel(): OSModel<SupportedModel> {
-  return new OSModel<SupportedModel>(ModelName.Identity, {}, "123");
+export function getDummyIdentityOSModel(): OSModel<SupportedIdentity> {
+  return new OSModel<SupportedIdentity>(ModelName.Identity, {}, DUMMY_MODEL_ID);
+}
+
+export function getDummyPushSubscriptionOSModel(): OSModel<SupportedSubscription> {
+  return new OSModel<SupportedSubscription>(ModelName.PushSubscriptions, {
+    type: SubscriptionType.ChromePush,
+    id: DUMMY_SUBSCRIPTION_ID,
+    token: DUMMY_PUSH_TOKEN,
+  }, DUMMY_MODEL_ID);
 }
 
 export const passIfBroadcastNTimes = (target: number, broadcastCount: number, pass: () => void) => {
