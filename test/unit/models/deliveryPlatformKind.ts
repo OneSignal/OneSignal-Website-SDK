22a70cdc00a9be67d70564167af03f874f18b57c
Josh Kasten <josh@onesignal.com>
2023-03-08 22:34:51 -0800

    rename enum Safari to SafariLegacy
    This clears up any confusion between the two Safari enums.

--- test/unit/models/deliveryPlatformKind.ts
+++ test/unit/models/deliveryPlatformKind.ts
@@ -4,7 +4,7 @@ import { DeliveryPlatformKind } from '../../../src/models/DeliveryPlatformKind';
 
 test(`delivery platform constants should be correct`, async t => {
   t.is(DeliveryPlatformKind.ChromeLike, 5);
-  t.is(DeliveryPlatformKind.Safari, 7);
+  t.is(DeliveryPlatformKind.SafariLegacy, 7);
   t.is(DeliveryPlatformKind.Firefox, 8);
   t.is(DeliveryPlatformKind.Edge, 12);
   t.is(DeliveryPlatformKind.SafariVapid, 17);
