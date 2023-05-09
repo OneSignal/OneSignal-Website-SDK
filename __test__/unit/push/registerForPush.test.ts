967b7f36ed67c3ebeb37a8b4a33a507298b6609b
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-05-08 20:12:53 -0500

    registerForPush Unit Tests

---
+++ __test__/unit/push/registerForPush.test.ts
@@ -0,0 +1,48 @@
+import { HttpHttpsEnvironment } from "../../support/models/HttpHttpsEnvironment";
+import { TestEnvironment } from "../../support/environment/TestEnvironment";
+import InitHelper from "../../../src/shared/helpers/InitHelper";
+import OneSignalEvent from "../../../src/shared/services/OneSignalEvent";
+import BrowserUserAgent from "../../support/models/BrowserUserAgent";
+
+
+//stub dismisshelper
+jest.mock('../../../src/shared/helpers/DismissHelper');
+
+describe('Register for push', () => {
+  beforeEach(async () => {
+    jest.useFakeTimers();
+    await TestEnvironment.initialize({
+      addPrompts: true,
+      httpOrHttps: HttpHttpsEnvironment.Https,
+      userAgent: BrowserUserAgent.Default,
+    });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('registerForPushNotifications: before OneSignal.initialized', async () => {
+    (global as any).OneSignal.initialized = false;
+    (global as any).OneSignal._initCalled = false;
+
+    const spy = jest.spyOn(InitHelper, 'registerForPushNotifications')
+    const promise = OneSignal.User.PushSubscription.optIn();
+
+    expect(spy).not.toHaveBeenCalled();
+    OneSignalEvent.trigger(OneSignal.EVENTS.SDK_INITIALIZED);
+    await promise;
+    expect(OneSignal.initialized).toBe(true);
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  test('registerForPushNotifications: after OneSignal.initialized', async () => {
+    (global as any).OneSignal.initialized = true;
+    (global as any).OneSignal._initCalled = false;
+
+    const spy = jest.spyOn(InitHelper, 'registerForPushNotifications')
+    await InitHelper.registerForPushNotifications();
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
+
