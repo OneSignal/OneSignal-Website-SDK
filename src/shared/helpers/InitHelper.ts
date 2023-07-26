04c97aed1731552a7e7a12dc73f97c73179bc1f8
Josh Kasten <josh@onesignal.com>
2023-07-26 17:01:20 +0000

    remove unnecessary optedOut check
    Since we no longer support the os.tc workaround there is no need to
    check the the indexDb for user preferences. Native notification
    permission status is checked later making this unnecessary as well from
    normal HTTPS setups.

--- src/shared/helpers/InitHelper.ts
+++ src/shared/helpers/InitHelper.ts
@@ -118,6 +118,8 @@ export default class InitHelper {
     await OneSignalEvent.trigger(OneSignal.EVENTS.SDK_INITIALIZED);
   }
 
+  // In-Addition to registering for push, this shows a native browser
+  // notification permission prompt, if needed.
   public static async registerForPushNotifications(options: RegisterOptions = {}): Promise<void> {
     if (options && options.modalPrompt) {
       /* Show the HTTPS fullscreen modal permission message. */
@@ -146,15 +148,7 @@ export default class InitHelper {
       return;
     }
 
-    /*
-     * We don't want to resubscribe if the user is opted out, and we can't check on HTTP, because the promise will
-     * prevent the popup from opening.
-     */
-    const subscription = await Database.getSubscription();
-    const isOptedOut = subscription.optedOut;
-    if (!isOptedOut) {
-      await SubscriptionHelper.registerForPush();
-    }
+    await SubscriptionHelper.registerForPush();
   }
 
   /**
