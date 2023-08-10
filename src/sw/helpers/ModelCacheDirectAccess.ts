e1547e3ad602deb1feefcd2eace78f56db8cd3e6
Josh Kasten <josh@onesignal.com>
2023-08-10 01:14:56 +0000

    fix confirm deliveries & notification open
    Both confirm deliveries & notification open were building their REST API
    with an old placeholder deviceId value from Database.getSubscription().
    To fix the problem we are now using the model cache where we started to
    store the subscription id with v16.
    
    This commit is scoped to fixing this deviceId issue with confirm
    deliveries & notification open only. A larger change is needed to
    clean this up completely.

---
+++ src/sw/helpers/ModelCacheDirectAccess.ts
@@ -0,0 +1,20 @@
+import EncodedModel from "../../core/caching/EncodedModel";
+import { ModelName } from "../../core/models/SupportedModels";
+import Database from "../../shared/services/Database";
+
+/**
+ * WARNING: This is a temp workaround for the ServiceWorker context only!
+ * PURPOSE: CoreModuleDirector doesn't work in the SW context.
+ * TODO: This is duplicated logic tech debt to address later
+ */
+export class ModelCacheDirectAccess {
+  static async getPushSubscriptionIdByToken(token: string): Promise<string | undefined> {
+    const pushSubscriptions = await Database.getAll<EncodedModel>(ModelName.PushSubscriptions);
+    for(const pushSubscription of pushSubscriptions) {
+      if (pushSubscription["token"] === token) {
+        return pushSubscription["id"] as string;
+      }
+    }
+    return undefined;
+  }
+}
