ff9a2166463f9c60d12c59ce7fce7978d29f2fcc
Josh Kasten <josh@onesignal.com>
2023-09-29 00:12:20 +0000

    Re-enable welcome notifications
    This existing code still uses include_player_ids on the OneSignal REST
    API POST /notifications. include_player_ids will be dropped by 2025 so
    a replacement will needed by then. This will most likely be a backend
    feature which will be a hook on a new push subscription create.

--- src/shared/helpers/EventHelper.ts
+++ src/shared/helpers/EventHelper.ts
@@ -85,7 +85,10 @@ export default class EventHelper {
   static async _onSubscriptionChanged(
     change: SubscriptionChangeEvent | undefined,
   ) {
-    // EventHelper.onSubscriptionChanged_showWelcomeNotification(change?.current.enabled);
+    EventHelper.onSubscriptionChanged_showWelcomeNotification(
+      change?.current?.optedIn,
+      change?.current?.id,
+    );
     EventHelper.onSubscriptionChanged_sendCategorySlidedownTags(
       change?.current?.optedIn,
     );
@@ -107,9 +110,15 @@ export default class EventHelper {
     }
   }
 
+  /**
+   * NOTE: This uses the OneSignal REST API POST /notifications with
+   * include_player_ids. This field will be dropped by 2025 so a
+   * replacement will needed by then.
+   */
   private static sendingOrSentWelcomeNotification = false;
   private static async onSubscriptionChanged_showWelcomeNotification(
     isSubscribed: boolean | undefined,
+    pushSubscriptionId: string | undefined | null,
   ) {
     if (OneSignal.__doNotShowWelcomeNotification) {
       Log.debug(
@@ -131,6 +140,10 @@ export default class EventHelper {
       return;
     }
 
+    if (!pushSubscriptionId) {
+      return;
+    }
+
     // Workaround only for this v15 branch; There are race conditions in the SDK
     // that result in the onSubscriptionChanged firing more than once sometimes.
     if (EventHelper.sendingOrSentWelcomeNotification) {
@@ -138,7 +151,6 @@ export default class EventHelper {
     }
     EventHelper.sendingOrSentWelcomeNotification = true;
 
-    const { deviceId } = await Database.getSubscription();
     const { appId } = await Database.getAppConfig();
     let title =
       welcome_notification_opts !== undefined &&
@@ -167,7 +179,7 @@ export default class EventHelper {
     Log.debug('Sending welcome notification.');
     OneSignalApiShared.sendNotification(
       appId,
-      [deviceId],
+      [pushSubscriptionId],
       { en: title },
       { en: message },
       url,
