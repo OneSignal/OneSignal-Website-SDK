d0d464154dc80614ee771ef703f9399ac78f903b
Rodrigo Gomez Palacio <rodrigo@onesignal.com>
2024-07-23 14:28:50 -0500

    Remove `sendNotification` API calls
    Motivation: cleaning up dead code. Shifting to use of local notifications

--- src/shared/api/OneSignalApiShared.ts
+++ src/shared/api/OneSignalApiShared.ts
@@ -1,41 +1,8 @@
 import { OutcomeRequestData } from '../../page/models/OutcomeRequestData';
-import Utils from '../context/Utils';
 import Log from '../libraries/Log';
 import OneSignalApiBase from './OneSignalApiBase';
 
 export default class OneSignalApiShared {
-  static sendNotification(
-    appId: string,
-    playerIds: Array<string>,
-    titles,
-    contents,
-    url,
-    icon,
-    data,
-    buttons,
-  ) {
-    const params = {
-      app_id: appId,
-      contents: contents,
-      include_player_ids: playerIds,
-      isAnyWeb: true,
-      data: data,
-      web_buttons: buttons,
-    };
-    if (titles) {
-      (params as any).headings = titles;
-    }
-    if (url) {
-      (params as any).url = url;
-    }
-    if (icon) {
-      (params as any).chrome_web_icon = icon;
-      (params as any).firefox_icon = icon;
-    }
-    Utils.trimUndefined(params);
-    return OneSignalApiBase.post('notifications', params);
-  }
-
   static async sendOutcome(data: OutcomeRequestData): Promise<void> {
     Log.info('Outcome payload:', data);
     try {
