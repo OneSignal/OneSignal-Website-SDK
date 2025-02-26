ad1a3b0023d3a3aca161ff8cdf9053cd7b20094a
Fadi George <fadii925@gmail.com>
2025-02-26 15:44:20 -0800

    vite build wip
    - add lib mode configurations
    - remove webpack packages and configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,4 +24,5 @@ module.exports = {
       },
     },
   ],
+  ignores: ['build/**/*'],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,54 @@
-import { defineConfig } from 'vite';
+import { defineConfig, LibraryOptions } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
+type Lib = 'sdk' | 'page' | 'worker';
+
+const libConfig: Record<Lib, LibraryOptions> = {
+  sdk: {
+    entry: './src/entries/sdk.ts',
+    fileName: () => 'OneSignalSdk.page.js',
+  },
+  page: {
+    entry: './src/entries/pageSdkInit.ts',
+    fileName: () => 'OneSignalSDK.page.es6.js',
+  },
+  worker: {
+    entry: './src/entries/worker.ts',
+    fileName: () => 'OneSignalSDK.sw.js',
+  },
+};
+
+/**
+ * Mode will be production if command is build otherwise it will be development unless manually set e.g.
+ * mode=staging vite ...
+ */
 export default defineConfig(({ mode }) => {
   const isDev = mode === 'development';
+  const lib = process.env.LIB as Lib;
+
   return {
     plugins: [mkcert()],
+    build: {
+      /**
+       * NOTE: Need to specify 2022 or later to not have declarations like
+       * `var It=Object.defineProperty;` above the IIFE.
+       */
+      target: 'es2022',
+      minify: process.env.ENV === 'production',
+      lib: {
+        ...libConfig[lib],
+        name: 'OneSignal',
+        formats: ['iife'],
+      },
+      outDir: 'build/releases',
+      emptyOutDir: true,
+      sourcemap: true,
+    },
     define: {
       __API_ORIGIN__: JSON.stringify(process.env.API_ORIGIN),
       __API_TYPE__: JSON.stringify(process.env.API),
       __BUILD_ORIGIN__: JSON.stringify(process.env.BUILD_ORIGIN),
-      __BUILD_TYPE__: JSON.stringify(mode),
+      __BUILD_TYPE__: JSON.stringify(process.env.ENV),
       __IS_HTTPS__: JSON.stringify(true),
       __LOGGING__: JSON.stringify(isDev),
       __NO_DEV_PORT__: JSON.stringify(process.env.NO_DEV_PORT),
