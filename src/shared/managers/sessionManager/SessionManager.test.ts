f75f32863ded2800b85d00bfbb2d62e19c7ed5eb
Josh Kasten <josh@onesignal.com>
2025-03-19 14:35:52 -0400

    removed unused mocks and switched to spyOn

--- src/shared/managers/sessionManager/SessionManager.test.ts
+++ src/shared/managers/sessionManager/SessionManager.test.ts
@@ -1,45 +1,27 @@
-import { SessionManager } from './SessionManager';
 import { TestEnvironment } from '__test__/support/environment/TestEnvironment';
+import { SessionManager } from './SessionManager';
 
-import { stub } from '__test__/support/helpers/general';
-import { PropertiesExecutor } from 'src/core/executors/PropertiesExecutor';
-import { IdentityExecutor } from 'src/core/executors/IdentityExecutor';
-import { SubscriptionExecutor } from 'src/core/executors/SubscriptionExecutor';
-
-import { setupLoginStubs } from '__test__/support/helpers/login';
-import LoginManager from 'src/page/managers/LoginManager';
 import {
   DUMMY_EXTERNAL_ID,
   DUMMY_ONESIGNAL_ID,
 } from '__test__/support/constants';
-
-vi.mock('../../../src/shared/libraries/Log');
-stub(PropertiesExecutor.prototype, 'getOperationsFromCache', []);
-stub(IdentityExecutor.prototype, 'getOperationsFromCache', []);
-stub(SubscriptionExecutor.prototype, 'getOperationsFromCache', []);
+import { setupLoginStubs } from '__test__/support/helpers/login';
+import LoginManager from 'src/page/managers/LoginManager';
 
 describe('SessionManager', () => {
-  afterEach(() => {
-    vi.resetAllMocks();
-  });
-
   describe('Switching Users', () => {
     beforeEach(async () => {
       await TestEnvironment.initialize({
         useMockIdentityModel: true,
-        useMockPushSubscriptionModel: true,
       });
+
       setupLoginStubs();
-      stub(
-        LoginManager,
-        'identifyOrUpsertUser',
-        Promise.resolve({
-          identity: {
-            external_id: DUMMY_EXTERNAL_ID,
-            onesignal_id: DUMMY_ONESIGNAL_ID,
-          },
-        }),
-      );
+      vi.spyOn(LoginManager, 'identifyOrUpsertUser').mockResolvedValue({
+        identity: {
+          external_id: DUMMY_EXTERNAL_ID,
+          onesignal_id: DUMMY_ONESIGNAL_ID,
+        },
+      });
     });
 
     test('handleOnFocus should wait for login promise', async () => {
