317973a01f6fb70812ec99529f6bf0d2e83701c2
Iryna Trush <iryna@onesignal.com>
2025-08-07 10:22:35 -0700

    Update things to be able to build web sdk locally

--- src/libraries/WorkerMessenger.ts
+++ src/libraries/WorkerMessenger.ts
@@ -192,7 +192,7 @@ export class WorkerMessenger {
     Log.debug(`(${location.origin}) [Worker Messenger] Page is now listening for messages.`);
   }
 
-  onWorkerMessageReceivedFromPage(event: ExtendableMessageEvent) {
+  onWorkerMessageReceivedFromPage(event: any) {
     const data: WorkerMessengerMessage = event.data;
 
     /* If this message doesn't contain our expected fields, discard the message */
@@ -235,7 +235,7 @@ export class WorkerMessenger {
   message topic. If no one is listening to the message, it is discarded;
   otherwise, the listener callback is executed.
   */
-  onPageMessageReceivedFromServiceWorker(event: ExtendableMessageEvent) {
+  onPageMessageReceivedFromServiceWorker(event: any) {
     const data: WorkerMessengerMessage = event.data;
 
     /* If this message doesn't contain our expected fields, discard the message */
--- typings/globals/serviceworker.d.ts
+++ typings/globals/serviceworker.d.ts
@@ -1,19 +1,18 @@
 declare module 'ServiceWorker' {
+    // eslint-disable-next-line no-var, @typescript-eslint/no-explicit-any
     var OneSignal: any;
     export = OneSignal;
 }
 
 interface PushSubscriptionChangeEvent extends ExtendableEvent {
-    readonly newSubscription: PushSubscription | null;
     readonly oldSubscription: PushSubscription | null;
-}
-
+    readonly newSubscription: PushSubscription | null;
+  }
+  
+/* eslint-disable no-var */
 declare var PushSubscriptionChangeEvent: {
     prototype: PushSubscriptionChangeEvent;
-    new(type: string, eventInitDict?: PushSubscriptionChangeEventInit): PushSubscriptionChangeEvent;
+    new(): PushSubscriptionChangeEvent;
 };
-
-interface PushSubscriptionChangeEventInit extends ExtendableEventInit {
-    newSubscription?: PushSubscription;
-    oldSubscription?: PushSubscription;
-}
+/* eslint-enable no-var */
+  
