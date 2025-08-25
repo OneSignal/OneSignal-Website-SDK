fc154960b6a1eb4742406b2d14881b3ea8b7ed9a
Fadi George <fadii925@gmail.com>
2025-08-25 14:05:16 -0700

    update process queue time

--- src/core/operationRepo/constants.ts
+++ src/core/operationRepo/constants.ts
@@ -1,5 +1,5 @@
 // Reference: OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/config/ConfigModel.kt
-export const OP_REPO_DEFAULT_FAIL_RETRY_BACKOFF = 5000;
-export const OP_REPO_POST_CREATE_DELAY = 3000;
-export const OP_REPO_EXECUTION_INTERVAL = 3000;
-export const OP_REPO_POST_CREATE_RETRY_UP_TO = 30_000;
+export const OP_REPO_DEFAULT_FAIL_RETRY_BACKOFF = 1000;
+export const OP_REPO_POST_CREATE_DELAY = 500;
+export const OP_REPO_EXECUTION_INTERVAL = 500;
+export const OP_REPO_POST_CREATE_RETRY_UP_TO = 15_000;
