b05ba5c900d08a9f584e3324174d389b368a58c6
Fadi George <fadii925@gmail.com>
2026-03-03 20:17:08 -0800

    test(bell): add _getTipForState method tests

--- src/page/bell/Message.test.ts
+++ src/page/bell/Message.test.ts
@@ -1,6 +1,7 @@
 import { TestEnvironment } from '__test__/support/environment/TestEnvironment';
 import { mockDelay } from '__test__/support/helpers/setup';
 import Bell from './Bell';
+import { BellState } from './constants';
 import Message from './Message';
 
 mockDelay();
@@ -22,5 +23,22 @@ describe('Message', () => {
     await message._display('message', 'Hello', 1000);
     expect(message._contentType).toBe('tip');
     expect(message._shown).toBe(false);
+  });
+
+  test('_getTipForState returns correct message per state', () => {
+    const bell = new Bell({ enable: false });
+    const message = new Message(bell);
+
+    bell._setState(BellState._Unsubscribed, true);
+    expect(message._getTipForState()).toBe('Subscribe to notifications');
+
+    bell._setState(BellState._Subscribed, true);
+    expect(message._getTipForState()).toBe("You're subscribed to notifications");
+
+    bell._setState(BellState._Blocked, true);
+    expect(message._getTipForState()).toBe("You've blocked notifications");
+
+    bell._setState(BellState._Uninitialized, true);
+    expect(message._getTipForState()).toBe('');
   });
 });
