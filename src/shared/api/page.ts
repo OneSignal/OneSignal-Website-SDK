ea565aadb20fdc69cbc56630cb4483e289fcc385
Fadi George <fadii925@gmail.com>
2026-03-23 13:18:37 -0700

    fix(api): add JSONP timeout and callback params

--- src/shared/api/page.ts
+++ src/shared/api/page.ts
@@ -8,7 +8,7 @@ export function jsonpLib(
   url: string,
   fn: (err: Error | null, data: ServerAppConfig) => void,
 ) {
-  JSONP(url, { prefix: '__jp', name: undefined }, fn);
+  JSONP(url, { prefix: '__jp', name: undefined, param: 'callback', timeout: 60000 }, fn);
 }
 
 export async function downloadServerAppConfig(
