27956ff5ed8abff0c661f43017a78f224558fbc6
Fadi George <fadii925@gmail.com>
2026-03-23 13:39:55 -0700

    style(api): format JSONP call for readability

--- src/shared/api/page.ts
+++ src/shared/api/page.ts
@@ -9,7 +9,11 @@ export function jsonpLib(
   fn: (err: Error | null, data: ServerAppConfig) => void,
 ) {
   // Explicit opts prevent prototype pollution
-  JSONP(url, { prefix: '__jp', name: undefined, param: 'callback', timeout: 60000 }, fn);
+  JSONP(
+    url,
+    { prefix: '__jp', name: undefined, param: 'callback', timeout: 60000 },
+    fn,
+  );
 }
 
 export async function downloadServerAppConfig(
